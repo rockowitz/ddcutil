e9247b34551c6080e9b87e1c2ef6c5baccf527c9
Sanford Rockowitz <rockowitz@minsoft.com>
2016-07-24 15:47:27 -0400

    fix comment

--- src/util/x11_util.h
+++ src/util/x11_util.h
@@ -35,7 +35,7 @@ typedef struct {
 } X11_Edid_Rec;
 
 
-GPtrArray * get_x11_edids();   // returns array of X11_Edid_rec
+GPtrArray * get_x11_edids();   // returns array of X11_Edid_Rec
 
 void free_x11_edids(GPtrArray * edidrecs);
 
