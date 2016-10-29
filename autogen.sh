206171fec9d6e2b3957250de71f45cc5214dce33
Sanford Rockowitz <rockowitz@minsoft.com>
2016-10-29 18:18:55 -0400

    1) Makefile.am: add target clang
    2) autogen.sh: modify for use by clang target
    
    3) .gitignore: more to ignore

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-autoreconf --force -I config -I m4 --install
+# assumes running from top level ddcutil directory
+autoreconf --force -I config -I m4 --install --verbose
+test -n "$NOCONFIGURE" || ./configure "$@"
+
