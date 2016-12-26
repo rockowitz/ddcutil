1d43e006422dd57fab2896c926994329c84befc5
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2016-12-25 22:12:46 -0500

    ddc_output.c: - show_feature_set_values(): add debug message

--- src/ddc/ddc_output.c
+++ src/ddc/ddc_output.c
@@ -549,6 +549,7 @@ show_feature_set_values(
             }
          }
       }
+      DBGMSF(debug,"ndx=%d, feature = 0x%02x Done", ndx, entry->code);
    }   // loop over features
 
    DBGMSF(debug, "Returning: %s", gsc_desc(master_status_code));
