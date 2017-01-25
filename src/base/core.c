8cf78aede583a5d971c14e4fab6dbf488bc45b79
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2017-01-25 08:04:47 -0500

    core.c/h: bump copyright year

--- src/base/core.c
+++ src/base/core.c
@@ -1,7 +1,7 @@
 /* core.c
  *
  * <copyright>
- * Copyright (C) 2014-2016 Sanford Rockowitz <rockowitz@minsoft.com>
+ * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
