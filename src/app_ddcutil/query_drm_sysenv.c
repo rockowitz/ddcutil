db4d4ebcb8902aa435e114c1ba9ea50e9c052f21
Sanford Rockowitz <rockowitz@minsoft.com>
2017-02-06 04:00:13 -0500

    query_drm_sysenv.c: - query driver and library version, bus device information, etc - code cleanup

--- src/app_ddcutil/query_drm_sysenv.c
+++ src/app_ddcutil/query_drm_sysenv.c
@@ -97,7 +97,17 @@ char * drm_bus_type_name(uint8_t bus) {
    return result;
 }
 
-void probe_one_device_using_libdrm(char * devname, int depth) {
+
+void report_drmVersion(drmVersion * vp, int depth) {
+   rpt_vstring(depth, "Version:     %d.%d.%d",
+                      vp->version_major, vp->version_minor, vp->version_patchlevel);
+   rpt_vstring(depth, "Driver:      %.*s", vp->name_len, vp->name);
+   rpt_vstring(depth, "Date:        %.*s", vp->date_len, vp->date);
+   rpt_vstring(depth, "Description: %.*s", vp->desc_len, vp->desc);
+}
+
+
+static void probe_open_device_using_libdrm(int fd, int depth) {
    int d1 = depth+1;
    int d2 = depth+2;
    int d3 = depth+3;
@@ -105,47 +115,70 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
    int rc;
 
    rpt_nl();
-   rpt_vstring(depth, "Probing device %s...", devname);
 
-   int supported = drmCheckModesettingSupported(devname);
-   DBGMSG("drmCheckModesettingSupported() returned %d", supported);
-
-   int dri_fd  = open(devname,O_RDWR | O_CLOEXEC);
-   if (dri_fd < 0) {
-      perror("Error opening %s\n");
-      return;
+   drmVersionPtr vp = drmGetVersion(fd);
+   if (vp) {
+      rpt_vstring(d1, "Driver version information:");
+      report_drmVersion(vp, d2);
+      drmFreeVersion(vp);
    }
-   // printf("dri_fd = %d\n", dri_fd);
+   else {
+      rpt_vstring(d1, "Error calling drmGetVersion().  errno=%s", linux_errno_desc(errno));
+   }
 
+   vp = drmGetLibVersion(fd);
+   if (vp) {
+      rpt_vstring(d1, "DRM library version information:");
+      report_drmVersion(vp, d2);
+      drmFreeVersion(vp);
+   }
+   else {
+      rpt_vstring(d1, "Error calling drmGetLibVersion().  errno=%s", linux_errno_desc(errno));
+   }
+
+   char * bufid = drmGetBusid(fd);
+   if (bufid) {
+      rpt_vstring(d1, "DRM Busid:  %s");
+      drmFreeBusid(bufid);
+   }
+   else {
+      rpt_vstring(d1, "Error calling drmGetBusid().  errno=%s", linux_errno_desc(errno));
+   }
+
+   rpt_nl();
    struct _drmDevice * ddev;
-   rc = drmGetDevice(dri_fd, &ddev);
+   rc = drmGetDevice(fd, &ddev);
    if (rc < 0) {
       rpt_vstring(depth, "Error calling drmGetDevice, errno=%s", linux_errno_desc(errno));
    }
    else {
-      rpt_vstring(d1, "bustype:  %d - %s", ddev->bustype, drm_bus_type_name(ddev->bustype));
+      rpt_vstring(d1, "Device information:");
+      rpt_vstring(d2, "bustype:             %d - %s",
+            ddev->bustype, drm_bus_type_name(ddev->bustype));
 #ifdef OLD
-      rpt_vstring(d1, "bus,dev,domain,func: %d, %d, %d, %d",
+      rpt_vstring(d2, "bus,dev,domain,func: %d, %d, %d, %d",
             ddev->businfo.pci->bus,
             ddev->businfo.pci->dev,
             ddev->businfo.pci->domain,
             ddev->businfo.pci->func);
 #endif
-      rpt_vstring(d1, "domain:bus:device.func: %04x:%02x:%02x.%d",
+      rpt_vstring(d2, "domain:bus:device.func: %04x:%02x:%02x.%d",
             ddev->businfo.pci->domain,
             ddev->businfo.pci->bus,
             ddev->businfo.pci->dev,
             ddev->businfo.pci->func);
-      rpt_vstring(d1, "vendor vid:pid:  0x%04x:0x%04x",
+      rpt_vstring(d2, "vendor    vid:pid:      0x%04x:0x%04x",
             ddev->deviceinfo.pci->vendor_id,
             ddev->deviceinfo.pci->device_id);
-      rpt_vstring(d1, "subvendor vid:pid: 0x%04x:0x%04x",
+      rpt_vstring(d2, "subvendor vid:pid:      0x%04x:0x%04x",
             ddev->deviceinfo.pci->subvendor_id,
             ddev->deviceinfo.pci->subdevice_id);
-      rpt_vstring(d1, "revision id: 0x%04x",
+      rpt_vstring(d2, "revision id:            0x%04x",
             ddev->deviceinfo.pci->revision_id);
       drmFreeDevice(&ddev);
    }
+
+
 
 #ifdef REF
    extern drmModePropertyPtr drmModeGetProperty(int fd, uint32_t propertyId);
@@ -154,13 +187,13 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
 
    rpt_nl();
    rpt_vstring(d1, "Retrieving DRM resources...");
-   drmModeResPtr res = drmModeGetResources(dri_fd);
+   drmModeResPtr res = drmModeGetResources(fd);
    if (!res) {
       int errsv = errno;
       rpt_vstring(d1, "Failure retrieving DRM resources, errno=%s", linux_errno_desc(errno));
       if (errsv == EINVAL)
          rpt_vstring(d1,"Driver apparently does not support DRM");
-      goto bye_close;
+      goto bye;
    }
    if (debug)
       report_drmModeRes(res, d2);
@@ -173,7 +206,7 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
    rpt_nl();
    rpt_vstring(d1, "Scanning defined properties...");
    for (int prop_id = 0; prop_id < 200; prop_id++) {
-      drmModePropertyPtr prop_ptr = drmModeGetProperty(dri_fd, prop_id);
+      drmModePropertyPtr prop_ptr = drmModeGetProperty(fd, prop_id);
       if (prop_ptr) {
          if (debug)
             report_drm_modeProperty(prop_ptr, d2);
@@ -197,8 +230,6 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
       }
    }
 
-   // printf("edid_prop_id=%u\n", edid_prop_id);
-
 
 #ifdef REF
    extern drmModePropertyBlobPtr drmModeGetPropertyBlob(int fd, uint32_t blob_id);
@@ -214,27 +245,16 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
    rpt_nl();
    rpt_vstring(d1, "Scanning connectors...");
    for (int i = 0; i < res->count_connectors; ++i) {
-       /* get information for each connector */
-      drmModeConnector * conn = drmModeGetConnector(dri_fd, res->connectors[i]);
+      drmModeConnector * conn = drmModeGetConnector(fd, res->connectors[i]);
       if (!conn) {
-         rpt_vstring(d1, "Cannot retrieve DRM connector id errno=%s",
+         rpt_vstring(d1, "Cannot retrieve DRM connector id %d errno=%s",
                          res->connectors[i], linux_errno_desc(errno));
          continue;
       }
       if (debug)
-         report_drmModeConnector(dri_fd, conn, d1) ;
+         report_drmModeConnector(fd, conn, d1) ;
 
-      // show:
-      //   connector_id
-      //   connector_type
-      //   connector_type_id     - what is this?
-
-      //   connection (i.e is connected?)
-      //   EDID property
-      //   subconnector properites
-      //   summarize remaining properties?
-
-      rpt_vstring(d1, "connector id: %d",       "connector_id:", conn->connector_id);
+      rpt_vstring(d1, "%-20s %u",       "connector_id:",      conn->connector_id);
       rpt_vstring(d2, "%-20s %d - %s",  "connector_type:",    conn->connector_type,  connector_type_name(conn->connector_type));
       rpt_vstring(d2, "%-20s %d",       "connector_type_id:", conn->connector_type_id);
       rpt_vstring(d2, "%-20s %d - %s",  "connection:", conn->connection, connector_status_name(conn->connection));
@@ -243,7 +263,7 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
          if (conn->props[ndx] == edid_prop_id) {
             rpt_vstring(d2, "EDID property");
             uint64_t blob_id = conn->prop_values[ndx];
-            drmModePropertyBlobPtr blob_ptr = drmModeGetPropertyBlob(dri_fd, blob_id);
+            drmModePropertyBlobPtr blob_ptr = drmModeGetPropertyBlob(fd, blob_id);
             if (!blob_ptr) {
                rpt_vstring(d3, "Blob not found");
             }
@@ -284,16 +304,15 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
                rpt_vstring(d2, "Subconnector not type enum!.  Value = %d", enum_value);
             }
 
-
          }
 #ifdef FUTURE
          else {
-            drmModePropertyPtr prop_ptr = drmModeGetProperty(dri_fd, conn->props[ndx]);
+            drmModePropertyPtr prop_ptr = drmModeGetProperty(fd, conn->props[ndx]);
             if (prop_ptr) {
                // now in connector report function
                // report_property_value(dri_fd, prop_ptr, conn->prop_values[ndx], 1);
                // to implement:
-               summarize_property_value(dri_fd, prop_ptr, conn->prop_values[ndx], d2);
+               summarize_property_value(fd, prop_ptr, conn->prop_values[ndx], d2);
                drmModeFreeProperty(prop_ptr);
             }
             else {
@@ -314,14 +333,31 @@ void probe_one_device_using_libdrm(char * devname, int depth) {
       drmModeFreeProperty(subconn_prop_ptr);
    }
 
-
-bye_close:
-   close(dri_fd);
-
+bye:
+   return;
 }
 
 
-/* Filter to find driN files for scandir() */
+static void probe_one_device_using_libdrm(char * devname, int depth) {
+   rpt_vstring(depth, "Probing device %s...", devname);
+
+   int supported = drmCheckModesettingSupported(devname);
+   DBGMSG("drmCheckModesettingSupported() returned %d", supported);
+
+   int fd  = open(devname,O_RDWR | O_CLOEXEC);
+   if (fd < 0) {
+      rpt_vstring(depth, "Error opening device %s, errno=%s",
+                         devname, linux_errno_desc(errno));
+      // perror("Error opening %s\n");
+   }
+   else {
+      probe_open_device_using_libdrm(fd, depth);
+      close(fd);
+   }
+}
+
+
+/* Filter to find driN files using scandir() in get_filenames_by_filter() */
 static int is_dri(const struct dirent *ent) {
    return !strncmp(ent->d_name, "card", strlen("card"));
 }
@@ -339,13 +375,23 @@ GPtrArray * get_dri_device_names_using_filesys() {
 }
 
 
-
+/* Main function for probing device information, particularly EDIDs,
+ * using libdrm.
+ *
+ * 2/2017:  Nvidia's proprietary drm driver does not appear to
+ * support the ioctls underlying the libdrm functions, and hence
+ * the functions, set errno=22 (EINVAL).
+ */
 void probe_using_libdrm() {
    rpt_title("Probing connected monitors using libdrm...",0);
+
+   int drm_available = drmAvailable();
+   rpt_vstring(0, "drmAvailable() returned %d", drm_available);
 
    GPtrArray * dev_names = get_dri_device_names_using_filesys();
    for (int ndx = 0; ndx < dev_names->len; ndx++) {
       char * dev_name = g_ptr_array_index(dev_names, ndx);
+      rpt_nl();
       probe_one_device_using_libdrm( dev_name, 0);
    }
    g_ptr_array_free(dev_names, true);
