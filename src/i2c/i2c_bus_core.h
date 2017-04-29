ee66c82394c5a739281e0e93fb5ed6c00672d15f
Sanford Rockowitz <rockowitz@minsoft.com>
2017-04-28 23:39:27 -0400

    i2c_bus_core.c: - clean out old iftested transitional code

--- src/i2c/i2c_bus_core.h
+++ src/i2c/i2c_bus_core.h
@@ -77,9 +77,7 @@ Bus_Info * i2c_get_bus_info_by_index(int busndx);
 void i2c_report_bus(int busno);
 int  i2c_report_buses(bool report_all, int depth);
 
-#ifdef PRE_DISPLAY_REC
-Display_Info_List i2c_get_displays_old();
-#endif
+
 GPtrArray* i2c_get_displays();
 
 Bus_Info * i2c_get_bus_info(int busno, Byte findopts);
@@ -90,9 +88,7 @@ Bus_Info * i2c_find_bus_info_by_mfg_model_sn(
               const char * model,
               const char * sn,
               Byte findopts);
-#ifdef PRE_DISPLAY_REC
-Bus_Info * i2c_find_bus_info_by_edid(const Byte * pEdidBytes, Byte findopts);
-#endif
+
 // void report_businfo(Bus_Info * bus_info);
 bool i2c_is_valid_bus(int busno, Call_Options callopts);
 
