059c2f31c87fee6088f98230220390a1fdffb83c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2017-11-11 18:51:42 -0500

    query_sysenv.c, query_sysenv_access.c/h: - move identify_i2c_devices(), check_i2c_devices() and supporting functions to query_sysenv_access.c

--- src/app_sysenv/query_sysenv_access.h
+++ src/app_sysenv/query_sysenv_access.h
@@ -28,6 +28,12 @@
 #ifndef SYSENV_ACCESS_H_
 #define QUERY_SYSENV_ACCESS_H_
 
+#include "util/data_structures.h"
 
+#include "app_sysenv/query_sysenv_base.h"
+
+Byte_Value_Array identify_i2c_devices();
+
+void check_i2c_devices(Env_Accumulator * accum);
 
 #endif /* QUERY_SYSENV_ACCESS_H_ */
