076b9456abd4631a875c81e041873564512fc08b
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2017-11-12 23:36:29 -0500

    displays.c: - hiddev_name_to_number():   - make coverify happy: test if str_to_int() succeeded, test with an assert()

--- src/base/displays.c
+++ src/base/displays.c
@@ -936,7 +936,8 @@ int hiddev_name_to_number(char * hiddev_name) {
       if (strlen(p) > 0) {
          // hiddev_number unchanged if error
          // n str_to_int() allows leading whitespace, not worth checking
-         str_to_int(p, &hiddev_number);
+         bool ok = str_to_int(p, &hiddev_number);
+         assert(ok);    // make coverity happy
       }
    }
    // DBGMSG("hiddev_name = |%s|, returning: %d", hiddev_name, hiddev_number);
