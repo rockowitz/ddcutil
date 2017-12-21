f973d9eb72080ade9c46b49692c4e6c5de53a39a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2017-12-21 04:29:58 -0500

    test_swig.py: - convert print to print() for Python3 compatibility

--- src/swig/test_swig.py
+++ src/swig/test_swig.py
@@ -25,54 +25,54 @@ def get_build_information():
 
 
 def test1():
-   print "Default output level: %d" % ddcs_get_output_level()
+   print( "Default output level: %d" % ddcs_get_output_level() )
    ddcs_set_output_level(DDCS_OL_VERBOSE);
-   print "Output level reset to %s" %ddcs_output_level_name(ddcs_get_output_level())
+   print( "Output level reset to %s" %ddcs_output_level_name(ddcs_get_output_level()) )
    
-   print "Initial report ddc errors setting: %s" % ddcs_is_report_ddc_errors_enabled()
+   print( "Initial report ddc errors setting: %s" % ddcs_is_report_ddc_errors_enabled() )
    ddcs_enable_report_ddc_errors(True)
  
    displayct = ddcs_report_active_displays(2)
-   print "Found %d active displays" % displayct 
+   print( "Found %d active displays" % displayct )
 
    did = ddcs_create_dispno_display_identifier(2)
-   print ddcs_repr_display_identifier(did)
+   print( ddcs_repr_display_identifier(did))
    dref = ddcs_get_display_ref(did)
-   print ddcs_repr_display_ref(dref)
+   print( ddcs_repr_display_ref(dref) )
    ddcs_report_display_ref(dref, 2)
    dh = ddcs_open_display(dref)
-   print ddcs_repr_display_handle(dh)
+   print( ddcs_repr_display_handle(dh) )
 
-   print "Name of feature 0x10: %s" % ddcs_get_feature_name(0x10)
+   print( "Name of feature 0x10: %s" % ddcs_get_feature_name(0x10) )
    # feature_flags = ddcs_get_feature_info_by_display(dh, 0x10)
    # print "Feature 0x10 flags: %s, %d, 0x%08x" % (feature_flags, feature_flags, feature_flags)
 
    vcp_val = ddcs_get_nontable_vcp_value(dh, 0x10)
-   print vcp_val
-   print "cur value: ", vcp_val.cur_value
-   print "max value: ", vcp_val.max_value
+   print( vcp_val )
+   print( "cur value: ", vcp_val.cur_value )
+   print( "max value: ", vcp_val.max_value )
    ddcs_set_nontable_vcp_value(dh, 0x10, 22)
-   print "value reset to", ddcs_get_nontable_vcp_value(dh, 0x10).cur_value
+   print( "value reset to", ddcs_get_nontable_vcp_value(dh, 0x10).cur_value )
 
    ddcs_set_nontable_vcp_value(dh, 0x10, vcp_val.cur_value)
-   print "value reset to", ddcs_get_nontable_vcp_value(dh, 0x10).cur_value
+   print( "value reset to", ddcs_get_nontable_vcp_value(dh, 0x10).cur_value )
 
    caps  = ddcs_get_capabilities_string(dh)
-   print "Capabilities: %s" % caps
+   print( "Capabilities: %s" % caps )
 
    profile_vals = ddcs_get_profile_related_values(dh)
-   print "Profile related values: %s" % profile_vals 
+   print( "Profile related values: %s" % profile_vals  )
 
 
-   # print vcp_val.cur_value()     # int object is not callable
+   # print( vcp_val.cur_value()     # int object is not callable )
    # ddcs_close_display(dh)
 
 if __name__ == "__main__":
-   print "(test_swig)"
-   print dir(ddc_swig)
-   print 
-   get_build_information()
-   print
+   print( "(test_swig)" )
+   print( dir(ddc_swig) )
+   print()
+   get_build_information() 
+   print()
    # test1()
 
 
