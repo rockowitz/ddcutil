8bd9e3282db3ca6859990a17b9bf7e5b70c3a402
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-03-27 20:43:49 -0400

    demo_capabilities.c: - add simple_report_parsed_capabilities, a simplified version of ddca_report_parsed_capabilities()

--- src/sample_clients/demo_capabilities.c
+++ src/sample_clients/demo_capabilities.c
@@ -75,6 +75,57 @@ DDCA_Display_Handle * open_first_display_by_dispno() {
 }
 
 
+/* This is a simplified version of API function ddca_report_parsed_capabilities(),
+ * illustrating use of DDCA_Capabilities.
+ */
+void
+simple_report_parsed_capabilities(DDCA_Capabilities * pcaps)
+{
+   assert(pcaps && memcmp(pcaps->marker, DDCA_CAPABILITIES_MARKER, 4) == 0);
+   printf("Unparsed capabilities string: %s\n", pcaps->unparsed_string);
+   printf("VCP version:     %d.%d\n", pcaps->version_spec.major, pcaps->version_spec.minor);
+   printf("Command codes:\n");
+   for (int cmd_ndx = 0; cmd_ndx < pcaps->cmd_ct; cmd_ndx++) {
+      uint8_t cur_code = pcaps->cmd_codes[cmd_ndx];
+      printf("   0x%02x\n", cur_code);
+   }
+   printf("VCP Feature codes:\n");
+   for (int code_ndx = 0; code_ndx < pcaps->vcp_code_ct; code_ndx++) {
+      DDCA_Cap_Vcp * cur_vcp = &pcaps->vcp_codes[code_ndx];
+      assert( memcmp(cur_vcp->marker, DDCA_CAP_VCP_MARKER, 4) == 0);
+
+      // char * feature_name = get_feature_name_by_id_and_vcp_version(
+      //                          cur_vcp->feature_code,
+      //                          pcaps->version_spec);
+      char * feature_name = ddca_get_feature_name(cur_vcp->feature_code);
+
+      printf("   Feature:  0x%02x (%s)\n", cur_vcp->feature_code, feature_name);
+
+      DDCA_Feature_Value_Entry * feature_value_table;
+      DDCA_Status ddcrc = ddca_get_simple_sl_value_table_by_vspec(
+            cur_vcp->feature_code,
+            pcaps->version_spec,
+            &feature_value_table);
+
+      if (cur_vcp->value_ct > 0) {
+         // printf("      Unparsed values:     %s\n", hexstring_t(cur_vcp->values, cur_vcp->value_ct) );
+         printf("      Values:\n");
+         for (int ndx = 0; ndx < cur_vcp->value_ct; ndx++) {
+            char * value_desc = "No lookup table";
+            if (ddcrc == 0) {
+               value_desc = "Unrecognized feature value";
+               ddca_get_simple_nc_feature_value_name_by_table(
+                          feature_value_table,
+                          cur_vcp->values[ndx],
+                          &value_desc);
+            }
+            printf("         0x%02x: %s\n", cur_vcp->values[ndx], value_desc);
+         }
+      }
+   }
+}
+
+
 /* Retrieves and reports the capabilities string for the first detected monitor.
  */
 void demo_get_capabilities() {
@@ -84,19 +135,19 @@ void demo_get_capabilities() {
 
    // printf("\n(%s) ===> Starting.  dh = %s\n", __func__, ddca_dh_repr(dh));
    char * capabilities = NULL;
-   printf("(%s) Calling ddca_get_capabilities_string...\n", __func__);
+   printf("Calling ddca_get_capabilities_string...\n");
    DDCA_Status rc =  ddca_get_capabilities_string(dh, &capabilities);
    if (rc != 0)
       FUNCTION_ERRMSG("ddca_get_capabilities_string", rc);
    else
-      printf("(%s) Capabilities: %s\n", __func__, capabilities);
-   printf("(%s) Second call should be fast since value cached...\n", __func__);
+      printf("Capabilities: %s\n", capabilities);
+   printf("Second call to ddca_get_capabilities() should be fast since value cached...\n");
    rc =  ddca_get_capabilities_string(dh, &capabilities);
    if (rc != 0)
       FUNCTION_ERRMSG("ddca_get_capabilities_string", rc);
    else {
-      printf("(%s) Capabilities: %s\n", __func__, capabilities);
-      printf("(%s) Try parsing the string...\n", __func__);
+      printf("Capabilities: %s\n", capabilities);
+      printf("Parse the string...\n");
         DDCA_Capabilities * pcaps = NULL;
       rc = ddca_parse_capabilities_string(
              capabilities,
@@ -104,12 +155,16 @@ void demo_get_capabilities() {
       if (rc != 0)
          FUNCTION_ERRMSG("ddca_parse_capabilities_string", rc);
       else {
-         printf("(%s) Parsing succeeded.  Report the result...\n", __func__);
-         DDCA_Output_Level saved_ol = ddca_set_output_level(DDCA_OL_NORMAL);
+         printf("Parsing succeeded.\n");
+         printf("\nReport the result using local function simple_report_parsed_capabilities()...\n");
+         simple_report_parsed_capabilities(pcaps);
+
+         printf("\nReport the result using API function ddca_report_parsed_capabilities()...\n");
+         DDCA_Output_Level saved_ol = ddca_set_output_level(DDCA_OL_VERBOSE);
          ddca_report_parsed_capabilities(pcaps, 0);
          ddca_free_parsed_capabilities(pcaps);
 
-         printf("(%s) Use \"ddcutil capabilities\" code to display capabilities...\n", __func__);
+         printf("\nUse \"ddcutil capabilities\" code to display capabilities...\n");
          ddca_set_output_level(DDCA_OL_VERBOSE);  // show both unparsed and parsed capabilities
          ddca_parse_and_report_capabilities(capabilities, 1);
          ddca_set_output_level(saved_ol);
@@ -117,8 +172,10 @@ void demo_get_capabilities() {
    }
 
 bye:
-   printf("(%s) Done.\n", __func__);
+   // printf("(%s) Done.\n", __func__);
+   return;
 }
+
 
 
 int main(int argc, char** argv) {
