1abdd613cd7fb0aec88e239e1555d6efeb2bea77
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-04-01 12:45:07 -0400

    dynamic_featues.c: - continued development

--- src/base/dynamic_features.c
+++ src/base/dynamic_features.c
@@ -21,17 +21,137 @@
  * </endcopyright>
  */
 
+#define _GNU_SOURCE      // for asprintf()
+
 #include <assert.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "ddcutil_status_codes.h"
 #include "ddcutil_types.h"
 
+#include "util/report_util.h"
 #include "util/string_util.h"
 
 #include "base/core.h"
 
 #include "dynamic_features.h"
+
+#ifdef REF
+typedef struct {
+   char *          mfg_id;     // [EDID_MFG_ID_FIELD_SIZE];
+   char *          model_name;  // [EDID_MODEL_NAME_FIELD_SIZE];
+   uint16_t        product_code;
+   GHashTable *    features;     // array of DDCA_Feature_Metadata
+} Dynamic_Features_Rec;
+
+
+
+#define DDCA_FEATURE_METADATA_MARKER  "FMET"
+/** Describes a VCP feature code, tailored for a specific VCP version */
+typedef
+struct {
+   char                                  marker[4];      /**< always "FMET" */
+   DDCA_Vcp_Feature_Code                 feature_code;   /**< VCP feature code */
+   DDCA_MCCS_Version_Spec                vspec;          /**< MCCS version    */
+   DDCA_Feature_Flags                    feature_flags;  /**< feature type description */
+   DDCA_Feature_Value_Table              sl_values;      /**< valid when DDCA_SIMPLE_NC set */
+   char *                                feature_name;   /**< feature name */
+   char *                                feature_desc;   /**< feature description */
+   // possibly add pointers to formatting functions
+} DDCA_Feature_Metadata;
+
+
+#endif
+
+// copied from sample code, more to more generic location
+
+/* Creates a string representation of DDCA_Feature_Flags bitfield.
+ *
+ * Arguments:
+ *    flags      feature characteristics
+ *
+ * Returns:      string representation, caller must free
+ */
+char * interpret_feature_flags(DDCA_Version_Feature_Flags flags) {
+   char * buffer = NULL;
+   int rc = asprintf(&buffer, "%s%s%s%s%s%s%s%s%s%s%s%s%s",
+       flags & DDCA_RO             ? "Read-Only, "                   : "",
+       flags & DDCA_WO             ? "Write-Only, "                  : "",
+       flags & DDCA_RW             ? "Read-Write, "                  : "",
+       flags & DDCA_STD_CONT       ? "Continuous (standard), "       : "",
+       flags & DDCA_COMPLEX_CONT   ? "Continuous (complex), "        : "",
+       flags & DDCA_SIMPLE_NC      ? "Non-Continuous (simple), "     : "",
+       flags & DDCA_COMPLEX_NC     ? "Non-Continuous (complex), "    : "",
+       flags & DDCA_NC_CONT        ? "Non-Continuous with continuous subrange, " :"",
+       flags & DDCA_WO_NC          ? "Non-Continuous (write-only), " : "",
+       flags & DDCA_NORMAL_TABLE   ? "Table (readable), "            : "",
+       flags & DDCA_WO_TABLE       ? "Table (write-only), "          : "",
+       flags & DDCA_DEPRECATED     ? "Deprecated, "                  : "",
+       flags & DDCA_SYNTHETIC      ? "Synthesized, "                 : ""
+       );
+   assert(rc >= 0);   // real life code would check for malloc() failure in asprintf()
+   // remove final comma and blank
+   if (strlen(buffer) > 0)
+      buffer[strlen(buffer)-2] = '\0';
+
+   return buffer;
+}
+
+
+
+// eventually move to a more shared location
+
+void
+dbgrpt_feature_metadata(
+      DDCA_Feature_Metadata * md,
+      int                     depth)
+{
+   int d0 = depth;
+   int d1 = depth+1;
+   int d2 = depth+2;
+   rpt_structure_loc("DDCA_Feature_Metadata", md, depth);
+   rpt_vstring(d0, "Feature code:      0x%02x",  md->feature_code);
+   rpt_vstring(d1, "MCCS version:      %d.%d",  md->vspec.major, md->vspec.minor);
+   rpt_vstring(d1, "Feature name:      %s",     md->feature_name);
+   rpt_vstring(d1, "Description:       %s",     md->feature_desc);
+   char * s = interpret_feature_flags(md->feature_flags);
+   rpt_vstring(d1, "Feature flags:     0x%04x", md->feature_flags);
+   rpt_vstring(d1, "Interpreted flags: %s", s);
+   free(s);
+   if (md->sl_values) {
+      rpt_label(d1, "SL values:");
+      DDCA_Feature_Value_Entry * curval = md->sl_values;
+      while(curval->value_code || curval->value_name) {
+         rpt_vstring(d2, "0x%02x  - %s", curval->value_code, curval->value_name);
+         curval++;
+      }
+   }
+}
+
+
+
+void
+dbgrpt_dynamic_features_rec(
+      Dynamic_Features_Rec * dfr,
+      int                    depth)
+{
+   int d1 = depth+1;
+   rpt_structure_loc("Dynamic_Features_Rec", dfr, depth);
+   rpt_vstring(d1, "mfg_id:         %s", dfr->mfg_id);
+   rpt_vstring(d1, "model_name:     %s", dfr->model_name);
+   rpt_vstring(d1, "product_code:   %u", dfr->product_code);
+   if (dfr->features) {
+      rpt_vstring(d1, "features count: %d", g_hash_table_size(dfr->features));
+      for (int ndx = 1; ndx < 256; ndx++) {
+         DDCA_Feature_Metadata * cur_feature =
+               g_hash_table_lookup(dfr->features, GINT_TO_POINTER(ndx));
+         if (cur_feature)
+            dbgrpt_feature_metadata(cur_feature, d1);
+
+      }
+   }
+}
 
 
 /* static */ void
@@ -73,6 +193,8 @@ attr_keyword(
       DDCA_Feature_Metadata * cur_feature_metadata,
       char *                  keyword)
 {
+   bool debug = true;
+   DBGMSF(debug, "keyword=|%s|", keyword);
    bool ok = true;
    DDCA_Feature_Flags * pflags = &cur_feature_metadata->feature_flags;
    if (streq(keyword, "RW"))
@@ -94,6 +216,7 @@ attr_keyword(
    else
       ok = false;
 
+   DBGMSF("Returning %s", SBOOL(ok));
    return ok;
 }
 
@@ -117,7 +240,140 @@ free_feature_metadata(
    assert(memcmp(info->marker, DDCA_FEATURE_METADATA_MARKER, 4) == 0);
    // DDCA_Feature_Metadata needs a marker field
 
-   // TO IMPLEMENT
+   // TODO
+}
+
+
+void
+free_dynamic_features_record(
+      Dynamic_Features_Rec frec)
+{
+    // TODO
+}
+
+
+
+void finalize_feature(
+      Dynamic_Features_Rec *  frec,
+      DDCA_Feature_Metadata * cur_feature_metadata,
+      GArray *                cur_feature_values,
+      const char *            filename,
+      GPtrArray *             errors)
+{
+   DDCA_Feature_Flags * pflags = &cur_feature_metadata->feature_flags;
+
+   if (cur_feature_values) {
+       // convert and set cur_feature_metadata->sl_values
+      // free cur_feature_values
+      DDCA_Feature_Value_Entry last_entry;
+      last_entry.value_code = 0x00;
+      last_entry.value_name = NULL;
+      g_array_append_val(cur_feature_values, last_entry);
+      cur_feature_metadata->sl_values = (DDCA_Feature_Value_Entry*) cur_feature_values->data;
+      // g_array_free(cur_feature_values, false);
+      // cur_feature_values = NULL;
+   }
+
+
+   if ( *pflags & (DDCA_RW | DDCA_RO | DDCA_WO) )
+      *pflags |= DDCA_RW;
+
+   if (cur_feature_metadata->sl_values) {
+      if (cur_feature_metadata->feature_flags & DDCA_COMPLEX_NC) {
+         if ( *pflags & DDCA_WO)
+            switch_bits(pflags, DDCA_COMPLEX_NC, DDCA_WO_NC);
+         else
+            switch_bits(pflags, DDCA_COMPLEX_NC, DDCA_SIMPLE_NC);
+      }
+
+      else if ( *pflags & (DDCA_COMPLEX_CONT | DDCA_TABLE))
+          ADD_ERROR(-1,  "Feature values specified for Continuous or Table feature");
+   }
+
+   if (*pflags & DDCA_NORMAL_TABLE & (*pflags & DDCA_WO))
+      switch_bits(pflags, DDCA_NORMAL_TABLE, DDCA_WO_TABLE);
+
+
+   g_hash_table_replace(
+         frec->features,
+         GINT_TO_POINTER(cur_feature_metadata->feature_code),
+         cur_feature_metadata);
+
+}
+
+typedef struct {
+   char * word;
+   char * rest;
+} Tokenized;
+
+Tokenized first_word(char * s) {
+   assert(s);
+   DBGMSG("Starting. s=|%s|", s);
+   Tokenized result = {NULL,NULL};
+   while (*s == ' ')
+         s++;
+   if (*s) {
+      char * end = s;
+      while (*++end && *end != ' ');
+      int wordlen = end-s;
+      result.word = malloc( wordlen+1);
+      memcpy(result.word, s, wordlen);
+      result.word[wordlen] = '\0';
+      result.rest = end;
+   }
+   DBGMSG("Returning: result.word=|%s|, result.rest=|%s|", result.word, result.rest);
+   return result;
+}
+
+
+char * canonicalize_feature_value(char * string_value) {
+   assert(string_value);
+
+   int bufsz = strlen(string_value) + 4;
+   char * buf = calloc(1, bufsz);
+   if (*string_value == 'X' || *string_value == 'x' ) {
+      snprintf(buf, bufsz, "0x%s", string_value+1);
+   }
+   else if (*(string_value + strlen(string_value)-1) == 'H' ||
+            *(string_value + strlen(string_value)-1) == 'h' )
+   {
+      int newlen = strlen(string_value)-1;
+      snprintf(buf, bufsz, "0x%.*s", newlen, string_value);
+   }
+   else
+      strcpy(buf, string_value);
+   // DBGMSG("string_value=|%s|, returning |%s|", string_value, buf);
+   return buf;
+}
+
+
+
+
+// like str_to_int(), but allows hex values
+bool str_to_int2(const char * sval, int * p_ival, bool allow_hex) {
+   bool debug = false;
+   if (debug)
+      printf("(%s) sval->|%s|\n", __func__, sval);
+
+   char * endptr;
+   bool ok = false;
+   if ( *sval != '\0') {
+      long result = strtol(sval, &endptr, (allow_hex) ? 0 : 10); // allow hex
+      // printf("(%s) sval=%p, endptr=%p, *endptr=|%c| (0x%02x), result=%ld\n",
+      //        __func__, sval, endptr, *endptr, *endptr, result);
+      if (*endptr == '\0') {
+         *p_ival = result;
+         ok = true;
+      }
+   }
+
+   if (debug) {
+      if (ok)
+        printf("(%s) sval=%s, Returning: %s, *ival = %d\n", __func__, sval, bool_repr(ok), *p_ival);
+      else
+        printf("(%s) sval=%s, Returning: %s\n", __func__, sval, bool_repr(ok));
+   }
+   return ok;
 }
 
 
@@ -142,7 +398,7 @@ create_monitor_dynamic_features(
    bool product_code_seen = false;
    frec->features = g_hash_table_new_full(
                               g_direct_hash,
-                              g_int_equal,
+                              g_direct_equal,   // g_int_equal,
                               NULL,                     // key_destroy_func
                               free_feature_metadata);   // value_destroy_func
 
@@ -158,11 +414,17 @@ create_monitor_dynamic_features(
       char *  rest;
 
       line = g_ptr_array_index(lines,linectr);
+      // char * line2 = strdup(line);
       linectr++;
 
       *s0 = '\0'; *s1 = '\0'; *s2 = '\0';
       head = line;
       while (*head == ' ') head++;
+
+
+      /* Tokenized t = */ first_word(head);
+
+
       ct = sscanf(head, "%31s %256s %15s", s0, s1, s2);
       if (ct > 0 && *s0 != '*' && *s0 != '#') {
          if (ct == 1) {
@@ -176,12 +438,13 @@ create_monitor_dynamic_features(
             while (*last == ' ' || *last == '\n') {
                *last-- = '\0';
             }
-            // DBGMSG("rest=|%s|", rest );
+            DBGMSG("rest=|%s|", rest );
 
             if (streq(s0, "PRODUCT_CODE")) {
                product_code_seen = true;
                int ival;
                bool ok = str_to_int(s1, &ival);
+               DBGMSG("ival: %d", ival);
                if (!ok) {
                    ADD_ERROR(linectr, "Invalid product_code \"%s\"", s1);
                }
@@ -198,7 +461,7 @@ create_monitor_dynamic_features(
             }
             else if (streq(s0, "MODEL")) {
                model_name_seen = true;
-               if ( !streq(s1, model_name) ) {
+               if ( !streq(rest, model_name) ) {
                   ADD_ERROR(linectr, "Unexpected model name \"%s\"", s1);
                }
 
@@ -223,55 +486,18 @@ create_monitor_dynamic_features(
             }
 
             else if (streq(s0, "FEATURE_CODE")) {
+               // n. cur_feature_metadata saved in frec
                if (cur_feature_metadata) {
-                  DDCA_Feature_Flags * pflags = &cur_feature_metadata->feature_flags;
-
+                  finalize_feature(
+                        frec,
+                        cur_feature_metadata,
+                        cur_feature_values,
+                        filename,
+                        errors);
                   if (cur_feature_values) {
-                      // convert and set cur_feature_metadata->sl_values
-                     // free cur_feature_values
-                     DDCA_Feature_Value_Entry last_entry;
-                     last_entry.value_code = 0x00;
-                     last_entry.value_name = NULL;
-                     g_array_append_val(cur_feature_values, last_entry);
-                     cur_feature_metadata->sl_values = (DDCA_Feature_Value_Entry*) cur_feature_values->data;
                      g_array_free(cur_feature_values, false);
                      cur_feature_values = NULL;
                   }
-
-
-                  if ( *pflags & (DDCA_RW | DDCA_RO | DDCA_WO) )
-                     *pflags |= DDCA_RW;
-
-                  if (cur_feature_metadata->sl_values) {
-                     if (cur_feature_metadata->feature_flags & DDCA_COMPLEX_NC) {
-                        if ( *pflags & DDCA_WO)
-                           switch_bits(pflags, DDCA_COMPLEX_NC, DDCA_WO_NC);
-                        else
-                           switch_bits(pflags, DDCA_COMPLEX_NC, DDCA_SIMPLE_NC);
-                     }
-
-                     else if ( *pflags & (DDCA_COMPLEX_CONT | DDCA_TABLE))
-                         ADD_ERROR(-1,  "Feature values specified for Continuous or Table feature");
-                  }
-
-                  if (*pflags & DDCA_NORMAL_TABLE & (*pflags & DDCA_WO))
-                     switch_bits(pflags, DDCA_NORMAL_TABLE, DDCA_WO_TABLE);
-
-                  if (!mfg_id_seen)
-                     ADD_ERROR(-1, "Missing MFG_ID");
-
-                  if (!model_name_seen)
-                     ADD_ERROR(-1, "Missing MODEL_NAME");
-
-                  if (!product_code_seen)
-                     ADD_ERROR(-1, "Missing PRODUCT_CODE");
-
-
-                  g_hash_table_replace(
-                        frec->features,
-                        GINT_TO_POINTER(cur_feature_metadata->feature_code),
-                        cur_feature_metadata);
-
                }
 
                cur_feature_metadata = calloc(1, sizeof(DDCA_Feature_Metadata));
@@ -299,13 +525,15 @@ create_monitor_dynamic_features(
                   ADD_ERROR(linectr, "Invalid feature value data \"%s\"", line);
                }
                else {   // found value code and name
-                  Byte feature_value;
-                  bool ok = hhs_to_byte_in_buf(s1, &feature_value);
+                  int feature_value;
+                   // Byte feature_value;
+                  // bool ok = hhs_to_byte_in_buf(s1, &feature_value);
+                  char * canonical = canonicalize_feature_value(s1);
+                  bool ok = str_to_int2(canonical, &feature_value, true);
+                  free(canonical);
                   if (!ok) {
                      ADD_ERROR(linectr, "Invalid feature value \"%s\"", s1);
                   }
-
-
                   else {     // valid feature value
                      if (!cur_feature_values)
                         cur_feature_values = g_array_new(false, false, sizeof(DDCA_Feature_Value_Entry));
@@ -324,13 +552,47 @@ create_monitor_dynamic_features(
       }       // non-comment line
    }          // one line of file
 
+   if (cur_feature_metadata) {
+      finalize_feature(
+            frec,
+            cur_feature_metadata,
+            cur_feature_values,
+            filename,
+            errors);
+      if (cur_feature_values) {
+         g_array_free(cur_feature_values, false);
+         cur_feature_values = NULL;
+      }
+   }
+
+   if (g_hash_table_size(frec->features) == 0)
+      ADD_ERROR(-1, "No feature codes defined");
+
+   if (!mfg_id_seen)
+      ADD_ERROR(-1, "Missing MFG_ID");
+
+   if (!model_name_seen)
+      ADD_ERROR(-1, "Missing MODEL_NAME");
+
+   if (!product_code_seen)
+      ADD_ERROR(-1, "Missing PRODUCT_CODE");
+
    if (errors->len > 0) {
+      DBGMSG("errors->len=%d", errors->len);
+      DBGMSG("errors->pdata: %p", errors->pdata);
+      DBGMSG("&errors->pdata: %p", &errors->pdata);
+      DBGMSG("*errors->pdata: %p", *errors->pdata);
+      Error_Info * first = g_ptr_array_index(errors, 0);
+      DBGMSG("first: %p", first);
+      char * detail = gaux_asprintf("Error(s) processing monitor definition file: %s", filename);
       master_err = errinfo_new_with_causes2(
                             DDCRC_BAD_DATA,
-                            "Error parsing file",
-                            (Error_Info**) &errors->pdata,
+                            detail,
+                            (Error_Info**) errors->pdata,
                             errors->len,
                             __func__);
+      free(detail);
+      DBGMSG("After errinfo_new_with_causes2()");
       g_ptr_array_free(errors, false);
       // free constructed record
       *dynamic_featues_loc = NULL;
@@ -338,6 +600,7 @@ create_monitor_dynamic_features(
    else {
       g_ptr_array_free(errors, false);
       *dynamic_featues_loc = frec;
+      dbgrpt_dynamic_features_rec(frec, 0);
    }
 
    return master_err;
