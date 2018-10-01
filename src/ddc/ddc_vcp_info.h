c4afa5388498fa4452c901c98d88744bf9a1c39f
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-09-30 22:49:24 -0400

    New source files ddc/ddc_vcp_info.c/h Modify ddc/Makefile.am
    New source file that will hold functions to access VCP feature
    information at the DDC level, in order to incorporate user-defined
    per-monitor feature metadata.

---
+++ src/ddc/ddc_vcp_info.h
@@ -0,0 +1,32 @@
+/* ddc_vcp_info.h
+ *
+ * Access VCP feature code descriptions at the DDC level in order to
+ * incorporate user-defined per-monitor feature information.
+ *
+ * <copyright>
+ * Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * </endcopyright>
+ */
+
+#ifndef DDC_VCP_INFO_H_
+#define DDC_VCP_INFO_H_
+
+
+
+#endif /* DDC_VCP_INFO_H_ */
