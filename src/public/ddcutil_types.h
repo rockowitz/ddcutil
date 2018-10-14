5e4b2133f22b5278124a4bdaf8a9d31e03d1d68b
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-10-14 08:48:04 -0400

    ddcutil_c_api.h, ddcutil_macros.h.in, ddcutil_status_codes.h, ddcutil_types.h - Convert file header to doxydoc form - Convert copyright notice to SPDX-License-Identifier form

--- src/public/ddcutil_types.h
+++ src/public/ddcutil_types.h
@@ -1,26 +1,3 @@
-/* ddcutil_types.h
- *
- * <copyright>
- * Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
 /** @file ddcutil_types.h
  *  @brief File ddcutil_types.h contains type declarations for the C API.
  *
@@ -29,6 +6,9 @@
  *  **ddcutil** implementation, whereas the function declarations are only referenced
  *  within the code that implements the API.
  */
+
+// Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DDCUTIL_TYPES_H_
 #define DDCUTIL_TYPES_H_
