501c4639f889a3f3977550b8e93a0c2d6a590e62
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-10-24 12:52:27 -0400

    dyn_feature_codes.c:
    dyn_format_detail_sl_lookup(), dyn_get_feature_metadata_by_dref():
    - rework code, STILL NOT RIGHT
    - turn on debug

--- src/dynvcp/dyn_feature_codes.c
+++ src/dynvcp/dyn_feature_codes.c
@@ -62,7 +62,7 @@ bool dyn_format_feature_detail_sl_lookup(
         char *                     buffer,
         int                        bufsz)
 {
-   bool debug = false;
+   bool debug = true;
    DBGMSF(debug, "Starting.");
 
    if (value_table) {
@@ -87,7 +87,7 @@ dyn_get_feature_metadata_by_dref(
       Display_Ref *         dref,
       bool                  with_default)
 {
-   bool debug = false;
+   bool debug = true;
    DBGMSF(debug, "Starting. feature_code=0x%02x, dref=%s, with_default=%s",
                  feature_code, dref_repr_t(dref), sbool(with_default));
    DBGMSF(debug, "dref->dfr=%p", dref->dfr);
@@ -129,16 +129,84 @@ dyn_get_feature_metadata_by_dref(
            free_version_feature_info(info);  // safe?
            result = calloc(1, sizeof(Internal_Feature_Metadata));
            result->external_metadata = meta;
+#ifdef REF
+           // Further refine the C/NC/TABLE categorization of the MCCS spec
+           // Exactly 1 of the following 8 bits is set
+           #define DDCA_STD_CONT     0x0080       /**< Normal continuous feature */
+           #define DDCA_COMPLEX_CONT 0x0040       /**< Continuous feature with special interpretation */
+           #define DDCA_SIMPLE_NC    0x0020       /**< Non-continuous feature, having a defined list of values in byte SL */
+           #define DDCA_COMPLEX_NC   0x0010       /**< Non-continuous feature, having a complex interpretation using one or more of SL, SH, ML, MH */
+           #define DDCA_NC_CONT      0x0800       /**< NC feature combining reserved values with continuous range */
+           // For WO NC features.  There's no interpretation function or lookup table
+           // Used to mark that the feature is defined for a version
+           #define DDCA_WO_NC        0x0008       /**< Used internally for write-only non-continuous features */
+           #define DDCA_NORMAL_TABLE 0x0004       /**< Normal RW table type feature */
+           #define DDCA_WO_TABLE     0x0002       /**< Write only table feature */
+#endif
+           if (meta->feature_flags & DDCA_STD_CONT) {
+              result->nontable_formatter = format_feature_detail_standard_continuous;
+           }
+           else if (meta->feature_flags & DDCA_COMPLEX_CONT) {
+              // TODO: handle case of special function defined
+              result->nontable_formatter = format_feature_detail_debug_bytes;
+           }
+           else if (meta->feature_flags & DDCA_SIMPLE_NC) {
+              if (meta->sl_values) {
+                 DBGMSG("format_feature_detail_sl_lookup");
+                 result->nontable_formatter = format_feature_detail_sl_lookup;
+              }
+              else {
+                 DBGMSG("format_feature_detail_sl_byte");
+                 result->nontable_formatter = format_feature_detail_sl_byte;
+              }
+           }
+           else if (meta->feature_flags & DDCA_COMPLEX_NC) {
+              if (pentry->nontable_formatter) {
+                 DBGMSG("nontable_formatter");
+                 result->nontable_formatter = pentry->nontable_formatter;
+              }
+              else {
+                 result->nontable_formatter = format_feature_detail_debug_bytes;
+              }
+           }
+           else if (meta->feature_flags & DDCA_NC_CONT) {
+              // TODO: handle case of special function defined
+              result->nontable_formatter = format_feature_detail_debug_bytes;
+           }
+           else if (meta->feature_flags & DDCA_NC_CONT) {
+              // program logic error?
+              result->nontable_formatter = NULL;
+           }
+           else if (meta->feature_flags & DDCA_NORMAL_TABLE) {
+              result->table_formatter = pentry->table_formatter;
+              if (!result->table_formatter)
+                 result->table_formatter = default_table_feature_detail_function;
+           }
+           else if (meta->feature_flags & DDCA_WO_TABLE) {
+              // program logic error?
+              result->table_formatter = NULL;
+           }
+           else {
+              PROGRAM_LOGIC_ERROR("No feature type set in meta->feature_flags");
+           }
+
+
+#ifdef OLD
            if (meta->feature_flags & DDCA_NON_TABLE) {
               if (pentry->nontable_formatter) {
+                 DBGMSG("nontable_formatter");
                  result->nontable_formatter = pentry->nontable_formatter;
               }
               else {
                  if (meta->feature_flags & DDCA_SIMPLE_NC) {
-                    if (meta->sl_values)
+                    if (meta->sl_values) {
+                       DBGMSG("format_feature_detail_sl_lookup");
                        result->nontable_formatter = format_feature_detail_sl_lookup;
-                    else
+                    }
+                    else {
+                       DBGMSG("format_feature_detail_sl_byte");
                        result->nontable_formatter = format_feature_detail_sl_byte;
+                    }
                  }
                  else if (meta->feature_flags & DDCA_STD_CONT)
                     result->nontable_formatter = format_feature_detail_standard_continuous;
@@ -153,7 +221,7 @@ dyn_get_feature_metadata_by_dref(
            }
            else
               PROGRAM_LOGIC_ERROR("Neither DDCA_TABLE nor DDCA_NON_TABLE is set");
-
+#endif
            if (pentry->vcp_global_flags & DDCA_SYNTHETIC)
               free_synthetic_vcp_entry(pentry);
         }
