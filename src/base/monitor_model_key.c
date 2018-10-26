67f521dbe6739f635d6aae756c17ba23ea0175c4
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-10-26 18:31:02 -0400

    monitor_model_key.c/h: - add function mmk_repr() that return a description of the key suitable for use in debug messages   - TODO: make thread safe - replace header and copyright with doxydoc, SPDX form

--- src/base/monitor_model_key.c
+++ src/base/monitor_model_key.c
@@ -1,25 +1,7 @@
-/* monitor_model_key.c
- *
- * <copyright>
- * Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
+/** \file monitor_model_key.c */
+
+// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <assert.h>
 #include <ctype.h>
@@ -188,5 +170,16 @@ monitor_model_string(DDCA_Monitor_Model_Key * model_id) {
 
    return result;
 }
+
+char * mmk_repr(DDCA_Monitor_Model_Key mmk) {
+   // TODO: make thread safe
+   static char buf[100];
+   if (!mmk.defined)
+      strcpy(buf, "[Undefined]");
+   else
+      snprintf(buf, 100, "[%s,%s,%d]", mmk.mfg_id, mmk.model_name, mmk.product_code);
+   return buf;
+}
+
 
 
--- src/base/monitor_model_key.h
+++ src/base/monitor_model_key.h
@@ -1,25 +1,7 @@
-/* monitor_model_key.h
- *
- * <copyright>
- * Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
+/** \file monitor_model_key.h */
+
+// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef MONITOR_MODEL_KEY_H_
 #define MONITOR_MODEL_KEY_H_
@@ -80,5 +62,6 @@ char *
 monitor_model_string(
       DDCA_Monitor_Model_Key * model_id);
 
+char * mmk_repr(DDCA_Monitor_Model_Key mmk);
 
 #endif /* MONITOR_MODEL_KEY_H_ */
