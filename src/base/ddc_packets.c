06a522e5c9588884dc50cd49cf57baeb87c69839
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-10-27 09:49:52 -0400

    ddc_packets.c/h: - change trace group of file from DDCA_TRC_DDC to DDCA_TRC_DDCIO - change header to doxydoc form, copyright to SPDX form

--- src/base/ddc_packets.c
+++ src/base/ddc_packets.c
@@ -1,31 +1,9 @@
-/* ddc_packets.c
- *
+/** \file ddc_packets.c
  * Functions for creating DDC packets and interpreting DDC response packets.
- *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
  */
 
-/** \file
- * Functions for creating DDC packets and interpreting DDC response packets.
- */
+// Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 /** \cond */
 #include <config.h>
@@ -50,7 +28,7 @@
 // Trace control
 //
 
-DDCA_Trace_Group TRACE_GROUP = DDCA_TRC_DDC;
+DDCA_Trace_Group TRACE_GROUP = DDCA_TRC_DDCIO;
 
 
 //
--- src/base/ddc_packets.h
+++ src/base/ddc_packets.h
@@ -1,31 +1,10 @@
-/* ddc_packets.h
- *
+/** \file ddc_packets.h
  * Functions for creating DDC packets and interpreting DDC response packets.
- *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
  */
 
-/** \file
- *  Functions for creating DDC packets and interpreting DDC response packets.
- */
+// Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifndef DDC_PACKETS_H_
 #define DDC_PACKETS_H_
 
