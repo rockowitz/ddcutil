c3ace292a49453d9e1e6c790a1b532e4a5bf8f8a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-11-03 00:28:51 -0400

    ddc_errono.c/h: - convert header to doxydoc form, copyright to SPDX form

--- src/base/ddc_errno.h
+++ src/base/ddc_errno.h
@@ -1,33 +1,9 @@
-/* ddc_errno.h
- *
- * Error codes internal to the application, which are
- * primarily ddcutil related.
- *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
 /** \file
  * Error codes internal to **ddcutil**.
  */
 
+// Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DDC_ERRNO_H_
 #define DDC_ERRNO_H_
@@ -35,7 +11,6 @@
 #include "public/ddcutil_status_codes.h"
 
 #include "base/status_code_mgt.h"
-
 
 
 Status_Code_Info * ddcrc_find_status_code_info(int rc);
