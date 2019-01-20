0bd082ad7bc0b6a6461f3d816755499a6ffd4f1c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-01-20 08:00:05 -0500

    update copyright year

--- src/libmain/api_feature_access.c
+++ src/libmain/api_feature_access.c
@@ -3,7 +3,7 @@
  *  Get, set, and format feature values
  */
 
-// Copyright (C) 2015-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2015-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "config.h"
--- src/libmain/api_metadata.c
+++ src/libmain/api_metadata.c
@@ -1,6 +1,6 @@
 // api_metadata.c
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "config.h"
