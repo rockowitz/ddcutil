701222ec1bd045f5546656a1ea28e1371b7c89e3
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-02-06 10:42:29 -0500

    ddcutil_c_api.h: - remove  deprecated functions ddca_mccs_version_id_name(), ddca_mccs_version_id_desc() - had been deprecated in release 0.9.0
    api_capabilities_internal.h:
    - save declarations here since functions not deleted from api_capabilities.c

--- src/libmain/api_capabilities_internal.h
+++ src/libmain/api_capabilities_internal.h
@@ -35,5 +35,42 @@ void ddca_parse_and_report_capabilities(
       DDCA_Display_Ref          dref,
       int                       depth);
 
+
+// DEPRECATED
+
+
+//
+// MCCS Version Id
+//
+
+/** \deprecated
+ *  Returns the symbolic name of a #DDCA_MCCS_Version_Id,
+ *  e.g. "DDCA_MCCS_V20."
+ *
+ *  @param[in]  version_id  version id value
+ *  @return symbolic name (do not free)
+ */
+__attribute__ ((deprecated))
+char *
+ddca_mccs_version_id_name(
+      DDCA_MCCS_Version_Id  version_id);
+
+/** \deprecated
+ *  Returns the descriptive name of a #DDCA_MCCS_Version_Id,
+ *  e.g. "2.0".
+ *
+ *  @param[in]  version_id  version id value
+ *  @return descriptive name (do not free)
+ *
+ *  @remark added to replace ddca_mccs_version_id_desc() during 0.9
+ *  development, but then use of DDCA_MCCS_Version_Id deprecated
+ */
+__attribute__ ((deprecated))
+char *
+ddca_mccs_version_id_desc(
+      DDCA_MCCS_Version_Id  version_id) ;
+
+
+
 #endif /* API_CAPABILITIES_INTERNAL_H_ */
 
