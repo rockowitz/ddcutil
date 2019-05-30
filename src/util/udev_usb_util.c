c3e026a3a5b83a0f1590233ca3d866b495be04da
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-05-30 07:47:01 -0400

    udev_usb_util.c: - lookup_udev_usb_device_by_devname:   - add then cooment out debug message   - convert header to doxydoc/SPDX form

--- src/util/udev_usb_util.c
+++ src/util/udev_usb_util.c
@@ -1,29 +1,9 @@
-/* udev_usb_util.c
- *
- * <copyright>
- * Copyright (C) 2016-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
  /** \file
   * USB specific udev utility functions
   */
+
+// Copyright (C) 2014-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 /** \cond */
 #include <assert.h>
@@ -38,8 +18,6 @@
 #include "udev_util.h"
 
 #include "udev_usb_util.h"
-
-
 
 
 Usb_Detailed_Device_Summary * new_usb_detailed_device_summary() {
@@ -101,6 +79,7 @@ void report_usb_detailed_device_summary(Usb_Detailed_Device_Summary * devsum, in
  *         NULL if not found
  */
 Usb_Detailed_Device_Summary * lookup_udev_usb_device_by_devname(char * devname) {
+   // printf("(%s) Starting. devname=%s\n", __func__, devname);
    int depth = 0;
    // int d1 = depth+1;
    struct udev * udev;
