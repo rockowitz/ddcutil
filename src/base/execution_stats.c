987e30d33623be067fb9943ee9202e06b6264989
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-07-29 20:13:02 -0400

    esxecution_stats.c: - show_specific_status_counts():   - fix DBGMSG() call - convert header to SPDX/doxydoc format, bump copyright year

--- src/base/execution_stats.c
+++ src/base/execution_stats.c
@@ -1,31 +1,9 @@
-/* execution_stats.c
- *
- * For recording and reporting the count and elapsed time of system calls.
- *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
-/** \file
+/** \file execution_stats.c
  * Record execution statistics, mainly the count and elapsed time of system calls.
  */
+
+// Copyright (C) 2014-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 /** \cond */
 #include <assert.h>
@@ -409,8 +387,8 @@ void show_specific_status_counts(Status_Code_Counts * pcounts) {
    if (debug) {
       DBGMSG("Keys.  keyct=%d", keyct);
       for (int ndx = 0; ndx < keyct; ndx++) {
-         DBGMSG( "keysp[%d]:  %d   %p  %d",
-                 ndx,  keysp[ndx], keysp[ndx], GPOINTER_TO_INT(keysp[ndx]) );
+         DBGMSG( "keysp[%d]:  %p  %d",
+                 ndx, keysp[ndx], GPOINTER_TO_INT(keysp[ndx]) );
       }
    }
    int summed_ct = 0;
