9d5e7574a9e4502d5937f4961a56552a0bd22511
Sanford Rockowitz <rockowitz@minsoft.com>
2019-08-31 21:24:02 -0400

    device_id_util.c: - convert copyright to SPDX form - load_multi_level_segment:   - cast to silence clang warning

--- src/util/device_id_util.c
+++ src/util/device_id_util.c
@@ -1,29 +1,10 @@
-/* device_id_util.c
- *
- * <copyright>
- * Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
 /** @file device_id_util.c
  * Lookup PCI and USB device ids
  */
+
+// Copyright (C) 2014-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 
 /** \cond */
 #include <assert.h>
@@ -379,7 +360,7 @@ static  Multi_Level_Map * load_multi_level_segment(
       }
    }
 
-   *curpos = linendx;
+   *curpos = (int*) linendx;
    return header;
 }
 
