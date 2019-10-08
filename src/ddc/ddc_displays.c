8affb9504e110cb8df3385907d6113ecea090e6c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-08 15:58:25 -0400

    ddc_displays.c: - ddc_report_display_by_dref()   - if I2C communication fails and the display is or appears to be     laptop display, issue message if output level is normal as well 	as verbose.  This should make detect command output clearer.

--- src/ddc/ddc_displays.c
+++ src/ddc/ddc_displays.c
@@ -429,27 +429,21 @@ ddc_report_display_by_dref(Display_Ref * dref, int depth) {
    if (output_level >= DDCA_OL_NORMAL) {
       if (!(dref->flags & DREF_DDC_COMMUNICATION_WORKING) ) {
          rpt_vstring(d1, "DDC communication failed");
+         char * msg = NULL;
+         if (dref->io_path.io_mode == DDCA_IO_I2C)
+         {
+             I2C_Bus_Info * curinfo = dref->detail;
+             if (curinfo->flags & I2C_BUS_EDP)
+                 msg = "This is an eDP laptop display. Laptop displays do not support DDC/CI.";
+             else if ( is_embedded_parsed_edid(dref->pedid) )
+                 msg = "This appears to be a laptop display. Laptop displays do not support DDC/CI.";
+         }
          if (output_level >= DDCA_OL_VERBOSE) {
-#ifdef OLD
-            if (streq(dref->pedid->model_name,   "Unspecified") &&
-                streq(dref->pedid->serial_ascii, "Unspecified") )
-            {
-               rpt_vstring(d1, "This appears to be a laptop display. Laptop displays do not support DDC/CI.");
+            if (!msg) {
+               msg = "Is DDC/CI enabled in the monitor's on-screen display?";
             }
-            else
-               rpt_vstring(d1, "Is DDC/CI enabled in the monitor's on-screen display?");
-#endif
-
-            char * msg = "Is DDC/CI enabled in the monitor's on-screen display?";
-            if (dref->io_path.io_mode == DDCA_IO_I2C) {
-               I2C_Bus_Info * curinfo = dref->detail;
-               if (curinfo->flags & I2C_BUS_EDP)
-                  msg = "This is a eDP laptop display. Laptop displays do not support DDC/CI.";
-               else if ( is_embedded_parsed_edid(dref->pedid) )
-                  msg = "This appears to be a laptop display. Laptop displays do not support DDC/CI.";
-               else
-                  msg = "Is DDC/CI enabled in the monitor's on-screen display?";
-            }
+         }
+         if (msg) {
             rpt_vstring(d1, msg);
          }
       }
