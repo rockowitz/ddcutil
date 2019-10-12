3af212978d809fb350944fcaf7bc17d9354a421d
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-12 11:54:02 -0400

    last_io_count.c/h: - new files that maintain a data structure of the timestamp of   the most recent IO for each /dev/i2c-N, i.e. for each monitor

---
+++ src/base/last_io_event.c
@@ -0,0 +1,136 @@
+// last_io_event.c
+
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <assert.h>
+#include <stdbool.h>
+#include <glib-2.0/glib.h>
+
+#include "last_io_event.h"
+
+
+
+static bool trace_finish_timestamps = false;
+
+// Maintain timestamps
+
+static GPtrArray * timestamps = NULL;
+
+void free_io_event_timestamp_internal(gpointer data) {
+   IO_Event_Timestamp * ptr = (IO_Event_Timestamp *) data;
+   assert(memcmp(ptr->marker, IO_EVENT_TIMESTAMP_MARKER, 4) == 0);
+   ptr->marker[3] = 'X';
+   // Do not free ptr->filename or ptr->function since these point
+   // to constants in compiled code
+   free(data);
+}
+
+IO_Event_Timestamp* find_io_event_timestamp(int fd) {
+   assert(timestamps);
+   IO_Event_Timestamp * result = NULL;
+   for (int ndx = 0; ndx < timestamps->len; ndx++) {
+      IO_Event_Timestamp * cur = g_ptr_array_index(timestamps, ndx);
+      if (cur->fd == fd) {
+         result = cur;
+         break;
+      }
+   }
+   // DBGMSG("Returning %p", result);
+   return result;
+}
+
+
+IO_Event_Timestamp * get_io_event_timestamp(int fd)
+{
+   if (!timestamps) {     // first call?
+      timestamps = g_ptr_array_new_full(4, free_io_event_timestamp_internal);
+   }
+   IO_Event_Timestamp * ts = find_io_event_timestamp(fd);
+   if (!ts) {
+      ts = calloc(1, sizeof(IO_Event_Timestamp));
+      memcpy(ts->marker, IO_EVENT_TIMESTAMP_MARKER, 4);
+      ts->fd = fd;
+      g_ptr_array_add(timestamps, ts);
+   }
+   assert(ts);
+   return ts;
+}
+
+// IO_Event_Timestamp * new_io_event_timestamp(int fd);
+
+
+void free_io_event_timestamp(int fd) {
+   if (timestamps) {
+      IO_Event_Timestamp * ts = find_io_event_timestamp(fd);
+      if (ts)
+         g_ptr_array_remove(timestamps, ts);
+   }
+}
+
+
+// *** Last IO
+
+void record_io_finish(
+      int           fd,
+      uint64_t      finish_time,
+      IO_Event_Type event_type,
+      char *        filename,
+      int           lineno,
+      char *        function)
+{
+   bool debug = false;
+
+   IO_Event_Timestamp * tsrec = get_io_event_timestamp(fd);
+   assert(tsrec);
+
+   uint64_t prior_nanos = 0;
+   uint64_t cur_nanos = 0;
+   uint64_t delta_nanos = 0;
+   uint64_t delta_nanos_then_millis = 0;
+
+   if (tsrec->finish_time != 0) {
+      prior_nanos = tsrec->finish_time;
+      cur_nanos = finish_time;
+      delta_nanos = cur_nanos - prior_nanos;
+      delta_nanos_then_millis = delta_nanos / (1000*1000);
+
+      assert(finish_time > tsrec->finish_time);
+
+      // DBGMSF(debug, "prior_nanos = %"PRIu64", cur_nanos = %"PRIu64", delta_nannos = %"PRIu64", delta millis from delta nanos: %"PRIu64,
+      //                prior_nanos,             cur_nanos,             delta_nanos, delta_nanos_then_millis);
+
+      // DBGMSF(debug, "prior_millis = %"PRIu64", cur_millis = %"PRIu64", delta_nullis = %"PRIu64,
+      //                prior_millis,             cur_millis,             delta_millis);
+      assert(cur_nanos >= prior_nanos);
+      // if (cur_nanos == prior_nanos)
+      //    DBGMSG("======= equal");
+
+   }
+
+   DBGTRC(trace_finish_timestamps | debug, DDCA_TRC_NONE,
+          "fd=%d, event_type = %-10s, function = %-20s, delta: %"PRIu64" nanosec, %"PRIu64" millisec)",     // PRIU64,
+                   fd, io_event_name(event_type), function, delta_nanos, delta_nanos_then_millis);
+
+   // tsrec->fd = fd;   // unnecessary
+   tsrec->event_type  = event_type;
+   tsrec->filename    = filename;
+   tsrec->lineno      = lineno;
+   tsrec->finish_time = finish_time;
+   tsrec->function    =  function;
+}
+
+
+
+
+
+// I2C_RECORD_IO_EVENT(
+//       IE_OPEN,
+//       ( fd = open(filename, (callopts & CALLOPT_RDONLY) ? O_RDONLY : O_RDWR) ),
+//       busno,
+//       fd
+//       );
+// if (fd >= 0)
+// I2C_RECORD_IO_FINISH_NOW(busno, IE_OPEN, fd);
+
+
---
+++ src/base/last_io_event.h
@@ -0,0 +1,78 @@
+/** @file last_io_event.h
+ *
+ */
+
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef LAST_IO_EVENT_H_
+#define LAST_IO_EVENT_H_
+
+#include "public/ddcutil_types.h"
+
+#include "execution_stats.h"
+
+
+#define IO_EVENT_TIMESTAMP_MARKER "IOET"
+typedef
+struct {
+   char          marker[4];
+   uint64_t      finish_time;
+   IO_Event_Type event_type;
+   char *        filename;
+   int           lineno;
+   char *        function;
+   int           fd;       // Linux file descriptor
+} IO_Event_Timestamp;
+
+IO_Event_Timestamp * get_io_event_timestamp(int fd);
+// IO_Event_Timestamp * new_io_event_timestamp(int fd);
+void free_io_event_timestamp(int fd);
+
+
+void record_io_finish(
+      int              fd,
+      uint64_t         finish_time,
+      IO_Event_Type    event_type,
+      char *           filename,
+      int              lineno,
+      char *           function);
+
+#define RECORD_IO_FINISH(_fd, _event_type, _timestamp) \
+   do \
+   record_io_finish(_fd, _timestamp , _event_type, (char*) __FILE__, __LINE__, (char*) __func__); \
+   while(0)
+
+
+#define RECORD_IO_FINISH_NOW(_fd, _event_type) \
+   do \
+   record_io_finish(_fd, cur_realtime_nanosec() , _event_type, (char*) __FILE__, __LINE__, (char*) __func__); \
+   while(0)
+
+
+
+
+#ifdef REF
+void log_io_call(
+        const IO_Event_Type  event_type,
+        const char *         location,
+        uint64_t             start_time_nanos,
+        uint64_t             end_time_nanos);
+#endif
+
+
+// combines calls
+#define RECORD_IO_EVENTX(_fd, _event_type, _cmd_to_time)  { \
+   uint64_t _start_time = cur_realtime_nanosec(); \
+   _cmd_to_time; \
+   uint64_t  end_time = cur_realtime_nanosec(); \
+   log_io_call(_event_type, __func__, _start_time, end_time); \
+   record_io_finish(_fd, end_time, _event_type, __FILE__, __LINE__, (char *)__func__); \
+}
+
+
+
+
+
+
+#endif /* LAST_IO_EVENT_H_ */
