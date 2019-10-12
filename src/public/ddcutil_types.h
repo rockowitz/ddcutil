69cde5fcec19aac7ff24765a6f03aadb7fb116ea
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-12 11:57:53 -0400

    ddcutil_types.h: - add enum DDCA_Trace_Options for specifying trace options,   currently DDCA_TRCOPT_TIMESTAMP to show timestamps and   DDCA_TRCOPT_THREAD_ID to show the thread id

--- src/public/ddcutil_types.h
+++ src/public/ddcutil_types.h
@@ -146,6 +146,12 @@ typedef enum {
 } DDCA_Trace_Group;
 
 
+typedef enum {
+   DDCA_TRCOPT_TIMESTAMP = 0x00,
+   DDCA_TRCOPT_THREAD_ID = 0x02,
+} DDCA_Trace_Options;
+
+
 //
 // Performance statistics
 //
