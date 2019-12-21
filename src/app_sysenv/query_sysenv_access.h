71dbf64916b6e408c1e719d0254ebb357af28d96
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-12-21 14:25:32 -0500

    query_sysenv_access.c/h: - convert headers to doxydoc/SPDX form

--- src/app_sysenv/query_sysenv_access.h
+++ src/app_sysenv/query_sysenv_access.h
@@ -1,29 +1,11 @@
-/* query_sysenv_access.h
+/* @file query_sysenv_access.h
  *
- * <copyright>
- * Copyright (C) 2014-2015 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
+ *  Checks on the the existence of and access to /dev/i2c devices
  */
- 
- /** \f
-  *  Checks on the the existence of and access to /dev/i2c devices
-  */
+
+// Copyright (C) 2014-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 
 #ifndef SYSENV_ACCESS_H_
 #define QUERY_SYSENV_ACCESS_H_
