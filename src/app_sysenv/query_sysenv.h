1e4911c24582b88d1dd712bfb7dcb48333081991
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-01-15 06:05:50 -0500

    Convert headers of remaining files in app_sysenv directory to doxydoc/spdx form

--- src/app_sysenv/query_sysenv.h
+++ src/app_sysenv/query_sysenv.h
@@ -3,6 +3,9 @@
  *  Primary file for the ENVIRONMENT command
  */
 
+// Copyright (C) 2014-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifndef QUERY_SYSENV_H_
 #define QUERY_SYSENV_H_
 
