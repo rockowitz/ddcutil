2facfbeecccdff0387d2ddd734d1f655103fee0e
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-02-07 07:38:32 -0500

    file_util.c/h: doxydox/SPDX hdeaders

--- src/util/file_util.h
+++ src/util/file_util.h
@@ -1,29 +1,10 @@
-/* file_util.h
- *
- * <copyright>
- * Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
+/** \file file_util.h
+ *  File utility functions
  */
 
-/** @file file_util.h
- * File utility functions
- */
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 
 #ifndef FILE_UTIL_H_
 #define FILE_UTIL_H_
@@ -58,6 +39,5 @@ GPtrArray * get_filenames_by_filter(const char * dirnames[], Dirent_Filter filte
 
 int filename_for_fd(int fd, char** p_fn);
 char * filename_for_fd_t(int fd);
-
 
 #endif /* FILE_UTIL_H_ */
