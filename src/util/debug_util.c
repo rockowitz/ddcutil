d787ce66fa20a9404a2c1738b5c07f681fe71b45
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-02-12 18:42:56 -0500

    debug_util.c/h: - add iftest for HAVE_EXECINFO_H depdenging on whether header file execinfo.h exists (as detected by configure) - convert headers to doxydoc/SPDX form

--- src/util/debug_util.c
+++ src/util/debug_util.c
@@ -1,32 +1,15 @@
-/* debug_util.c
- *
- * <copyright>
- * Copyright (C) 2016-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
 /** @file debug_util.c
+ *
  * Functions for debugging
  */
 
+// Copyright (C) 2016-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 /** \cond */
+#ifdef HAVE_EXECINFO_H
 #include <execinfo.h>
+#endif
 #include <glib-2.0/glib.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -39,7 +22,7 @@
 
 #include "debug_util.h"
 
-
+#ifdef HAVE_EXECINFO_H
 /* Extracts the function name and offset from a backtrace line
  *
  * \param  bt_line   line returned by backtrace()
@@ -81,6 +64,7 @@ static char * extract_function(char * bt_line, bool name_only) {
       printf("(%s) Returning |%s|\n", __func__, result);
    return result;
 }
+#endif
 
 
 #ifdef OLD
@@ -136,6 +120,7 @@ void show_backtrace(int stack_adjust)
 #endif
 
 GPtrArray * get_backtrace(int stack_adjust) {
+#ifdef HAVE_EXECINFO_H
    bool debug = false;
    if (debug)
       printf("(%s) Starting.  stack_adjust = %d\n", __func__, stack_adjust);
@@ -179,12 +164,15 @@ GPtrArray * get_backtrace(int stack_adjust) {
       free(strings);
    }
    return result;
+#else
+   return NULL;
+#endif
 }
 
 void show_backtrace(int stack_adjust) {
    GPtrArray * callstack = get_backtrace(stack_adjust);
    if (!callstack) {
-      perror("backtrace_symbols unavailable");
+      perror("backtrace unavailable");
    }
    else {
       printf("Current call stack:\n");
