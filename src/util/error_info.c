8be38fbd1ecc1de0005666263b1c949d56d89c56
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-02-12 18:30:29 -0500

    error_info.c/h: - convert headers to doxydoc/SPDX form

--- src/util/error_info.c
+++ src/util/error_info.c
@@ -1,27 +1,5 @@
-/* ddc_error.c
+/** \f error_info.c
  *
- * <copyright>
- * Copyright (C) 2017-2918 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
-/** \f
  *  Struct for reporting errors.
  *
  *  #Error_Info provides a pseudo-exception framework that can be integrated
@@ -31,10 +9,13 @@
  *  error is retained for use by higher levels in the call stack.
  */
 
+// Copyright (C) 2017-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+
 /** \cond */
 
 // #define _GNU_SOURCE     // for reallocarray() in stdlib.h
-
 #include <assert.h>
 #include <glib-2.0/glib.h>
 #include <stdlib.h>
--- src/util/error_info.h
+++ src/util/error_info.h
@@ -1,29 +1,17 @@
-/* error_info.h
+/** \f error_info.h
  *
- * <copyright>
- * Copyright (C) 2017 Sanford Rockowitz <rockowitz@minsoft.com>
+ *  Struct for reporting errors that collects causes
  *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
+ *  #Error_Info provides a pseudo-exception framework that can be integrated
+ *  with more traditional status codes.  Instead of returning a status code,
+ *  a C function returns a pointer to an #Error_Info instance in the case of
+ *  an error, or NULL if there is no error.  Information about the cause of an
+ *  error is retained for use by higher levels in the call stack.
  */
 
-/** \f
- *  Struct for reporting errors that collects causes
- */
+// Copyright (C) 2017-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 
 #ifndef ERROR_INFO_H_
 #define ERROR_INFO_H_
