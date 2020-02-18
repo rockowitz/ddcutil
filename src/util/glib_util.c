08e24460ca6893d47ccae181a8d9af9a0f917cc1
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-02-17 22:04:51 -0500

    glib_util.c/h: - get_thread_fixed_buffer():   - use g_new0() instead of g_new() to ensure all bytes of the allocated buffer are 0   - convert headers to doxydoc/spdx form

--- src/util/glib_util.c
+++ src/util/glib_util.c
@@ -1,30 +1,10 @@
-/* glib_util.c
- *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
 /** @file glib_util.c
  *
- *  Functions for use with glib.
+ *  Utility functions for glib.
  */
+
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 /** \cond */
 #include <assert.h>
@@ -380,8 +360,8 @@ get_thread_fixed_buffer(
    // printf("(%s) this_thread=%p, buf=%p\n", __func__, this_thread, buf);
 
    if (!buf) {
-      buf = g_new(char, buffer_size);
-      buf[0] = '\0';     // (sort of) mark buffer as unused
+      buf = g_new0(char, buffer_size);   //g_new0 initializes to 0
+      // buf[0] = '\0';     // (sort of) mark buffer as unused
       g_private_set(buf_key_ptr, buf);
    }
 
--- src/util/glib_util.h
+++ src/util/glib_util.h
@@ -1,31 +1,11 @@
-/* glib_util.h
+/** @file glib_util.h
  *
- * Utility functions for glib.
- *
- * <copyright>
- * Copyright (C) 2014-2016 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
+ *  Utility functions for glib.
  */
 
-/** @file glib_util.h
- *  Functions for use with glib
- */
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 
 #ifndef GLIB_UTIL_H_
 #define GLIB_UTIL_H_
