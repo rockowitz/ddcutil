a7f990f9de2ffe40d4283380d7fe17eb7dacf310
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-03-24 21:40:25 -0400

    app_capabilities.c/h: - clean up includes

--- src/app_ddcutil/app_capabilities.c
+++ src/app_ddcutil/app_capabilities.c
@@ -1,108 +1,43 @@
-// app_capabilities.c
+/** \file app_capabilities.c
+ *
+ *  Capabilities functions factored out of main.c
+ */
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
-
- 
-
-// TEMPORARILY EVERYTHING FROM MAIN
-
 
 /** \cond */
 #include <config.h>
 
 #include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <setjmp.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "util/data_structures.h"
 #include "util/error_info.h"
-#include "util/failsim.h"
 #include "util/report_util.h"
 #include "util/string_util.h"
-#include "util/sysfs_util.h"
-/** \endcond */
 
 #include "public/ddcutil_types.h"
 
-#include "base/adl_errors.h"
-#include "base/base_init.h"
 #include "base/core.h"
-#include "base/ddc_errno.h"
-#include "base/ddc_packets.h"
 #include "base/displays.h"
-#include "base/dynamic_sleep.h"
-#include "base/linux_errno.h"
-#include "base/parms.h"
-#include "base/sleep.h"
-#include "base/status_code_mgt.h"
-#include "base/thread_retry_data.h"
-#include "base/tuned_sleep.h"
-#include "base/thread_sleep_data.h"
+/** \endcond */
 
 #include "vcp/parse_capabilities.h"
-#include "vcp/vcp_feature_codes.h"
 
-#include "dynvcp/dyn_dynamic_features.h"
 #include "dynvcp/dyn_parsed_capabilities.h"
 
-#include "i2c/i2c_bus_core.h"
-#include "i2c/i2c_strategy_dispatcher.h"
-#include "adl/adl_shim.h"
-
-#include "usb/usb_displays.h"
-
-#include "ddc/ddc_displays.h"
-#include "ddc/ddc_multi_part_io.h"
-#include "ddc/ddc_output.h"
-#include "ddc/ddc_packet_io.h"
 #include "ddc/ddc_read_capabilities.h"
-#include "ddc/ddc_services.h"
-#include "ddc/ddc_try_stats.h"
-#include "ddc/ddc_vcp_version.h"
-#include "ddc/ddc_vcp.h"
-
-#include "cmdline/cmd_parser_aux.h"    // for parse_feature_id_or_subset(), should it be elsewhere?
-#include "cmdline/cmd_parser.h"
-#include "cmdline/parsed_cmd.h"
-
-#include "app_ddcutil/app_dynamic_features.h"
-#include "app_ddcutil/app_dumpload.h"
-#include "app_ddcutil/app_getvcp.h"
-#include "app_ddcutil/app_setvcp.h"
-
-#include "app_sysenv/query_sysenv.h"
-#ifdef USE_USB
-#include "app_sysenv/query_sysenv_usb.h"
-#endif
-
-#ifdef INCLUDE_TESTCASES
-#include "test/testcases.h"
-#endif
-
-#ifdef USE_API
-#include "public/ddcutil_c_api.h"
-#endif
-
 
 #include "app_ddcutil/app_capabilities.h"
 
-// Default race class for this file
+// Default trace class for this file
 // static DDCA_Trace_Group TRACE_GROUP = DDCA_TRC_TOP;
 
 
-
-
-
-// TODO: refactor
-//       originally just displayed capabilities, now returns parsed capabilities as well
-//       these actions should be separated
 Parsed_Capabilities *
 perform_get_capabilities_by_display_handle(Display_Handle * dh) {
    // FILE * fout = stdout;
@@ -160,8 +95,6 @@ perform_get_capabilities_by_display_handle(Display_Handle * dh) {
    DBGMSF(debug, "Returning: %p", pcap);
    return pcap;
 }
-
-
 
 
 void perform_show_parsed_capabilities(char * capabilities_string, Display_Handle * dh, Parsed_Capabilities * pcap) {
--- src/app_ddcutil/app_capabilities.h
+++ src/app_ddcutil/app_capabilities.h
@@ -1,21 +1,25 @@
-// app_capabilities.h
+/** \file app_capabilities.h
+ *
+ *  Capabilities functions factored out from main.c
+ */
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
-
- 
 
 #ifndef APP_CAPABILITIES_H_
 #define APP_CAPABILITIES_H_
 
 #include "base/displays.h"
-// #include "base/capabilities.h"
 #include "vcp/parse_capabilities.h"
 
+Parsed_Capabilities *
+perform_get_capabilities_by_display_handle(
+      Display_Handle * dh);
 
-Parsed_Capabilities *  perform_get_capabilities_by_display_handle(Display_Handle * dh);
-void perform_show_parsed_capabilities(char * capabilities_string, Display_Handle * dh, Parsed_Capabilities * pcap);
-
-
+void
+perform_show_parsed_capabilities(
+      char * capabilities_string,
+      Display_Handle * dh,
+      Parsed_Capabilities * pcap);
 
 #endif /* APP_CAPABILITIES_H_ */
