37a81d8f1e569dad390ef26aca6c80ac547ce57b
root <root@banner.minsoft.com>
2020-04-21 06:00:43 -0400

    parse_capabilities.h: - add enum typedef Parsed_Capabilities_validity - add interpretation function parsed_cpabiliteis_validity_name() - struct Parsded_Capabilities:   - add fields: model, raw_commands_segment_valid. caps_validity, messages   - remove field validity - remove parse_capabilities_buffer (unused, definition is iftested out) - rename parsed_capabilities_man_support_table_reads() -> parsed_capapilities_supports_table_reads() - rename parsed_capabiliteis_feature_ids() -> get_parsed_capabilities_feature_ids() - add dbgrpt_parsed_capabilities()

--- src/vcp/parse_capabilities.h
+++ src/vcp/parse_capabilities.h
@@ -1,7 +1,7 @@
 /** @file parse_capabilities.h
  */
 
-// Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef PARSE_CAPABILITIES_H_
@@ -15,28 +15,38 @@
 
 #include "base/vcp_version.h"
 
+typedef enum {
+   CAPABILITIES_VALID,
+   CAPABILITIES_USABLE,
+   CAPABILITIES_INVALID
+} Parsed_Capabilities_Validity;
+
 
 #define PARSED_CAPABILITIES_MARKER "CAPA"
 /** Contains parsed capabilities information */
 typedef struct {
    char                    marker[4];             // always "CAPA"
    char *                  raw_value;
-   char *                  mccs_version_string;
-   bool                    raw_cmds_segment_seen;
-   bool                    raw_vcp_features_seen;
    bool                    raw_value_synthesized;
-   bool                    invalid;
-   Byte_Value_Array        commands;             // each stored byte is command id
-   GPtrArray *             vcp_features;         // entries are Capabilities_Feature_Record *
+   char *                  model;
+   char *                  mccs_version_string;
    DDCA_MCCS_Version_Spec  parsed_mccs_version;  // parsed mccs_version_string, DDCA_VSPEC_UNKNOWN if parsing fails
+   bool                    raw_cmds_segment_seen;
+   bool                    raw_cmds_segment_valid;
+   Byte_Value_Array        commands;             // each stored byte is command id
+   bool                    raw_vcp_features_seen;
+   GPtrArray *             vcp_features;         // entries are Capabilities_Feature_Record *
+   Parsed_Capabilities_Validity caps_validity;
+   GPtrArray *             messages;
 } Parsed_Capabilities;
 
 
-Parsed_Capabilities* parse_capabilities_buffer(Buffer * capabilities);
 Parsed_Capabilities* parse_capabilities_string(char * capabilities);
 void                 free_parsed_capabilities(Parsed_Capabilities * pcaps);
-Byte_Bit_Flags       parsed_capabilities_feature_ids(Parsed_Capabilities * pcaps, bool readable_only);
-bool                 parsed_capabilities_may_support_table_commands(Parsed_Capabilities * pcaps);
+Byte_Bit_Flags       get_parsed_capabilities_feature_ids(Parsed_Capabilities * pcaps, bool readable_only);
+bool                 parsed_capabilities_supports_table_commands(Parsed_Capabilities * pcaps);
+char *               parsed_capabilities_validity_name(Parsed_Capabilities_Validity validity);
+void                 dbgrpt_parsed_capabilities(Parsed_Capabilities * pcaps, int depth);
 
 
 // Tests
