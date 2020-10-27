9a3b3bd9e3418332ebba8b50020d95f3e299613d
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-10-26 21:48:07 -0400

    iftest out ADL specific code
    query_sysenv_modules.c:
    - check_i2c_dev_module():
      - iftest out fglrx specific code, in preparation for deletion

--- src/app_sysenv/query_sysenv_modules.c
+++ src/app_sysenv/query_sysenv_modules.c
@@ -179,13 +179,17 @@ void check_i2c_dev_module(Env_Accumulator * accum, int depth) {
    if (!is_builtin)
       rpt_vstring(d1,"Module %s is %sloaded", "i2c_dev", (is_loaded) ? "" : "NOT ");
 
+#ifdef HAVE_ADL
    bool module_required = !only_fglrx(accum->driver_list);
    if (!module_required) {
       rpt_nl();
       rpt_vstring(d0,"Using only proprietary fglrx driver. Module i2c_dev not required.");
       accum->module_i2c_dev_needed = false;
    }
-   else if (!is_builtin) {
+   else
+#endif
+   if (!is_builtin) {
+      assert(accum->dev_i2c_device_numbers);    // already set
       if (bva_length(accum->dev_i2c_device_numbers) == 0 && !is_loaded ) {
          rpt_nl();
          rpt_vstring(d0, "No /dev/i2c-N devices found, and module i2c_dev is not loaded.");
