739795501d269e42a077ce1c45749ac74ef3771c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-10-26 23:28:28 -0400

    move reporting functions and macros from query_sysenv_sysfs.c to sysfs_util.c/h

--- src/util/sysfs_util.h
+++ src/util/sysfs_util.h
@@ -66,4 +66,95 @@ bool
 sysfs_is_ignorable_i2c_device(
       int busno);
 
+void rpt_attr_output(
+      int depth,
+      const char * node,
+      const char * op,
+      const char * value);
+
+
+typedef bool (*Fn_Filter)(const char * fn, const char * val);
+
+char * get_single_subdir_name(char * dirname, Fn_Filter filter, char * val);
+
+char *
+assemble_sysfs_path2(
+      char *        buffer,
+      int           bufsz,
+      const char *  fn_segment,
+      va_list       ap);
+
+bool
+rpt2_attr_text(
+      int          depth,
+      char **      value_loc,
+      char *       fn_segment,
+      ...);
+
+#define RPT2_ATTR_TEXT(depth, value_loc, fn_segment, ...) \
+   rpt2_attr_text(depth, value_loc, fn_segment, ##__VA_ARGS__, NULL)
+
+bool rpt2_attr_binary(
+      int          depth,
+      GByteArray ** value_loc,
+      const char *  fn_segment,
+      ...);
+
+bool rpt2_attr_edid(
+       int           depth,
+       GByteArray ** value_loc,
+       char *        fn_segment,
+       ...);
+
+#define RPT2_ATTR_EDID(depth, value_loc, fn_segment, ...) \
+   rpt2_attr_edid(depth, value_loc, fn_segment, ##__VA_ARGS__, NULL)
+
+
+bool
+rpt2_attr_realpath(
+      int          depth,
+      char **      value_loc,
+      char *       fn_segment,
+      ...);
+
+
+#define RPT2_ATTR_REALPATH(depth, value_loc, fn_segment, ...) \
+   rpt2_attr_realpath(depth, value_loc, fn_segment, ##__VA_ARGS__, NULL)
+
+
+bool
+rpt2_attr_realpath_basename(
+      int          depth,
+      char **      value_loc,
+      const char * fn_segment,
+      ...);
+
+#define RPT2_ATTR_REALPATH_BASENAME(depth, value_loc, fn_segment, ...) \
+   rpt2_attr_realpath_basename(depth, value_loc, fn_segment, ##__VA_ARGS__, NULL)
+
+
+bool rpt2_attr_single_subdir(
+      int          depth,
+      char **      value_loc,
+      Fn_Filter    predicate_function,
+      char *       predicate_value,
+      const char * fn_segment,
+      ...);
+
+#define RPT2_ATTR_SINGLE_SUBDIR(depth, value_loc, predicate_func, predicate_val, fn_segment, ...) \
+   rpt2_attr_single_subdir(depth, value_loc, predicate_func, predicate_val, fn_segment, ##__VA_ARGS__, NULL)
+
+
+bool rpt2_attr_note_subdir(
+      int          depth,
+      char **      value_loc,
+      const char * fn_segment,
+      ...);
+
+#define RPT2_ATTR_NOTE_SUBDIR(depth, value_loc, fn_segment, ...) \
+   rpt2_attr_note_subdir(depth, value_loc, fn_segment,  ##__VA_ARGS__, NULL)
+
+
+
+
 #endif /* SYSFS_UTIL_H_ */
