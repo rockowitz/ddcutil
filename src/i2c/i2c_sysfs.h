160b07b3d5ef64431842d8fabd953aaa12f42191
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-10-30 20:30:25 -0400

    factor out i2c sys query from i2c_bus_core.c to new i2c_sysfs.c/h

---
+++ src/i2c/i2c_sysfs.h
@@ -0,0 +1,35 @@
+/** \file i2c_sysfs.h
+ *
+ *  Query /sys file system for information on I2C devices
+ */
+
+// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef I2C_SYSFS_H_
+#define I2C_SYSFS_H_
+
+typedef struct {
+   int     busno;
+   bool    is_display_port;
+   char *  pci_device_path;
+   char *  drm_connector_path;
+   char *  connector;
+   char *  ddc_path;
+   char *  linked_ddc_filename;
+   char *  device_name;
+   char *  drm_dp_aux_name;
+   char *  drm_dp_aux_dev;
+   char *  i2c_dev_name;
+   char *  i2c_dev_dev;
+   char *  driver;
+   char *  ddc_name;
+   char *  ddc_i2c_dev_name;
+   char *  ddc_i2c_dev_dev;
+} I2C_Sys_Info;
+
+void           free_i2c_sys_info(I2C_Sys_Info * info);
+I2C_Sys_Info * get_i2c_sys_info(   int busno, int depth);
+void           report_i2c_sys_info(int busno, int depth);
+
+#endif /* I2C_SYSFS_H_ */
