a4df7ab39fd88c536643677f6233a08567a080c2
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-11-10 19:25:37 -0500

    ddc detect: display count erroneously set to 0
    ddc_displays.c:
    - ddc_report_displays():
      - display count was erroneously being reset to 0

--- src/ddc/ddc_displays.c
+++ src/ddc/ddc_displays.c
@@ -541,7 +541,6 @@ ddc_report_displays(bool include_invalid_displays, int depth) {
          rpt_title("",0);
       }
    }
-   display_ct = 0;
    if (display_ct == 0) {
       rpt_vstring(depth, "No %sdisplays found.", (!include_invalid_displays) ? "active " : "");
       if ( get_output_level() >= DDCA_OL_NORMAL )
