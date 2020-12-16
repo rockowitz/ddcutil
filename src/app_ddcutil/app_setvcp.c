6d32219ad9badfeab28a17723d214b583fee849e
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-12-16 05:15:34 -0500

    convert app_set_vcp_value() to use new setvcp argument parsing

--- src/app_ddcutil/app_setvcp.c
+++ src/app_ddcutil/app_setvcp.c
@@ -88,54 +88,70 @@ parse_vcp_value(
 /** Parses the Set VCP arguments passed and sets the new value.
  *
  *   \param  dh         display handle
- *   \param  feature    feature code (as string)
+ *   \param  feature    feature code
+ *   \param  value_type indicates if a relative value
  *   \param  new_value  new feature value (as string)
  *   \param  force      attempt to set feature even if feature code unrecognized
  *   \return NULL if success, Error_Info if error
  */
-// TODO: consider moving value parsing to command parser
 Error_Info *
 app_set_vcp_value(
       Display_Handle * dh,
+#ifdef OLD
       char *           feature,
+#else
+      Byte             feature_code,
+      Setvcp_Value_Type value_type,
+#endif
       char *           new_value,
       bool             force)
 {
    FILE * ferr = stderr;
-   bool debug = false;
+   bool debug = true;
+#ifdef OLD
    DBGTRC(debug,DDCA_TRC_TOP, "Starting. feature=%s, new_value=%s, force=%s", feature, new_value, sbool(force));
+#else
+   DBGTRC(debug,DDCA_TRC_TOP, "Starting. feature=0x%02x, new_value=%s, value_type=%s, force=%s",
+                feature_code, new_value, setvcp_value_type_name(value_type), sbool(force));
+
+#endif
    assert(new_value && strlen(new_value) > 0);
 
    DDCA_Status                ddcrc = 0;
    Error_Info *               ddc_excp = NULL;
    long                       longtemp;
+#ifdef OLD
    Byte                       feature_code;
+#endif
    Display_Feature_Metadata * dfm = NULL;
    bool                       good_value = false;
    DDCA_Any_Vcp_Value         vrec;
 
+#ifdef OLD
    bool ok = any_one_byte_hex_string_to_byte_in_buf(feature, &feature_code);
    if (!ok) {
       f0printf(ferr, "Invalid VCP feature code: %s\n", feature);
       ddc_excp = errinfo_new2(DDCRC_ARG, __func__, "Invalid VCP feature code: %s\n", feature);
       goto bye;
    }
+#endif
 
    dfm = dyn_get_feature_metadata_by_dh_dfm(feature_code,dh, (force || feature_code >= 0xe0) );
    if (!dfm) {
-      f0printf(ferr, "Unrecognized VCP feature code: %s\n", feature);
+      f0printf(ferr, "Unrecognized VCP feature code: 0x%02x\n", feature_code);
       ddc_excp = errinfo_new2(DDCRC_UNKNOWN_FEATURE, __func__,
-                              "Unrecognized VCP feature code: %s", feature);
+                              "Unrecognized VCP feature code: 0x%02x", feature_code);
       goto bye;
    }
 
    if (!(dfm->feature_flags & DDCA_WRITABLE)) {
-      f0printf(ferr, "Feature %s (%s) is not writable\n", feature, dfm->feature_name);
+      f0printf(ferr, "Feature 0x%02x (%s) is not writable\n", feature_code, dfm->feature_name);
       ddc_excp = errinfo_new2(DDCRC_INVALID_OPERATION, __func__,
-                 "Feature %s (%s) is not writable", feature, dfm->feature_name);
+                 "Feature 0x%02x (%s) is not writable", feature_code, dfm->feature_name);
       goto bye;
    }
 
+#ifdef OLD
    // Check for relative values
    char value_prefix = ' ';
    if (new_value[0] == '+' || new_value[0] == '-') {
@@ -143,9 +159,14 @@ app_set_vcp_value(
       value_prefix = new_value[0];
       new_value = new_value+1;
    }
+#endif
 
    if (dfm->feature_flags & DDCA_TABLE) {
+#ifdef OLD
       if (value_prefix != ' ') {
+#else
+      if (value_type != VALUE_TYPE_ABSOLUTE) {
+#endif
          f0printf(ferr, "Relative VCP values valid only for Continuous VCP features\n");
          ddc_excp = errinfo_new2(DDCRC_INVALID_OPERATION, __func__,
                                  "Relative VCP values valid only for Continuous VCP features");
@@ -175,7 +196,11 @@ app_set_vcp_value(
          goto bye;
       }
 
+#ifdef OLD
       if ( value_prefix != ' ') {
+#else
+      if (value_type != VALUE_TYPE_ABSOLUTE) {
+#endif
          if ( !(dfm->feature_flags & DDCA_CONT) ) {
             f0printf(ferr, "Relative VCP values valid only for Continuous VCP features\n");
             // char * feature_name =  get_version_sensitive_feature_name(entry, vspec);
@@ -205,13 +230,21 @@ app_set_vcp_value(
             goto bye;
          }
 
+#ifdef OLD
          if ( value_prefix == '+') {
+#else
+         if ( value_type == VALUE_TYPE_RELATIVE_PLUS) {
+#endif
             longtemp = parsed_response->cur_value + longtemp;
             if (longtemp > parsed_response->max_value)
                longtemp = parsed_response->max_value;
          }
          else {
+#ifdef OLD
             assert( value_prefix == '-');
+#else
+            assert( value_type == VALUE_TYPE_RELATIVE_MINUS);
+#endif
             longtemp = parsed_response->cur_value - longtemp;
             if (longtemp < 0)
                longtemp = 0;
@@ -232,17 +265,18 @@ app_set_vcp_value(
       ddcrc = ERRINFO_STATUS(ddc_excp);
       switch(ddcrc) {
       case DDCRC_VERIFY:
-            f0printf(ferr, "Verification failed for feature %02x\n", feature_code);
+            // f0printf(ferr, "Verification failed for feature %02x\n", feature_code);
             ddc_excp = errinfo_new_with_cause3(ddcrc, ddc_excp, __func__,
                                                "Verification failed for feature %02x", feature_code);
             break;
       default:
          // Is this proper error message?
-         f0printf(ferr, "Setting value failed for feature %02x. rc=%s\n", feature_code, psc_desc(ddcrc));
-         if (ddcrc == DDCRC_RETRIES)
-            f0printf(ferr, "    Try errors: %s\n", errinfo_causes_string(ddc_excp));
+         // f0printf(ferr, "Setting value failed for feature %02x. rc=%s\n", feature_code, psc_desc(ddcrc));
+         // if (ddcrc == DDCRC_RETRIES)
+         //    f0printf(ferr, "    Try errors: %s\n", errinfo_causes_string(ddc_excp));
          ddc_excp = errinfo_new_with_cause3(ddcrc, ddc_excp, __func__,
-                                            "Setting value failed for feature %02x", feature_code);
+                                            "Setting value failed for feature %02x, rc=%s",
+                                            feature_code, psc_desc(ddcrc));
       }
    }
 
--- src/app_ddcutil/app_setvcp.h
+++ src/app_ddcutil/app_setvcp.h
@@ -3,7 +3,7 @@
  *  Implement the SETVCP command
  */
 
-// Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef APP_SETVCP_H_
@@ -11,13 +11,19 @@
 
 #include "util/error_info.h"
 
+#include "cmdline/parsed_cmd.h"
 #include "base/displays.h"
 #include "base/status_code_mgt.h"
 
 Error_Info *
 app_set_vcp_value(
         Display_Handle * dh,
+#ifdef OLD
         char *           feature,
+#else
+      Byte             feature_code,
+      Setvcp_Value_Type value_type,
+#endif
         char *           new_value,
         bool             force);
 
