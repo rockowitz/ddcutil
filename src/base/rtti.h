852fe445d42f163c0533e6e0d94d7b07b47e2620
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-12-21 10:54:37 -0500

    rtti.h: add macro RTTI_ADD_FUNC()

--- src/base/rtti.h
+++ src/base/rtti.h
@@ -2,11 +2,13 @@
  * Primitive runtime type information facilities
  */
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2020 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef RTTI_H_
 #define RTTI_H_
+
+#define RTTI_ADD_FUNC(_NAME) rtti_func_name_table_add(_NAME, #_NAME);
 
 void   rtti_func_name_table_add(void * func_addr, const char * func_name);
 char * rtti_get_func_name_by_addr(void * ptr);
