cadcd2a4a656eab6f58af50313955323ed982e73
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-12-25 14:02:57 -0500

    factor out TESTCASES code from main() into new files testcases.c/h

---
+++ src/app_ddcutil/app_testcases.c
@@ -0,0 +1,49 @@
+// app_testcases.c
+
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "ddcutil_status_codes.h"
+
+#include "util/error_info.h"
+#include "base/core.h"
+#include "cmdline/parsed_cmd.h"
+#include "ddc/ddc_vcp.h"
+#include "test/testcases.h"
+
+bool
+app_testcaes(Parsed_Cmd parsed_cmd)
+{
+   int testnum;
+   bool ok = true;
+   int ct = sscanf(parsed_cmd->args[0], "%d", &testnum);
+   if (ct != 1) {
+      f0printf(fout, "Invalid test number: %s\n", parsed_cmd->args[0]);
+      ok = false;
+   }
+   else {
+      ddc_ensure_displays_detected();
+      ok = true;
+      // Why is ddc_save_current_settings() call here?
+      Error_Info * ddc_excp = ddc_save_current_settings(dh);
+      if (ddc_excp)  {
+         f0printf(fout(), "Save current settings failed. rc=%s\n", psc_desc(ddc_excp->status_code));
+         if (ddc_excp->status_code == DDCRC_RETRIES)
+            f0printf(fout(), "    Try errors: %s", errinfo_causes_string(ddc_excp) );
+         errinfo_report(ddc_excp, 0);   // ** ALTERNATIVE **/
+         errinfo_free(ddc_excp);
+         // ERRINFO_FREE_WITH_REPORT(ddc_excp, report_exceptions);
+         ok = false;
+      }
+      if (ok) {
+         if (!parsed_cmd->pdid)
+            parsed_cmd->pdid = create_dispno_display_identifier(1);   // default monitor
+         ok = execute_testcase(testnum, parsed_cmd->pdid);
+      }
+   }
+
+return ok;
+}
---
+++ src/app_ddcutil/app_testcases.h
@@ -0,0 +1,15 @@
+// app_testcases.h
+
+// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef APP_TESTCASES_H_
+#define APP_TESTCASES_H_
+
+#include <stdbool.h>
+#include "cmdline/parsed_cmd.h"
+
+bool
+app_testcaes(Parsed_Cmd parsed_cmd);
+
+#endif /* APP_TESTCASES_H_ */
