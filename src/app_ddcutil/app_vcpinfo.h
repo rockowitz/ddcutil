fc261bed7aecba45df8b434c7b388c23a077c439
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-12-25 03:09:31 -0500

    app_vcpinfo.h: delete report_vcp_feature_table_entry
    used only internally in app_vcpinfo.c

--- src/app_ddcutil/app_vcpinfo.h
+++ src/app_ddcutil/app_vcpinfo.h
@@ -1,6 +1,6 @@
 /** \file app_vcpinfo.h
  *
- *  vcpinfo and (deprecated) listvcp commands
+ *  Implement vcpinfo and (deprecated) listvcp commands
  */
 
 // Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
@@ -16,15 +16,9 @@
 #include "vcp/vcp_feature_codes.h"
 #include "vcp/vcp_feature_set.h"
 
-
 void
 app_listvcp(
       FILE * fh);
-
-void
-report_vcp_feature_table_entry(
-      VCP_Feature_Table_Entry * vfte,
-      int                       depth);
 
 bool
 app_vcpinfo(
