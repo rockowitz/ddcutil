bdd8efb7239626722e0e55235a89b1ae39bba660
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-01-03 00:15:08 -0500

    add function report_build_options()
    - report_build_options():
      - reports the options with which the program was built

--- src/base/build_info.c
+++ src/base/build_info.c
@@ -1,33 +1,14 @@
 /** \file build_info.c
  *
- *  Build Information: version, build timestamp, etc.
+ *  Build Information: version, build options etc.
  */
 
-/* build_info.c
- *
- * <copyright>
- * Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <config.h>
 
+#include "util/report_util.h"
 #include "base/build_info.h"
 
 const char * BUILD_VERSION = VERSION;      /**< ddcutil version */
@@ -37,3 +18,90 @@ const char * BUILD_VERSION = VERSION;      /**< ddcutil version */
 const char * BUILD_DATE = "mmm dd yyyy";   /**<  build date, to be patched at link time */
 // const char * BUILD_TIME = __TIME__;
 const char * BUILD_TIME = "hh:mm:ss";      /**< build time, to be patched at link time */
+
+
+void report_build_options(int depth) {
+   int d1 = depth+1;
+   rpt_label(depth, "General Build Options:");
+
+// doesn't work, fails if option name undefined
+// #define REPORT_BUILD_OPTION(_name) rpt_vstring(d1, "%-20s %s", #_name ":", ( _name ## "+0" ) ? "Set" : "Not Set" )
+
+#define IS_SET(_name) \
+      rpt_vstring(d1, "%-20s Defined", #_name ":");
+#define NOT_SET(_name) \
+      rpt_vstring(d1, "%-20s Not defined", #_name ":");
+
+#ifdef BUILD_SHARED_LIB
+   IS_SET(BUILD_SHARED_LIB);
+#else
+   NOT_SET(BUILD_SHARED_LIB);
+#endif
+
+#ifdef ENABLE_ENVCMDS
+   IS_SET(ENABLE_ENVCMDS);
+#else
+   NOT_SET(ENABLE_ENVCMDS);
+#endif
+
+#ifdef ENABLE_FAILSIM
+   IS_SET(ENABLE_FAILSIM);
+#else
+   NOT_SET(ENABLE_FAILSIM);
+#endif
+
+#ifdef ENABLE_UDEV
+   IS_SET(ENABLE_UDEV);
+#else
+   NOT_SET(ENABLE_UDEV);
+#endif
+
+#ifdef USE_X11
+   IS_SET(USE_X11);
+#else
+   NOT_SET(USE_X11);
+#endif
+
+#ifdef USE_LIBDRM
+   IS_SET(USE_LIBDRM);
+#else
+   NOT_SET(USE_LIBDRM);
+#endif
+
+#ifdef USE_USB
+   IS_SET(USE_USB);
+#else
+   NOT_SET(USE_USB);
+#endif
+
+#ifdef WITH_ASAN
+   IS_SET(WITH_ASAN);
+#else
+   NOT_SET(WITH_ASAN);
+#endif
+
+   rpt_nl();
+
+   rpt_label(depth, "Private Build Options:");
+
+#ifdef TARGET_LINUX
+   IS_SET(TARGET_LINUX);
+#else
+   NOT_SET(TARGET_LINUX);
+#endif
+
+#ifdef TARGET_BSD
+   IS_SET(TARGET_BSD);
+#else
+   NOT_SET(TARGET_BSD);
+#endif
+
+#ifdef INCLUDE_TESTCASES
+   IS_SET(INCLUDE_TESTCASES);
+#else
+   NOT_SET(INCLUDE_TESTCASES);
+#endif
+
+   rpt_nl();
+}
+
--- src/base/build_info.h
+++ src/base/build_info.h
@@ -1,30 +1,10 @@
 /** \file build_info.h
  *
- *  Build Information: version, build timestamp, etc.
+ *  Build Information: version, compilation options, etc.
  */
 
-/* build_info.h
- *
- * <copyright>
- * Copyright (C) 2014-2018 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
+// Copyright (C) 2014-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef BUILD_INFO_H_
 #define BUILD_INFO_H_
@@ -33,5 +13,7 @@ extern const char * BUILD_VERSION;
 
 // extern const char * BUILD_DATE;    // unimplemented
 // extern const char * BUILD_TIME;    // unimplemented
+
+void report_build_options(int depth);
 
 #endif /* BUILD_INFO_H_ */
