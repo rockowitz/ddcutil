bebfd210035b139120e1491c941f842daa192df3
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-02-11 11:06:00 -0500

    changes for determining if i2c-dev is built in to kernel
    linux_util.c/h:
    - is_module_builtin():
      - look for modules.builtin under /lib64 and /lib32 as well as /lib
    - get_kernel_config_parm():
      - new function that get the value of a kernel compilation build parameter
        from file /boot/config*
      - an alternative way to check if module is built into kernel, withoug
        being concerned with the location modules.builtin
    - is_readable_file():
      - checks that a file is readable by simply trying to open it and read,
        without having to be concerned with all the caveats for access() etc

--- src/util/linux_util.c
+++ src/util/linux_util.c
@@ -2,28 +2,116 @@
  * Miscellaneous Linux utilities
  */
 
-// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2020-2021 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <glib-2.0/glib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <sys/utsname.h>
+#include <unistd.h>
 
 #include "file_util.h"
 #include "string_util.h"
 
-/** Checks if a module is built into the kernel.
+bool is_readable_file(const char * filename)
+{
+   // avoid all the rules re permissions, file type, links, ls etc
+   // just try to read from the file
+   bool result = false;
+   int fd = open(filename, O_RDONLY);
+   if (fd > 0) {
+      char buf;
+      if (read(fd, &buf, 1) > 0)
+         result = true;
+      close(fd);
+   }
+   return result;
+}
+
+
+/** Gets the value of a kernel configuration parameter from file
+  * /boot/config-KERNEL_RELEASE", where KERNEL_RELEASE is the kernel release name.
   *
-  * \param  module_name  simple module name, as it appears in the file system, e.g. i2c-dev
-  * \return true/false
+  * @param     parm_name   parameter name
+  * @param     buffer      buffer in which to return value
+  * @param     bufsz       size of buffer
+  * @parm      1           configuration parm found, value is in buffer
+  * @retval    0           configuration parm not found
+  * @retval    < 0         error reading configuration file
   */
-bool is_module_builtin(char * module_name)
+
+int get_kernel_config_parm(const char * parm_name, char * buffer, int bufsz)
 {
    bool debug = false;
-   bool result = false;
+   if (debug)
+      printf("(%s) Staring. parm_name=%s, buffer=%p, bufsz=%d\n",
+             __func__, parm_name, buffer, bufsz);
+   buffer[0] = '\n';
+
+   struct utsname utsbuf;
+   int rc = uname(&utsbuf);
+   assert(rc == 0);
+
+   char config_fn[100];
+   snprintf(config_fn, 100, "/boot/config-%s", utsbuf.release);
+
+   char search_str[40];
+    snprintf(search_str, 40, "%s=", parm_name);
+    if (debug)
+       printf("(%s) search_str=|%s|, len=%ld\n", __func__, search_str, (unsigned long) strlen(search_str));
+
+    GPtrArray * lines = g_ptr_array_new_full(400, g_free);
+    char * terms[2];
+    terms[0] = search_str;
+    terms[1] = NULL;
+    int unfiltered_ct = read_file_with_filter(lines, config_fn, terms, false, 0);
+    if (unfiltered_ct < 0) {
+       rc = unfiltered_ct;  // -errno
+    }
+    else if (lines->len == 0) {   // count after filtering
+       rc = 0;
+    }
+    else {
+       assert(lines->len == 1);
+       char * aline = g_ptr_array_index(lines, 0);
+       char * value = aline + strlen(search_str);
+       if (debug)
+          printf("(%s) strlen(search_str)=%ld aline=%p->|%s|, value=%p->|%s|\n",
+                __func__, (unsigned long)strlen(search_str), aline, aline, value, value);
+       assert(strlen(value) < bufsz);
+       // snprintf(buffer, bufsz, "%s", value);
+       strcpy(buffer, value);
+       rc = 1;
+    }
+    g_ptr_array_free(lines, true);
+
+    if (debug)
+       printf("(%s) rc=%d, strlen(buffer) = %ld\n", __func__, rc, (unsigned long) strlen(buffer));
+
+    ASSERT_IFF(rc==1, strlen(buffer) > 0);
+    if (debug)
+       printf("(%s) Done. parm=%s, returning %d, result=%s\n",
+              __func__,  parm_name, rc, buffer);
+    return rc;
+}
+
+
+
+/** Reads the modules.builtin file to see a module is built into the kernel.
+  *
+  * \param  module_name  simple module name, as it appears in the file system, e.g. i2c-dev
+  * \retval 1   is built in
+  * \retval 0   not built in
+  * \retval < 0 error reading the modules.builtin file, value is -errno
+  */
+int is_module_builtin(char * module_name)
+{
+   bool debug = false;
+   int result = 0;
 
    struct utsname utsbuf;
    int rc = uname(&utsbuf);
@@ -32,27 +120,40 @@ bool is_module_builtin(char * module_name)
    char modules_builtin_fn[100];
    snprintf(modules_builtin_fn, 100, "/lib/modules/%s/modules.builtin", utsbuf.release);
 
-   char ko_name[40];
-   snprintf(ko_name, 40, "%s.ko", module_name);
+   if ( !is_readable_file(modules_builtin_fn) ) {
+      snprintf(modules_builtin_fn, 100, "/lib64/modules/%s/modules.builtin", utsbuf.release);
+      if (!is_readable_file(modules_builtin_fn))  {
+         if ( !is_readable_file(modules_builtin_fn) ) {
+            snprintf(modules_builtin_fn, 100, "/lib32/modules/%s/modules.builtin", utsbuf.release);
+            if (!is_readable_file(modules_builtin_fn))  {
+               modules_builtin_fn[0] = '\n';
+               result = -errno;
+            }
+         }
+      }
+   }
+   if (result == 0) {
+      char ko_name[40];
+      snprintf(ko_name, 40, "%s.ko", module_name);
 
-   result = false;
-   GPtrArray * lines = g_ptr_array_new_full(400, g_free);
-   char * terms[2];
-   terms[0] = ko_name;
-   terms[1] = NULL;
-   int unfiltered_ct = read_file_with_filter(lines, modules_builtin_fn, terms, false, 0);
-   if (unfiltered_ct < 0) {
-      char buf[100];
-      strerror_r(errno, buf, 100);
-      fprintf(stderr, "Error reading file %s: %s\n", modules_builtin_fn, buf);
-      fprintf(stderr, "Assuming module %s is not built in to kernel\n", module_name);
+      GPtrArray * lines = g_ptr_array_new_full(400, g_free);
+      char * terms[2];
+      terms[0] = ko_name;
+      terms[1] = NULL;
+      int unfiltered_ct = read_file_with_filter(lines, modules_builtin_fn, terms, false, 0);
+      if (unfiltered_ct < 0) {
+         result = unfiltered_ct;   // -errno
+      }
+      else if (lines->len == 0)
+         result = 0;
+      else {
+         assert(lines->len == 1);
+         result = 1;
+      }
+      g_ptr_array_free(lines, true);
    }
-   else {
-      result = (lines->len == 1);
-   }
-   g_ptr_array_free(lines, true);
    if (debug)
-      printf("(%s) module_name=%s, returning %s\n",__func__,  module_name, sbool(result));
+      printf("(%s) module_name=%s, returning %d\n",__func__,  module_name, result);
    return result;
 }
 
--- src/util/linux_util.h
+++ src/util/linux_util.h
@@ -2,7 +2,7 @@
  *  Miscellaneous Linux utiliites
  */
 
-// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2021 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef LINUX_UTIL_H_
@@ -10,6 +10,8 @@
 
 #include <stdbool.h>
 
-bool is_module_builtin(char * module_name);
+int get_kernel_config_parm(const char * parm_name, char * buffer, int bufsz);
+
+int is_module_builtin(char * module_name);
 
 #endif /* LINUX_UTIL_H_ */
