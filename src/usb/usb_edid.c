cfae050c759060dc99cafc5eca6694fa003b72f9
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-02-20 21:55:45 -0500

    include glib-2.0/glib.h instead of glib.h

--- src/usb/usb_edid.c
+++ src/usb/usb_edid.c
@@ -12,7 +12,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <glib.h>
+#include <glib-2.0/glib.h>
 #include <linux/hiddev.h>
 #include <linux/limits.h>
 #include <stddef.h>
@@ -21,7 +21,6 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-// #include <wchar.h>
 
 #include "util/device_id_util.h"
 #include "util/report_util.h"
