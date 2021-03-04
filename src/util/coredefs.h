e250309f445cadf634d4c2f157d52a3814e64d83
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-03-04 17:42:38 -0500

    revert to defining STRCPY() as (void) g_strlcpy()
    The coverity tail has been wagging the valid code dog.

--- src/util/coredefs.h
+++ src/util/coredefs.h
@@ -12,9 +12,30 @@
 #include "config.h"
 #include "coredefs_base.h"
 
-// Workaround for coverity complaints re g_strlcpy(), g_strlcat()
 
+// defined in glib:
+// #define MIN(a,b) ( ((a) < (b) ? (a) : (b) )
+
+#ifdef NO
 #define STRLCPY(_dst, _src, _size) \
+   do { \
+      int ctr = 0;  \
+      while  ( ctr < ((_size)-1) && *(_src[ctr]) {  \
+         {_dest[ctr] = _src[ctr] } \
+         ctr++; \
+       } \
+      _dst[ctr] = '\0'; \
+   } while (0)
+#endif
+
+// i give up
+#define STRLCPY(_dst, _src, _size)  (void) g_strlcpy( (_dst), (_src), (_size) )
+
+
+
+// Workaround for coverity complaints re g_strlcpy(), g_strlcat()
+// there's a problem here, string was truncated
+#define STRLCPY0(_dst, _src, _size) \
 do { \
    assert(sizeof(_dst) >= 1); \
    strncpy(_dst, _src, _size); _dst[sizeof(_dst)-1] = '\0'; \
