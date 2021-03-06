67329a058cdbd5b94846bf0e7fefee3a18883634
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-03-06 02:11:40 -0500

    changes for use with libddcutil

--- src/cmdline/parsed_cmd.c
+++ src/cmdline/parsed_cmd.c
@@ -146,103 +146,105 @@ void dbgrpt_parsed_cmd(Parsed_Cmd * parsed_cmd, int depth) {
    int d1 = depth+1;
    int d2 = depth+2;
    rpt_structure_loc("Parsed_Cmd", parsed_cmd, depth);
-   rpt_str("raw_command",       NULL, parsed_cmd->raw_command,        d1);
-   rpt_int_as_hex( "cmd_id",    NULL, parsed_cmd->cmd_id,             d1);
+   if (parsed_cmd) {
+      rpt_str("raw_command",       NULL, parsed_cmd->raw_command,        d1);
+      rpt_int_as_hex( "cmd_id",    NULL, parsed_cmd->cmd_id,             d1);
 
-   rpt_structure_loc("pdid", parsed_cmd->pdid,                        d1);
-   if (parsed_cmd->pdid)
-       dbgrpt_display_identifier(parsed_cmd->pdid,                    d2);
+      rpt_structure_loc("pdid", parsed_cmd->pdid,                        d1);
+      if (parsed_cmd->pdid)
+          dbgrpt_display_identifier(parsed_cmd->pdid,                    d2);
 
-   rpt_structure_loc("fref", parsed_cmd->fref,                        d1);
-   if (parsed_cmd->fref)
-       dbgrpt_feature_set_ref(parsed_cmd->fref,                       d2);
+      rpt_structure_loc("fref", parsed_cmd->fref,                        d1);
+      if (parsed_cmd->fref)
+          dbgrpt_feature_set_ref(parsed_cmd->fref,                       d2);
 
-   rpt_int_as_hex(
-            "stats",            NULL, parsed_cmd->stats_types,                       d1);
-   rpt_bool("ddcdata",          NULL, parsed_cmd->flags & CMD_FLAG_DDCDATA,          d1);
-   rpt_str( "output_level",     NULL, output_level_name(parsed_cmd->output_level),   d1);
-   rpt_bool("force_slave_addr", NULL, parsed_cmd->flags & CMD_FLAG_FORCE_SLAVE_ADDR, d1);
-   rpt_bool("verify_setvcp",    NULL, parsed_cmd->flags & CMD_FLAG_VERIFY,           d1);
-   rpt_bool("timestamp_trace",  NULL, parsed_cmd->flags & CMD_FLAG_TIMESTAMP_TRACE,  d1);
-   rpt_int_as_hex(
-            "traced_groups",    NULL,  parsed_cmd->traced_groups,                    d1);
-   if (parsed_cmd->traced_functions) {
-      char * joined = g_strjoinv(", ", parsed_cmd->traced_functions);
-      rpt_str("traced_functions", NULL, joined, d1);
-      free(joined);
-   }
-   else
-      rpt_str("traced_functions", NULL, "none", d1);
-   if (parsed_cmd->traced_files) {
-      char * joined = g_strjoinv(", ", parsed_cmd->traced_files);
-      rpt_str("traced_files", NULL, joined, d1);
-      free(joined);
-   }
-   else
-      rpt_str("traced_files", NULL, "none", d1);
+      rpt_int_as_hex(
+               "stats",            NULL, parsed_cmd->stats_types,                       d1);
+      rpt_bool("ddcdata",          NULL, parsed_cmd->flags & CMD_FLAG_DDCDATA,          d1);
+      rpt_str( "output_level",     NULL, output_level_name(parsed_cmd->output_level),   d1);
+      rpt_bool("force_slave_addr", NULL, parsed_cmd->flags & CMD_FLAG_FORCE_SLAVE_ADDR, d1);
+      rpt_bool("verify_setvcp",    NULL, parsed_cmd->flags & CMD_FLAG_VERIFY,           d1);
+         rpt_bool("timestamp_trace",  NULL, parsed_cmd->flags & CMD_FLAG_TIMESTAMP_TRACE,  d1);
+         rpt_int_as_hex(
+                  "traced_groups",    NULL,  parsed_cmd->traced_groups,                    d1);
+         if (parsed_cmd->traced_functions) {
+            char * joined = g_strjoinv(", ", parsed_cmd->traced_functions);
+            rpt_str("traced_functions", NULL, joined, d1);
+            free(joined);
+         }
+         else
+            rpt_str("traced_functions", NULL, "none", d1);
+         if (parsed_cmd->traced_files) {
+            char * joined = g_strjoinv(", ", parsed_cmd->traced_files);
+            rpt_str("traced_files", NULL, joined, d1);
+            free(joined);
+         }
+         else
+            rpt_str("traced_files", NULL, "none", d1);
 
-   rpt_int( "argct",       NULL,  parsed_cmd->argct, d1);
-   int ndx = 0;
-   for (ndx = 0; ndx < parsed_cmd->argct; ndx++) {
-      printf("   argument %d:  %s\n", ndx, parsed_cmd->args[ndx]);
-   }
-   char buf[30];
-   g_snprintf(buf,30, "%d,%d,%d", parsed_cmd->max_tries[0], parsed_cmd->max_tries[1],
-                                     parsed_cmd->max_tries[2] );
-   rpt_str("raw_command",        NULL, parsed_cmd->raw_command,                               d1);
-   rpt_str("max_retries",        NULL, buf,                                                   d1);
+         rpt_int( "argct",       NULL,  parsed_cmd->argct, d1);
+         int ndx = 0;
+         for (ndx = 0; ndx < parsed_cmd->argct; ndx++) {
+            printf("   argument %d:  %s\n", ndx, parsed_cmd->args[ndx]);
+         }
+         char buf[30];
+         g_snprintf(buf,30, "%d,%d,%d", parsed_cmd->max_tries[0], parsed_cmd->max_tries[1],
+                                           parsed_cmd->max_tries[2] );
+         rpt_str("raw_command",        NULL, parsed_cmd->raw_command,                               d1);
+         rpt_str("max_retries",        NULL, buf,                                                   d1);
 
-   rpt_bool("enable_failure_simulation", NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_FAILSIM,   d1);
-   rpt_str("failsim_control_fn", NULL, parsed_cmd->failsim_control_fn,                        d1);
-   rpt_bool("nodetect",          NULL, parsed_cmd->flags & CMD_FLAG_NODETECT,                 d1);
-   rpt_bool("async",             NULL, parsed_cmd->flags & CMD_FLAG_ASYNC,                    d1);
-   rpt_bool("report_freed_exceptions", NULL, parsed_cmd->flags & CMD_FLAG_REPORT_FREED_EXCP,  d1);
-   rpt_bool("force",             NULL, parsed_cmd->flags & CMD_FLAG_FORCE,                    d1);
-   rpt_bool("notable",           NULL, parsed_cmd->flags & CMD_FLAG_NOTABLE,                  d1);
-   rpt_bool("rw only",           NULL, parsed_cmd->flags & CMD_FLAG_RW_ONLY,                  d1);
-   rpt_bool("ro only",           NULL, parsed_cmd->flags & CMD_FLAG_RO_ONLY,                  d1);
-   rpt_bool("wo only",           NULL, parsed_cmd->flags & CMD_FLAG_WO_ONLY,                  d1);
-   rpt_bool("show unsupported",  NULL, parsed_cmd->flags & CMD_FLAG_SHOW_UNSUPPORTED,         d1);
-   rpt_bool("enable udf",        NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_UDF,               d1);
-   rpt_bool("enable usb",        NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_USB,               d1);
-   rpt_bool("timestamp prefix:", NULL, parsed_cmd->flags & CMD_FLAG_TIMESTAMP_TRACE,          d1);
-   rpt_bool("thread id prefix:", NULL, parsed_cmd->flags & CMD_FLAG_THREAD_ID_TRACE,          d1);
-   rpt_bool("enable cached capabilities:",
-                                 NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_CACHED_CAPABILITIES, d1);
-   rpt_bool("ignore cached capabilities:",
-                                 NULL, parsed_cmd->flags & CMD_FLAG_IGNORE_CACHED_CAPABILITIES, d1);
-// rpt_bool("clear persistent cache:",
-//                                 NULL, parsed_cmd->flags & CMD_FLAG_CLEAR_PERSISTENT_CACHE, d1);
-   rpt_str ("MCCS version spec", NULL, format_vspec(parsed_cmd->mccs_vspec),                  d1);
-// rpt_str ("MCCS version id",   NULL, vcp_version_id_name(parsed_cmd->mccs_version_id),      d1);
+         rpt_bool("enable_failure_simulation", NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_FAILSIM,   d1);
+         rpt_str("failsim_control_fn", NULL, parsed_cmd->failsim_control_fn,                        d1);
+         rpt_bool("nodetect",          NULL, parsed_cmd->flags & CMD_FLAG_NODETECT,                 d1);
+         rpt_bool("async",             NULL, parsed_cmd->flags & CMD_FLAG_ASYNC,                    d1);
+         rpt_bool("report_freed_exceptions", NULL, parsed_cmd->flags & CMD_FLAG_REPORT_FREED_EXCP,  d1);
+         rpt_bool("force",             NULL, parsed_cmd->flags & CMD_FLAG_FORCE,                    d1);
+         rpt_bool("notable",           NULL, parsed_cmd->flags & CMD_FLAG_NOTABLE,                  d1);
+         rpt_bool("rw only",           NULL, parsed_cmd->flags & CMD_FLAG_RW_ONLY,                  d1);
+         rpt_bool("ro only",           NULL, parsed_cmd->flags & CMD_FLAG_RO_ONLY,                  d1);
+         rpt_bool("wo only",           NULL, parsed_cmd->flags & CMD_FLAG_WO_ONLY,                  d1);
+         rpt_bool("show unsupported",  NULL, parsed_cmd->flags & CMD_FLAG_SHOW_UNSUPPORTED,         d1);
+      rpt_bool("enable udf",        NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_UDF,               d1);
+      rpt_bool("enable usb",        NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_USB,               d1);
+      rpt_bool("timestamp prefix:", NULL, parsed_cmd->flags & CMD_FLAG_TIMESTAMP_TRACE,          d1);
+      rpt_bool("thread id prefix:", NULL, parsed_cmd->flags & CMD_FLAG_THREAD_ID_TRACE,          d1);
+      rpt_bool("enable cached capabilities:",
+                                    NULL, parsed_cmd->flags & CMD_FLAG_ENABLE_CACHED_CAPABILITIES, d1);
+      rpt_bool("ignore cached capabilities:",
+                                    NULL, parsed_cmd->flags & CMD_FLAG_IGNORE_CACHED_CAPABILITIES, d1);
+   // rpt_bool("clear persistent cache:",
+   //                                 NULL, parsed_cmd->flags & CMD_FLAG_CLEAR_PERSISTENT_CACHE, d1);
+      rpt_str ("MCCS version spec", NULL, format_vspec(parsed_cmd->mccs_vspec),                  d1);
+   // rpt_str ("MCCS version id",   NULL, vcp_version_id_name(parsed_cmd->mccs_version_id),      d1);
 
 #ifdef FUTURE
-   char * interpreted_flags = vnt_interpret_flags(parsed_cmd->flags, cmd_flag_table, false, ", ");
-   rpt_str("flags", NULL, interpreted_flags, d1);
-   free(interpreted_flags);
+      char * interpreted_flags = vnt_interpret_flags(parsed_cmd->flags, cmd_flag_table, false, ", ");
+      rpt_str("flags", NULL, interpreted_flags, d1);
+      free(interpreted_flags);
 #endif
 
-   rpt_vstring(d1, "sleep multiplier: %9.1f", parsed_cmd->sleep_multiplier);
-   rpt_bool("timeout I2C IO:",   NULL, parsed_cmd->flags & CMD_FLAG_TIMEOUT_I2C_IO,          d1);
-   rpt_bool("reduce sleeps:",    NULL, parsed_cmd->flags & CMD_FLAG_REDUCE_SLEEPS,           d1);
-   rpt_bool("defer sleeps:",     NULL, parsed_cmd->flags & CMD_FLAG_DEFER_SLEEPS,            d1);
-   rpt_bool("dynamic_sleep_adjustment:", NULL, parsed_cmd->flags & CMD_FLAG_DSA,             d1);
-   rpt_bool("per_thread_stats:", NULL, parsed_cmd->flags & CMD_FLAG_PER_THREAD_STATS,        d1);
-   rpt_bool("x52 not fifo:",     NULL, parsed_cmd->flags & CMD_FLAG_X52_NO_FIFO,             d1);
-   rpt_int("setvcp value count:",NULL, parsed_cmd->setvcp_values->len,                       d1);
-   for (int ndx = 0; ndx < parsed_cmd->setvcp_values->len; ndx++) {
-      Parsed_Setvcp_Args * elem = &g_array_index(parsed_cmd->setvcp_values, Parsed_Setvcp_Args, ndx);
-      rpt_vstring(d2, "feature_code: 0x%02x, relative: %s, value: %s",
-                      elem->feature_code,
-                      setvcp_value_type_name(elem->feature_value_type),
-                      elem->feature_value);
+      rpt_vstring(d1, "sleep multiplier: %9.1f", parsed_cmd->sleep_multiplier);
+      rpt_bool("timeout I2C IO:",   NULL, parsed_cmd->flags & CMD_FLAG_TIMEOUT_I2C_IO,          d1);
+      rpt_bool("reduce sleeps:",    NULL, parsed_cmd->flags & CMD_FLAG_REDUCE_SLEEPS,           d1);
+      rpt_bool("defer sleeps:",     NULL, parsed_cmd->flags & CMD_FLAG_DEFER_SLEEPS,            d1);
+      rpt_bool("dynamic_sleep_adjustment:", NULL, parsed_cmd->flags & CMD_FLAG_DSA,             d1);
+      rpt_bool("per_thread_stats:", NULL, parsed_cmd->flags & CMD_FLAG_PER_THREAD_STATS,        d1);
+      rpt_bool("x52 not fifo:",     NULL, parsed_cmd->flags & CMD_FLAG_X52_NO_FIFO,             d1);
+      rpt_int("setvcp value count:",NULL, parsed_cmd->setvcp_values->len,                       d1);
+      for (int ndx = 0; ndx < parsed_cmd->setvcp_values->len; ndx++) {
+         Parsed_Setvcp_Args * elem = &g_array_index(parsed_cmd->setvcp_values, Parsed_Setvcp_Args, ndx);
+         rpt_vstring(d2, "feature_code: 0x%02x, relative: %s, value: %s",
+                         elem->feature_code,
+                         setvcp_value_type_name(elem->feature_value_type),
+                         elem->feature_value);
+      }
+      rpt_int( "edid_read_size:",   NULL, parsed_cmd->edid_read_size,                d1);
+      rpt_int( "i1",                NULL, parsed_cmd->i1,                            d1);
+      rpt_bool("f1",                NULL, parsed_cmd->flags & CMD_FLAG_F1,           d1);
+      rpt_bool("f2",                NULL, parsed_cmd->flags & CMD_FLAG_F2,           d1);
+      rpt_bool("f3",                NULL, parsed_cmd->flags & CMD_FLAG_F3,           d1);
+      rpt_bool("f4",                NULL, parsed_cmd->flags & CMD_FLAG_F4,           d1);
+      rpt_bool("f5",                NULL, parsed_cmd->flags & CMD_FLAG_F5,           d1);
+      rpt_bool("f6",                NULL, parsed_cmd->flags & CMD_FLAG_F6,           d1);
    }
-   rpt_int( "edid_read_size:",   NULL, parsed_cmd->edid_read_size,                d1);
-   rpt_int( "i1",                NULL, parsed_cmd->i1,                            d1);
-   rpt_bool("f1",                NULL, parsed_cmd->flags & CMD_FLAG_F1,           d1);
-   rpt_bool("f2",                NULL, parsed_cmd->flags & CMD_FLAG_F2,           d1);
-   rpt_bool("f3",                NULL, parsed_cmd->flags & CMD_FLAG_F3,           d1);
-   rpt_bool("f4",                NULL, parsed_cmd->flags & CMD_FLAG_F4,           d1);
-   rpt_bool("f5",                NULL, parsed_cmd->flags & CMD_FLAG_F5,           d1);
-   rpt_bool("f6",                NULL, parsed_cmd->flags & CMD_FLAG_F6,           d1);
 }
