15b7a7fe5bf1578393a62494f18a1da6c9c66a01
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-03-19 11:12:43 -0400

    delete old commented out declaration of init_msg_control()

--- src/base/core.h
+++ src/base/core.h
@@ -45,12 +45,6 @@
    assert(_struct_ptr && memcmp(_struct_ptr->marker, _marker_value, 4) == 0)
 
 //
-// Initialization
-//
-// void init_msg_control();
-
-
-//
 // Standard function call arguments and return values
 //
 
