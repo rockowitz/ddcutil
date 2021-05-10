4909d28904c1958643ea3de1570fe862b8fcf750
Sanford Rockowitz <rockowitz@minsoft.com>
2021-05-10 18:35:02 -0400

    init_tracing(): call set_trace_destination() if --s1 set

--- src/ddc/common_init.c
+++ src/ddc/common_init.c
@@ -47,6 +47,8 @@ init_tracing(Parsed_Cmd * parsed_cmd)
        dbgtrc_show_thread_id = true;                      // extern in core.h
     report_freed_exceptions = parsed_cmd->flags & CMD_FLAG_REPORT_FREED_EXCP;   // extern in core.h
     set_trace_levels(parsed_cmd->traced_groups);
+    if (parsed_cmd->s1)
+       set_trace_destination(parsed_cmd->s1);
     if (parsed_cmd->traced_functions) {
        for (int ndx = 0; ndx < ntsa_length(parsed_cmd->traced_functions); ndx++)
           add_traced_function(parsed_cmd->traced_functions[ndx]);
