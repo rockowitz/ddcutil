8ac989dbd2375e00339c699b05d56b3aa9b84d7b
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-05-21 14:47:07 -0400

    submaster_initializer(): call set_default_thread_output_level()
    submaster_initializer():
    - call set_default_thread_output_level() to set the output
      level any subsequently created threads, in addition to calling
      set_output_level() for the current thread

--- src/ddc/common_init.c
+++ src/ddc/common_init.c
@@ -177,7 +177,8 @@ submaster_initializer(Parsed_Cmd * parsed_cmd) {
     i2c_set_io_strategy(DEFAULT_I2C_IO_STRATEGY);
     ddc_set_verify_setvcp(parsed_cmd->flags & CMD_FLAG_VERIFY);
 
-    set_output_level(parsed_cmd->output_level);
+    set_output_level(parsed_cmd->output_level);  // current thread
+    set_default_thread_output_level(parsed_cmd->output_level); // for future threads
     enable_report_ddc_errors( parsed_cmd->flags & CMD_FLAG_DDCDATA );
     // TMI:
     // if (show_recoverable_errors)
