3864658becc81c3a390c604b3519b9b91a8faf4a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-10-27 18:09:23 -0400

    add master initializer for libmain files
    - function init_api_services() in api_services_internal.c
    - currently just a placeholder

---
+++ src/libmain/api_services_internal.c
@@ -0,0 +1,14 @@
+// api_services_internal.c
+
+#include <stdio.h>
+
+#include "api_capabilities_internal.h"
+
+// Copyright (C) 2021 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+void init_api_services() {
+   // printf("(%s) Executing...\n", __func__);
+   // init_api_capabilities();
+}
+
---
+++ src/libmain/api_services_internal.h
@@ -0,0 +1,11 @@
+// api_services_intenral.h
+
+// Copyright (C) 2021 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef API_SERVICES_INTERNAL_H_
+#define API_SERVICES_INTERNAL_H_
+
+void init_api_services();
+
+#endif /* API_SERVICES_INTERNAL_H_ */
