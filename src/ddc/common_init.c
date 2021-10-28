4dc6735b206b6ef60daf7ca4d8c1108ecd743a12
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-10-28 09:58:08 -0400

    move trace_to_syslog assignment to init_tracing()

--- src/ddc/common_init.c
+++ src/ddc/common_init.c
@@ -40,7 +40,8 @@ init_tracing(Parsed_Cmd * parsed_cmd)
    bool debug = false;
    if (debug)
       printf("(%s) Starting.\n",__func__);
-      
+   if (parsed_cmd->flags && (CMD_FLAG_SYSLOG|CMD_FLAG_F3))
+      trace_to_syslog = true;
    if (parsed_cmd->flags & CMD_FLAG_TIMESTAMP_TRACE)      // timestamps on debug and trace messages?
        dbgtrc_show_time = true;                           // extern in core.h
    if (parsed_cmd->flags & CMD_FLAG_WALLTIME_TRACE)       // wall timestamps on debug and trace messages?
