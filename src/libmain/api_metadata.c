e56982d7a7e65272bb0b49282214079a9d825909
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-11-04 01:17:05 -0400

    ddca_dbgrpt_feature_metadata(): turn off debug

--- src/libmain/api_metadata.c
+++ src/libmain/api_metadata.c
@@ -841,7 +841,7 @@ ddca_dbgrpt_feature_metadata(
       DDCA_Feature_Metadata * md,
       int                     depth)
 {
-   bool debug = true;
+   bool debug = false;
    DBGTRC_STARTING(debug, TRACE_GROUP, "");
    // rpt_push_output_dest(stdout);
    dbgrpt_ddca_feature_metadata(md, depth);
