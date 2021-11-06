de95eaf7d2de7099c0000515a067bbc823379d2a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-11-06 05:02:21 -0400

    merge_command_tokens(), apply_config_file(): improve tracing

--- src/util/ddcutil_config_file.c
+++ src/util/ddcutil_config_file.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <wordexp.h>
 
+#include "debug_util.h"
 #include "simple_ini_file.h"
 #include "string_util.h"
 #include "xdg_util.h"
@@ -170,47 +171,54 @@ int merge_command_tokens(
       char *** merged_argv_loc)
 {
    bool debug = false;
+   DBGF(debug, "Starting. old_argc=%d, old_argc=%p, config_token_ct=%d, config_tokens=%p, merged_argv_loc=%p",
+               old_argc, old_argv, config_token_ct, config_tokens, merged_argv_loc);
 
    *merged_argv_loc = NULL;
-   int merged_argv  = 0;
+   int merged_argc  = 0;
 
    if (config_token_ct > 0) {
       int new_ct = config_token_ct + old_argc + 1;
-      if (debug)
-         printf("(%s) config_token_ct = %d, argc=%d, new_ct=%d\n",
-               __func__, config_token_ct, old_argc, new_ct);
+      DBGF(debug, "config_token_ct = %d, argc=%d, new_ct=%d", config_token_ct, old_argc, new_ct);
       char ** combined = calloc(new_ct, sizeof(char *));
+      DBGF(debug, "Allocated combined=%p", combined);
       combined[0] = strdup(old_argv[0]);   // command
-      // printf("(%s) Allocated %p -> |%s|\n", __func__, combined[0], combined[0] );
+      if (debug)
+         printf("%s) Allocated combined[0] = %p -> |%s|\n", __func__, combined[0], combined[0] );
+      DBGF(debug, "Copying config_token_ct=%d config file tokens", config_token_ct);
       int new_ndx = 1;
       for (int prefix_ndx = 0; prefix_ndx < config_token_ct; prefix_ndx++, new_ndx++) {
          combined[new_ndx] = strdup(config_tokens[prefix_ndx]);
-         // printf("(%s) Allocated %p->%s\n", __func__, combined[new_ndx], combined[new_ndx]);
+         if(debug)
+            printf("(%s) Allocated combined[%d]=%p -> |%s|\n",
+                   __func__, new_ndx, combined[new_ndx], combined[new_ndx]);
       }
+      DBGF(debug, "Copying %d arguments from old_argv", old_argc-1);
       for (int old_ndx = 1; old_ndx < old_argc; old_ndx++, new_ndx++) {
          combined[new_ndx] = strdup(old_argv[old_ndx]);
-         // printf("(%s) Allocated %p->%s\n", __func__, combined[new_ndx], combined[new_ndx]);
+         if (debug) printf("(%s) Allocated combined[%d] = %p -> |%s|\n",
+               __func__, new_ndx, combined[new_ndx], combined[new_ndx]);
       }
       combined[new_ndx] = NULL;
       if (debug)
          printf("(%s) Final new_ndx = %d\n", __func__, new_ndx);
       *merged_argv_loc = combined;
-      merged_argv      = new_ct - 1;
-      assert(merged_argv == ntsa_length(combined));
+      merged_argc      = new_ct - 1;
+      assert(merged_argc == ntsa_length(combined));
    }
    else {
       *merged_argv_loc = ntsa_copy(old_argv, true);
-      merged_argv      = old_argc;
+      merged_argc      = old_argc;
    }
 
    if (debug) {
        printf("(%s) Returning %d, *merged_argv_loc=%p\n",
-              __func__, merged_argv, *merged_argv_loc);
-       printf("(%s) tokens:\n", __func__);
+              __func__, merged_argc, *merged_argv_loc);
+       printf("(%s) *merged_arv_loc tokens:\n", __func__);
        ntsa_show(*merged_argv_loc);
    }
 
-   return merged_argv;
+   return merged_argc;
 }
 
 
@@ -245,9 +253,7 @@ int apply_config_file(
       GPtrArray *  errmsgs)
 {
    bool debug = false;
-   if (debug)
-      printf("(%s) Starting. application_name=%s, errmsgs=%p\n",
-             __func__, application_name, errmsgs);
+   DBGF(debug, "Starting. application_name=%s, errmsgs=%p", application_name, errmsgs);
 
    *untokenized_config_options_loc = NULL;
    *configure_fn_loc = NULL;
@@ -262,9 +268,8 @@ int apply_config_file(
                            errmsgs,
                            debug);   // verbose
    ASSERT_IFF(read_config_rc==0, *untokenized_config_options_loc);
-   if (debug)
-      printf("(%s) read_ddcutil_config_file() returned %d, configure_fn: %s\n",
-             __func__, read_config_rc, *configure_fn_loc);
+   DBGF(debug, "read_ddcutil_config_file() returned %d, configure_fn: %s",
+             read_config_rc, *configure_fn_loc);
 
    if (read_config_rc == -ENOENT) {
       *new_argv_loc = ntsa_copy(old_argv, true);
@@ -278,10 +283,11 @@ int apply_config_file(
       char ** cmd_prefix_tokens = NULL;
       int prefix_token_ct =
             tokenize_options_line(*untokenized_config_options_loc, &cmd_prefix_tokens);
-      if (debug){
-         printf("(%s) prefix_token_ct = %d\n", __func__, prefix_token_ct);
+
+      DBGF(debug, "prefix_token_ct = %d, cmd_prefix_tokens: ", prefix_token_ct);
+      if (debug)
          ntsa_show(cmd_prefix_tokens);
-      }
+
       *new_argc_loc =  merge_command_tokens(
                           old_argc,
                           old_argv,
@@ -290,14 +296,15 @@ int apply_config_file(
                           new_argv_loc);
       assert(*new_argc_loc == ntsa_length(*new_argv_loc));
 
+      DBGF(debug, "calling ntsa_free() for cmd_prefix_tokens=%p ...", cmd_prefix_tokens);
       ntsa_free(cmd_prefix_tokens, true);
       // ntsa_free(old_argv, false);  // can't free system's argv
    }
 
    if (debug) {
-       printf("(%s) Done.     *new_argc_loc=%d, *new_argv_loc=%p, returning %d\n",
-                    __func__, *new_argc_loc, *new_argv_loc, result);
-       printf("(%s) tokens:\n", __func__);
+       DBGF(debug, "Done.     *new_argc_loc=%d, *new_argv_loc=%p, returning %d",
+                    *new_argc_loc, *new_argv_loc, result);
+       printf("(%s) *new_argv_loc tokens:\n", __func__);
        ntsa_show(*new_argv_loc);
        printf("(%s) *untokenized_config_options_loc=%p->|%s|\n",
               __func__, *untokenized_config_options_loc, *untokenized_config_options_loc);
