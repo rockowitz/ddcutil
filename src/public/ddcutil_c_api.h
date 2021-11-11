9c65edf6c029b1af75646a6e2f636fc7fe4f36be
Sanford Rockowitz <rockowitz@minsoft.com>
2021-11-10 20:38:24 -0500

    Remove "force slave address" functions from API
    Affects:
    - ddca_enable_force_slave_address()
    - ddca_is_force_slave_address_enabled()
    
    Turned out not to be needed by Clightd. Avoid publishing
    functions without a clear need.
    
    Function declarations archived in api_base_internal.h

--- src/public/ddcutil_c_api.h
+++ src/public/ddcutil_c_api.h
@@ -290,28 +290,6 @@ ddca_set_sleep_multiplier(double multiplier);
 double
 ddca_get_sleep_multiplier();
 
-/** Controls the force I2C slave address setting.
- *
- *  Normally, ioctcl operation I2C_SLAVE is used to set the I2C slave address.
- *  If that returns EBUSY and this setting is in effect, slave address setting
- *  is retried using operation I2C_SLAVE_FORCE.
- *
- *  \param[in] onoff true/false
- *  \return  prior value
- *  \since 1.2.1
- */
-bool
-ddca_enable_force_slave_address(bool onoff);
-
-/** Query the force I2C slave address setting.
- *
- *  \return true/false
- *  \since 1.2.1
- */
-bool
-ddca_is_force_slave_address_enabled(void);
-
-
 
 //
 // Output Redirection
