3ea0b92d9681ed3d5dc8c489fc1db5632082b10c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-11-19 09:27:02 -0500

    new source files for collecting sysfs filter functions

---
+++ src/util/sysfs_filter_functions.c
@@ -0,0 +1,8 @@
+// sysfs_filter_functions.c
+
+// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+ 
+
+
---
+++ src/util/sysfs_filter_functions.h
@@ -0,0 +1,15 @@
+// sysfs_filter_functions.h
+
+// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+ 
+
+#ifndef SYSFS_FILTER_FUNCTIONS_H_
+#define SYSFS_FILTER_FUNCTIONS_H_
+
+
+
+
+
+#endif /* SYSFS_FILTER_FUNCTIONS_H_ */
