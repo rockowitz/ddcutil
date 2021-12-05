8c54f25fb8355fea8a1bb22272be73f1a8de6575
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-12-05 15:47:04 -0500

    correct documentation of DDCA_TRC_RETRY

--- src/public/ddcutil_types.h
+++ src/public/ddcutil_types.h
@@ -146,7 +146,7 @@ typedef enum {
  DDCA_TRC_VCP   = 0x0200,       /**< VCP layer, feature definitions */
  DDCA_TRC_DDCIO = 0x0400,       /**< DDC IO functions */
  DDCA_TRC_SLEEP = 0x0800,       /**< low level sleeps */
- DDCA_TRC_RETRY = 0x1000,       /**< successful retries, subset of DDCA_TRC_SLEEP */
+ DDCA_TRC_RETRY = 0x1000,       /**< successful retries, subset of DDCA_TRC_DDCIO */
 
  DDCA_TRC_NONE = 0x0000,        /**< all tracing disabled    */
  DDCA_TRC_ALL  = 0xffff         /**< all tracing enabled     */
