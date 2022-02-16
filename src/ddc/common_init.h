90091a96617e30b0d427ee942414c4ab42fd5b7d
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2022-02-16 09:58:56 -0500

    correct copyright years

--- src/ddc/common_init.h
+++ src/ddc/common_init.h
@@ -1,6 +1,6 @@
 /** \file common_init.h */
 
-// Copyright (C) 2022 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2021-2022 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef COMMON_INIT_H_
