d51210d76e0284aae882fc5e9680823a47e64139
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2022-02-27 08:22:11 -0500

    change DEFAULT_I2C_IO_STRATEGY to I2C_IO_STRATEGY_IOCTL

--- src/base/parms.h
+++ src/base/parms.h
@@ -40,8 +40,8 @@
 //
 
 // One of the following 2 defines must be enabled:
-// #define DEFAULT_I2C_IO_STRATEGY  I2C_IO_STRATEGY_IOCTL ///< Use ioctl() calls
-#define DEFAULT_I2C_IO_STRATEGY        I2C_IO_STRATEGY_FILEIO  ///< Use read() and write()
+#define DEFAULT_I2C_IO_STRATEGY  I2C_IO_STRATEGY_IOCTL ///< Use ioctl() calls
+// #define DEFAULT_I2C_IO_STRATEGY        I2C_IO_STRATEGY_FILEIO  ///< Use read() and write()
 #define DEFAULT_I2C_READ_BYTEWISE      false                   ///< Use single byte reads
 #define DEFAULT_EDID_WRITE_BEFORE_READ true
 #define DEFAULT_EDID_READ_SIZE           0                     ///< 128, 256, 0=>dynamic
