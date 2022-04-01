a792fd863be43a2c4b8c006094ea298cf273e28d
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2022-04-01 09:41:58 -0400

    report_performance_options(): iftest out call to is_sleep_supression_enabled()

--- src/app_ddcutil/main.c
+++ src/app_ddcutil/main.c
@@ -160,9 +160,9 @@ report_performance_options(int depth)
       int d1 = depth+1;
       rpt_label(depth, "Performance and Retry Options:");
       rpt_vstring(d1, "Deferred sleep enabled:                      %s", sbool( is_deferred_sleep_enabled() ) );
-// #ifdef SLEEP_SUPPRESSION
+#ifdef OLD   // SLEEP_SUPPRESSION
       rpt_vstring(d1, "Sleep suppression (reduced sleeps) enabled:  %s", sbool( is_sleep_suppression_enabled() ) );
-// #endif
+#endif
       bool dsa_enabled =  tsd_get_dsa_enabled_default();
       rpt_vstring(d1, "Dynamic sleep adjustment enabled:            %s", sbool(dsa_enabled) );
       if ( dsa_enabled )
