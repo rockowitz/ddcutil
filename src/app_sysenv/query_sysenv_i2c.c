3434761778acd163ccdda52c6070944683259d0c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2022-04-28 02:45:07 -0400

    insert #include "base/parms.h" before reference to I2C_IO_IOCTL_ONLY

--- src/app_sysenv/query_sysenv_i2c.c
+++ src/app_sysenv/query_sysenv_i2c.c
@@ -6,6 +6,8 @@
 // Copyright (C) 2014-2022 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "base/parms.h"    // for I2C_IO_IOCTL_ONLY
+
 /** \cond */
 #include <assert.h>
 #include <errno.h>
