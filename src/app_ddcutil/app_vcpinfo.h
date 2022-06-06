56b33c557863b85852ffcec28358a0e3f9d29474
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2022-06-06 01:13:06 -0400

    add include

--- src/app_ddcutil/app_vcpinfo.h
+++ src/app_ddcutil/app_vcpinfo.h
@@ -1,21 +1,21 @@
-/** \file app_vcpinfo.h
+/** @file app_vcpinfo.h
  *
- *  Implement vcpinfo and (deprecated) listvcp commands
+ *  Implement VCPINFO and (deprecated) LISTVCP commands
  */
 
-// Copyright (C) 2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2020-2022 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef APP_VCPINFO_H_
 #define APP_VCPINFO_H_
 
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "cmdline/parsed_cmd.h"
 
 void
-app_listvcp(
-      FILE * fh);
+app_listvcp(FILE * fh);
 
 bool
 app_vcpinfo(Parsed_Cmd * parsed_cmd);
