3d46b150f45d99ebbc39242b1378df41f03499f0
Sanford Rockowitz <rockowitz@minsoft.com>
2022-09-26 12:16:13 -0400

    fix comment
    change from 1.4.0-dev

--- src/base/linux_errno.h
+++ src/base/linux_errno.h
@@ -2,7 +2,7 @@
  * Linux errno descriptions
  */
 
-// Copyright (C) 2014-2021 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2022 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef BASE_LINUX_ERRNO_H_
