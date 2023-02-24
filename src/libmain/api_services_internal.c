93ef4167e80913a5a2fb51ee6f9d6d885f2d0f28
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-02-24 09:31:22 -0500

    init_api_services(): call init functions for several api_*.c files
    - init_api_feature_access(), init_api_displays(),
      init_api_capabilities(), init_api_metdata()

--- src/libmain/api_services_internal.c
+++ src/libmain/api_services_internal.c
@@ -1,14 +1,26 @@
-// api_services_internal.c
+/** @file api_services_internal.c */
+
+// Copyright (C) 2021-2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <stdio.h>
 
-#include "api_capabilities_internal.h"
+#include "base/core.h"
 
-// Copyright (C) 2021 Sanford Rockowitz <rockowitz@minsoft.com>
-// SPDX-License-Identifier: GPL-2.0-or-later
+#include "api_capabilities_internal.h"
+#include "api_displays_internal.h"
+#include "api_feature_access_internal.h"
+#include "api_metadata_internal.h"
+
+#include "api_services_internal.h"
+
 
 void init_api_services() {
-   // printf("(%s) Executing...\n", __func__);
-   // init_api_capabilities();
+   DBGMSG("Starting");
+   init_api_feature_access();
+   init_api_displays();
+   init_api_capabilities();
+   init_api_metadata();
+   DBGMSG("Done");
 }
 
