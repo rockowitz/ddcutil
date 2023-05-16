467eaa74891cf86ac72d32c205b2ddec173ef5e5
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-05-15 23:01:42 -0400

    ddc_store_displays_cache(): create path $HOME/.cache/ddcutil if it doesn't exist
    use fopen_mkdir() instead of fopen()
    
    also call SEVEREMSG() if error

--- src/ddc/ddc_serialize.c
+++ src/ddc/ddc_serialize.c
@@ -539,10 +539,10 @@ bool ddc_store_displays_cache() {
    bool ok = true;
    char * json_text = ddc_serialize_displays_and_buses();
    char * fn = get_displays_cache_file_name();
-   FILE * fp = fopen(fn, "w");
+   FILE * fp = fopen_mkdir(fn, "w");
    if (!fp) {
       // TODO: properly emit error message
-      DBGMSG("Error opening file %s:%s", fn, strerror(errno));
+      SEVEREMSG("Error opening file %s:%s", fn, strerror(errno));
    }
    else {
       size_t bytes_written = fwrite(json_text, strlen(json_text), 1, fp);
