00b07dca449e8eeca5fa0eee9a74ab8f40b0a0f5
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-05-25 13:12:43 -0400

    init_app_ddcutil_services(): call init_app_vcpinfo()

--- src/app_ddcutil/app_ddcutil_services.c
+++ src/app_ddcutil/app_ddcutil_services.c
@@ -1,8 +1,8 @@
-/** @file app_services.c
+/** @file app_ddcutil_services.c
  *  Initialize files in directory app_ddcutil
  */
 
-// Copyright (C) 2022 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2023 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "config.h"
@@ -16,6 +16,7 @@
 #endif
 #include "app_probe.h"
 #include "app_setvcp.h"
+#include "app_vcpinfo.h"
 #include "app_watch.h"
 #include "app_vcpinfo.h"
 
@@ -29,6 +30,7 @@ void init_app_ddcutil_services() {
 #endif
    init_app_probe();
    init_app_setvcp();
+   init_app_vcpinfo();
    init_app_watch();
    // main initialized by local init call
 }
