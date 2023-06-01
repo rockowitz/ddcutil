e655e287690e432729e5004fd0781ed0b309d6fb
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-06-01 17:33:57 -0400

    clear out stale declarations, format

--- src/ddc/ddc_serialize.h
+++ src/ddc/ddc_serialize.h
@@ -9,27 +9,16 @@
 #include <glib-2.0/glib.h>
 #include <stdbool.h>
 
-#include "base/core.h"
 #include "base/displays.h"
-#include "base/i2c_bus_base.h"
 
-extern bool       display_caching_enabled;
-extern GPtrArray* deserialized_displays;
-extern GPtrArray* deserialized_buses;
-
+extern bool   display_caching_enabled;
 void          ddc_enable_displays_cache(bool onoff);
-
-Display_Ref * ddc_find_deserialized_display(int busno, Byte* edidbytes);
-
-char *        ddc_serialize_displays_and_buses();
-GPtrArray *   ddc_deserialize_displays(const char * jstring);
-GPtrArray *   ddc_deserialize_buses(const char * jstring);
-
 char *        ddc_displays_cache_file_name();
 bool          ddc_store_displays_cache();
 void          ddc_restore_displays_cache();
 void          ddc_erase_displays_cache();
-
+Display_Ref * ddc_find_deserialized_display(int busno, Byte* edidbytes);
 void          init_ddc_serialize();
+void          terminate_ddc_serialize();
 
 #endif /* DDC_SERIALIZE_H_ */
