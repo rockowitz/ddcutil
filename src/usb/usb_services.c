a7346b219abaa957d778479ba30e2e2de72626cb
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-06-03 23:33:37 -0400

    manages initialization and termination for directory usb
    - functions: init_usb_services(), terminate_usb_services()

---
+++ src/usb/usb_services.c
@@ -0,0 +1,20 @@
+/** @file usb_services.c */
+
+// Copyright (C) 2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "usb_base.h"
+#include "usb_displays.h"
+#include "usb_edid.h"
+
+#include "usb_services.h"
+
+ void init_usb_services() {
+    init_usb_base();
+    init_usb_displays();
+    init_usb_edid();
+ }
+
+ void terminate_usb_services() {
+    terminate_usb_displays();
+ }
---
+++ src/usb/usb_services.h
@@ -0,0 +1,12 @@
+/** @file usb_services.h */
+
+// Copyright (C) 2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef USB_SERVICES_H_
+#define USB_SERVICES_H_
+
+void init_usb_services();
+void terminate_usb_services();
+
+#endif /* USB_SERVICES_H_ */
