ff7f38dfce24bc9d1ae15f6a1ffc604adbb98da8
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-06-09 22:39:29 -0400

    cleanup, reduce api_capabilities_internal.h to init_api_capabilities()

--- src/libmain/api_capabilities.c
+++ src/libmain/api_capabilities.c
@@ -388,6 +388,34 @@ bye:
 }
 
 
+#ifdef UNPUBLISHED
+// UNPUBLISHED
+/** Parses a capabilities string, and reports the parsed string
+ *  using the code of command "ddcutil capabilities".
+ *
+ *  The report is written to the current FOUT location.
+ *
+ *  The detail level written is sensitive to the current output level.
+ *
+ *  @param[in]  capabilities_string  capabilities string
+ *  @param[in]  dref                 display reference
+ *  @param[in]  depth  logical       indentation depth
+ *
+ *  @remark
+ *  This function exists as a development aide.  Internally, ddcutil uses
+ *  a different data structure than DDCA_Parsed_Capabilities.  That
+ *  data structure uses internal collections that are not exposed at the
+ *  API level.
+ *  @remark
+ *  Signature changed in 0.9.3
+ *  @since 0.9.0
+ */
+void ddca_parse_and_report_capabilities(
+      char *                    capabilities_string,
+      DDCA_Display_Ref          dref,
+      int                       depth);
+
+
 // UNPUBLISHED
 void
 ddca_parse_and_report_capabilities(
@@ -399,6 +427,7 @@ ddca_parse_and_report_capabilities(
       dyn_report_parsed_capabilities(pcaps, NULL, dref, 0);
       free_parsed_capabilities(pcaps);
 }
+#endif
 
 
 DDCA_Feature_List
--- src/libmain/api_capabilities_internal.h
+++ src/libmain/api_capabilities_internal.h
@@ -9,35 +9,6 @@
 
 #include "public/ddcutil_types.h"
 
-// UNPUBLISHED
-/** Parses a capabilities string, and reports the parsed string
- *  using the code of command "ddcutil capabilities".
- *
- *  The report is written to the current FOUT location.
- *
- *  The detail level written is sensitive to the current output level.
- *
- *  @param[in]  capabilities_string  capabilities string
- *  @param[in]  dref                 display reference
- *  @param[in]  depth  logical       indentation depth
- *
- *  @remark
- *  This function exists as a development aide.  Internally, ddcutil uses
- *  a different data structure than DDCA_Parsed_Capabilities.  That
- *  data structure uses internal collections that are not exposed at the
- *  API level.
- *  @remark
- *  Signature changed in 0.9.3
- *  @since 0.9.0
- */
-void ddca_parse_and_report_capabilities(
-      char *                    capabilities_string,
-      DDCA_Display_Ref          dref,
-      int                       depth);
-
-
-// DEPRECATED
-
 
 #ifdef UNUSED
 
