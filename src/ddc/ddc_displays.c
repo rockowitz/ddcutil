76d0f2a1bb5121be325eda88eb07f574cb023f7a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-06-26 06:51:59 -0400

    Only try to retore serialized display ref if slave addr x37 detected at I2C level
    The is the second of 3 changes intended to addres issue #322

--- src/ddc/ddc_displays.c
+++ src/ddc/ddc_displays.c
@@ -719,7 +719,9 @@ ddc_detect_all_displays(GPtrArray ** i2c_open_errors_loc) {
       I2C_Bus_Info * businfo = i2c_get_bus_info_by_index(busndx);
       if ( (businfo->flags & I2C_BUS_ADDR_0X50)  && businfo->edid ) {
          Display_Ref * dref = NULL;
-         if (display_caching_enabled) {
+         // Do not restore serialized display ref if slave address x37 inactive
+         // Prevents creating a display ref with stale contents
+         if (display_caching_enabled && businfo->flags&I2C_BUS_ADDR_0X37 ) {
             dref = copy_display_ref(ddc_find_deserialized_display(businfo->busno, businfo->edid->bytes));
             if (dref)
                dref->detail = businfo;
