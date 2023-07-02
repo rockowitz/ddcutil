5a424ecc9ec961e38744ef2d456f3a973757cff9
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-07-02 09:29:24 -0400

    option --f6 no longer used for internal DSA stats

--- src/app_ddcutil/app_experimental.c
+++ src/app_ddcutil/app_experimental.c
@@ -35,7 +35,7 @@ report_experimental_options(Parsed_Cmd * parsed_cmd, int depth)
    REPORT_FLAG_OPTION(3, "DDC Null Message never indicates invalid feature");
    REPORT_FLAG_OPTION(4, "Read strategy tests");
    REPORT_FLAG_OPTION(5, "EDID read does not use I2C layer");
-   REPORT_FLAG_OPTION(6, "Report internal DSA stats");
+   REPORT_FLAG_OPTION(6, "Unused");
    REPORT_FLAG_OPTION(7, "Enable displays cache");
    REPORT_FLAG_OPTION(8, "Use UDEV to watch for display changes");
 
