7f0c1c7eedfc62dd6942b95d6722d0f026c3edb6
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-07-22 08:13:21 -0400

    set dsa2_step_floor using --fl1

--- src/app_ddcutil/app_experimental.c
+++ src/app_ddcutil/app_experimental.c
@@ -43,7 +43,7 @@ report_experimental_options(Parsed_Cmd * parsed_cmd, int depth)
    REPORT_FLAG_OPTION(11,"Unused");
    REPORT_FLAG_OPTION(12,"Unused");
 
-   rpt_vstring(depth+1, "Utility option --i1:          set dsa2_step_floor");
+   rpt_vstring(depth+1, "Utility option --i1:          set dsa2_step_floor by step number");
    rpt_vstring(depth+1, "Utility option --i2:          NULL Response Hack Millis");
 
    rpt_vstring(depth+1, "Utility option --s1:          Unused");
@@ -51,7 +51,7 @@ report_experimental_options(Parsed_Cmd * parsed_cmd, int depth)
    rpt_vstring(depth+1, "Utility option --s3:          Unused");
    rpt_vstring(depth+1, "Utility option --s4:          Unused");
 
-   rpt_vstring(depth+1, "Utility option --fl1:         Unused");
+   rpt_vstring(depth+1, "Utility option --fl1:         set dsa2_step_floor, in milliseconds");
    rpt_vstring(depth+1, "Utility option --fl2:         Unused");
 
    rpt_nl();
