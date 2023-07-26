54bc18d2a8f59f49dc6147c7f43b04efafa2f722
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-07-25 20:36:08 -0400

    clean up doxydoc, copyright

--- src/util/x11_util.h
+++ src/util/x11_util.h
@@ -1,7 +1,6 @@
-/* x11_util.h
+/** @file x11_util.h  Utilities for X11
  *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
+ * Copyright (C) 2014-2023 Sanford Rockowitz <rockowitz@minsoft.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -18,11 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
- */
-
-/** @file x11_util.h
- * Utilities for X11
  */
 
 #ifndef X11_UTIL_H_
