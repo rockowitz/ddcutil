2ffd4a4aa60b7bfd22d16cee3d318791cb060c81
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-09-25 12:01:34 -0400

    is_kernel_module_builtin(): calling read_file_with_filter() with arg free_strings=true causes double free

--- src/util/linux_util.c
+++ src/util/linux_util.c
@@ -239,7 +239,7 @@ bool is_module_built_in(const char * module_name) {
     terms[0] = module_name1;
     terms[1] = module_name2;  // probably same as module_name1, but not worth optimizing
     terms[2] = NULL;
-    int unfiltered_ct = read_file_with_filter(lines, builtin_fn, terms, false, 0, true);
+    int unfiltered_ct = read_file_with_filter(lines, builtin_fn, terms, false, 0, false);
     if (unfiltered_ct < 0) {   //  = -errno
        fprintf(stderr, "Error reading file %s: %s\n", builtin_fn, strerror(errno));
        fprintf(stderr, "Assuming module %s is not built in to kernel\n", module_name);
