59cd22dfdb8c3307da9d31a9623cd8abd5ce120f
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-11-02 09:21:46 -0400

    ddc_report_display_by_dref(): only report dpms status for valid displays

--- src/ddc/ddc_display_ref_reports.c
+++ src/ddc/ddc_display_ref_reports.c
@@ -338,7 +338,7 @@ ddc_report_display_by_dref(Display_Ref * dref, int depth) {
          }
          if (msg) {
             rpt_vstring(d1, msg);
-            if (dref->flags & DREF_DPMS_SUSPEND_STANDBY_OFF) {
+            if (dref->dispno > 0 && (dref->flags & DREF_DPMS_SUSPEND_STANDBY_OFF)) {
                report_drm_dpms_status(d1, businfo->drm_connector_name);
             }
          }
