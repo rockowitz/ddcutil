ef2465d3b94fa104379b576c770afe291694e14a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-11-17 18:17:50 -0500

    document utility option --i1, sets i2c_businfo_async_threshold

--- src/app_ddcutil/app_experimental.c
+++ src/app_ddcutil/app_experimental.c
@@ -45,7 +45,7 @@ report_experimental_options(Parsed_Cmd * parsed_cmd, int depth)
    REPORT_FLAG_OPTION(13, "Unused");
    REPORT_FLAG_OPTION(14, "Do not try to get EDID from /sys");
 
-   rpt_vstring(depth+1, "Utility option --i1:          Unused");
+   rpt_vstring(depth+1, "Utility option --i1:          sets i2c_businfo_async_threshold");
    rpt_vstring(depth+1, "Utility option --i2:          NULL Response Hack Millis");
 
    rpt_vstring(depth+1, "Utility option --s1:          Unused");
