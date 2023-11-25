e0e64fb91982944dd9eff4a6e2d01eda275cb3f4
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2023-11-25 16:35:14 -0500

    bump copyright year

--- src/app_sysenv/query_sysenv_detailed_bus_pci_devices.c
+++ src/app_sysenv/query_sysenv_detailed_bus_pci_devices.c
@@ -4,7 +4,7 @@
  *  that performs minimal filtering of attributes.
  */
 
-// Copyright (C) 2022 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2022-2023 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
  /** \cond */
