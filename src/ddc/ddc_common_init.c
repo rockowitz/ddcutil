1fa816c70801d9dda3605d48f6b46866e3526527
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2024-02-05 15:24:14 -0500

    submaster_initializer(): use check_all_video_adapters_implement_drm()
    if tracing, report results all 3 variants for checking drm:
    - all_displays_drm2()
    - check_all_video_adapters_implement_drm()
    - all_sysfs_i2c_info_drm()

--- src/ddc/ddc_common_init.c
+++ src/ddc/ddc_common_init.c
@@ -410,15 +410,23 @@ submaster_initializer(Parsed_Cmd * parsed_cmd) {
    //    parsed_cmd->stats = true;
 
 #ifdef USE_LIBDRM
-   rpt_nl();
-   all_displays_drm2();
-   rpt_nl();
-   check_all_video_adapters_implement_drm();
-   rpt_nl();
-   drm_enabled = all_sysfs_i2c_info_drm(/*rescan=*/false);
+   // rpt_nl();
+   bool result1 = all_displays_drm2();
+   // rpt_nl();
+   bool result2 = check_all_video_adapters_implement_drm();
+   // rpt_nl();
+   bool result3 = all_sysfs_i2c_info_drm(/*rescan=*/false);
+   if (IS_DBGTRC(debug, DDCA_TRC_NONE)) {
+      DBGTRC_NOPREFIX(true, DDCA_TRC_NONE, "all_displays_drm2() returned %s", sbool(result1));
+      DBGTRC_NOPREFIX(true, DDCA_TRC_NONE, "check_all_video_adapters_implement_drm() returned %s", sbool(result2));
+      DBGTRC_NOPREFIX(true, DDCA_TRC_NONE, "all_sysfs_i2c_info_drm() returned %s", sbool(result3));
+   }
+
+   drm_enabled = result2;
    if (parsed_cmd->flags & CMD_FLAG_F12)
       drm_enabled = false;
-   rpt_nl();
+
+   // rpt_nl();
 #else
    drm_enabled = false;
 #endif
