fa4ee736c527c64b42637e6388a81285e80a20a3
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2024-02-07 04:38:55 -0500

    delete all_displays_drm2(), no longer in libdrm_util.c
    Please enter the commit message for your changes. Lines starting

--- src/util/libdrm_util.h
+++ src/util/libdrm_util.h
@@ -1,29 +1,9 @@
-/* libdrm_util.h
- *
- * <copyright>
- * Copyright (C) 2017 Sanford Rockowitz <rockowitz@minsoft.com>
- *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
+/** @file libdrm_util.h
+ *  Utilities for use with libdrm
  */
 
-/** @file libdrm_util.h
- * Utilities for use with libdrm
- */
+// Copyright (C) 2017-2024 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef LIBDRM_UTIL_H_
 #define LIBDRM_UTIL_H_
@@ -47,7 +27,5 @@ void report_drm_modeProperty(    drmModePropertyRes * p,          int depth);
 
 void summarize_drm_modeProperty(drmModePropertyRes * p, int depth);
 void report_property_value(int fd, drmModePropertyPtr prop_ptr, uint64_t prop_value, int depth) ;
-
-bool all_displays_drm2();
 
 #endif /* LIBDRM_UTIL_H_ */
