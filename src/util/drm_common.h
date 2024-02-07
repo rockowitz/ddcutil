ef005d434fcdde06665d82ff750d201c8b9b7bd0
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2024-02-07 07:43:09 -0500

    whitespace only

--- src/util/drm_common.h
+++ src/util/drm_common.h
@@ -11,17 +11,17 @@
 #include <stdbool.h>
 /** \endcond */
 
- #ifdef UNUSED
- Bit_Set_32   get_sysfs_drm_card_numbers();
- #endif
+#ifdef UNUSED
+Bit_Set_32   get_sysfs_drm_card_numbers();
+#endif
 
- bool         check_drm_supported_using_drm_api(char * busid2);
- bool         adapter_supports_drm_using_drm_api(const char * adapter_path);
- bool         all_displays_drm_using_drm_api();
- bool         check_video_adapters_list_implements_drm(GPtrArray * adapter_devices);
- GPtrArray *  get_video_adapter_devices();
- bool         check_all_video_adapters_implement_drm();
- GPtrArray *  get_dri_device_names_using_filesys();
- bool         all_video_adapters_support_drm_using_drm_api(GPtrArray * adapter_paths);
+bool         check_drm_supported_using_drm_api(char * busid2);
+bool         adapter_supports_drm_using_drm_api(const char * adapter_path);
+bool         all_displays_drm_using_drm_api();
+bool         check_video_adapters_list_implements_drm(GPtrArray * adapter_devices);
+GPtrArray *  get_video_adapter_devices();
+bool         check_all_video_adapters_implement_drm();
+GPtrArray *  get_dri_device_names_using_filesys();
+bool         all_video_adapters_support_drm_using_drm_api(GPtrArray * adapter_paths);
 
 #endif /* DRM_COMMON_H_ */
