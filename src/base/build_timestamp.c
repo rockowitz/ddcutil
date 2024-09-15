1cdcedd2d6627cba71ed626b55b8b2c523a718a5
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2024-09-15 15:54:41 -0400

    if BUILD_TIMESTAMP not defined, set BUILD_DATE, BUILD_TIME to "Not set"

--- src/base/build_timestamp.c
+++ src/base/build_timestamp.c
@@ -6,8 +6,16 @@
 // Copyright (C) 2024 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "config.h"
+
 #include "base/build_details.h"       // created by Makefile
 #include "base/build_timestamp.h"
 
+#ifdef BUILD_TIMESTAMP
 const char * BUILD_DATE = __DATE__;
 const char * BUILD_TIME = __TIME__;
+#else
+const char * BUILD_DATE = "Not set";
+const char * BUILD_TIME = "Not set";
+#endif
+
