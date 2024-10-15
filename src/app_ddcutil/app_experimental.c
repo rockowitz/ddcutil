6c20f6f182d695b9156a867610e9760f86cc0fb8
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2024-10-15 04:06:57 -0400

    --f16 controls whether alternative display watch alrorithm is used

--- src/app_ddcutil/app_experimental.c
+++ src/app_ddcutil/app_experimental.c
@@ -61,7 +61,7 @@ report_experimental_options(Parsed_Cmd * parsed_cmd, int depth)
    REPORT_FLAG_OPTION(15, "Unused");
 #endif
    // REPORT_FLAG_OPTION(16, "Simple report /sys/class/drm");
-   REPORT_FLAG_OPTION(16, "Force  Watch_Mode_Udev_I2C");
+   REPORT_FLAG_OPTION(16, "Force  Watch_Mode_Full_Poll");
    REPORT_FLAG_OPTION(17, "Do not use sysfs connector_id");
    REPORT_FLAG_OPTION(18, "Always report UDEV events");
    REPORT_FLAG_OPTION(19, "Unused");
