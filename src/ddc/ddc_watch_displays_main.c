629e083a87443192edf7b7e9502c533d4e47de4a
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2024-10-17 22:51:01 -0400

    fix up includes

--- src/ddc/ddc_watch_displays_main.c
+++ src/ddc/ddc_watch_displays_main.c
@@ -48,7 +48,7 @@
 #include "i2c/i2c_sysfs_base.h"
 #include "i2c/i2c_bus_core.h"
 #include "i2c/i2c_dpms.h"
-#include <i2c/i2c_sys_drm_connector.h>
+#include "i2c/i2c_sys_drm_connector.h"
 
 #include "ddc/ddc_displays.h"
 #include "ddc/ddc_packet_io.h"
