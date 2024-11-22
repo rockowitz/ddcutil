5d77c20c646ef3ad5c04f1868295d87f33dfb35d
Sanford Rockowitz <rockowitz@minsoft.com>
2024-11-22 04:40:37 -0500

    add option --disable_ddc

--- src/cmdline/cmd_parser_goption.c
+++ src/cmdline/cmd_parser_goption.c
@@ -1239,6 +1239,7 @@ parse_command(
       {"ignore-usb-vid-pid", '\0', 0, G_OPTION_ARG_STRING_ARRAY, &ignored_vid_pid, "USB device to ignore","vid:pid" },
       {"ignore-hiddev", '\0', 0, G_OPTION_ARG_CALLBACK, ignored_hiddev_arg_func,  "USB device to ignore", "hiddev number"},
 #endif
+      {"disable-ddc",   '\0', 0, G_OPTION_ARG_STRING_ARRAY, &parsed_cmd->ddc_disabled,  "Disable DDC for monitor","monitor model id" },
 
 #ifdef FUTURE
       {"force-slave-address",
