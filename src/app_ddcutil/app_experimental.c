5d07e687671ae195188b4dc037737893b6465960
Sanford Rockowitz <rockowitz@minsoft.com>
2024-12-13 02:10:12 -0500

    bring up to date with current use
    --i6 now unused
    --i10 sets xevent polling loop millisec
    
    reflect renames of symbolic constants

--- src/app_ddcutil/app_experimental.c
+++ src/app_ddcutil/app_experimental.c
@@ -78,11 +78,11 @@ report_experimental_options(Parsed_Cmd * parsed_cmd, int depth)
    rpt_vstring(depth+1, "Utility option --i3:          flock_poll_millisec (default = %d)", DEFAULT_FLOCK_POLL_MILLISEC);
    rpt_vstring(depth+1, "Utility option --i4:          flock_max_wait_millisec (default = %d", DEFAULT_FLOCK_MAX_WAIT_MILLISEC);
    rpt_vstring(depth+1, "Utility option --i5:          Max retries for setvcp verification failure");
-   rpt_vstring(depth+1, "Utility option --i6:          Polling loop sleep multiplier");
+   rpt_vstring(depth+1, "Utility option --i6:          Unused");
    rpt_vstring(depth+1, "Utility option --i7           Stabilization poll millisec (default=%d)", DEFAULT_STABILIZATION_POLL_MILLISEC);
-   rpt_vstring(depth+1, "Utility option --i8:          Display watch udev loop millisec (default = %d)", DEFAULT_UDEV_POLL_LOOP_MILLISEC);
-   rpt_vstring(depth+1, "Utility option --i9:          Display watch non-udev polling loop millisec (default=%d)", DEFAULT_NONUDEV_POLL_LOOP_MILLISEC);
-   rpt_vstring(depth+1, "Utility option --i10:         Unused");
+   rpt_vstring(depth+1, "Utility option --i8:          Display watch udev loop millisec (default = %d)", DEFAULT_UDEV_WATCH_LOOP_MILLISEC);
+   rpt_vstring(depth+1, "Utility option --i9:          Display watch non-udev polling loop millisec (default=%d)", DEFAULT_POLL_WATCH_LOOP_MILLISEC);
+   rpt_vstring(depth+1, "Utility option --i10:         Display watch xevent polling loop millisec (default=%d)", DEFAULT_XEVENT_WATCH_LOOP_MILLISEC);
    rpt_vstring(depth+1, "Utility option --i11:         Unused");
    rpt_vstring(depth+1, "Utility option --i12:         Unused");
    rpt_vstring(depth+1, "Utility option --i13:         Unused");
