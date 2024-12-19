3068050177b81e6a0b5129cdfd83956963b73a33
Sanford Rockowitz <rockowitz@minsoft.com>
2024-12-19 00:09:41 -0500

    delete init_experimental_options()

--- src/app_ddcutil/app_experimental.h
+++ src/app_ddcutil/app_experimental.h
@@ -1,6 +1,6 @@
 /** \file app_experimental.h */
 
-// Copyright (C) 2021 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2021-2024 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef APP_EXPERIMENTAL_H_
@@ -8,7 +8,6 @@
 
 #include "cmdline/parsed_cmd.h"
 
-bool init_experimental_options(Parsed_Cmd* parsed_cmd);
 void report_experimental_options(Parsed_Cmd * parsed_cmd, int depth);
 
 void test_display_detection_variants();
