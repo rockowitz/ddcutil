ecf189631d180ff2bbd8c10b2d4e9ef4d4fc00b7
Sanford Rockowitz <rockowitz@minsoft.com>
2024-12-20 02:27:54 -0500

    ddc_emit_or_queue_display_status_event() calls adds dref_hash emtry

--- src/ddc/ddc_status_events.c
+++ src/ddc/ddc_status_events.c
@@ -201,7 +201,7 @@ void ddc_emit_or_queue_display_status_event(
       DDCA_IO_Path            io_path,
       GArray*                 queue)
 {
-   bool debug = false;
+   bool debug = true;
    if (dref) {
       DBGTRC_STARTING(debug, TRACE_GROUP, "dref=%p->%s, DREF_REMOVED=%s, event_type=%d=%s, connector_name=%s",
             dref, dref_repr_t(dref), SBOOL(dref->flags&DREF_REMOVED),
@@ -232,6 +232,9 @@ void ddc_emit_or_queue_display_status_event(
    }
    else
       ddc_emit_display_status_record(evt);
+
+   add_published_dref_id_by_dref(dref);
+
 
    DBGTRC_DONE(debug, TRACE_GROUP, "");
 }
