ab79612d3e517938ac13e85b150fe4b642522118
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2025-01-15 10:42:34 -0500

    check_busno(): memory leak

--- src/i2c/i2c_sysfs_base.c
+++ src/i2c/i2c_sysfs_base.c
@@ -485,7 +485,7 @@ bool check_busno(
       terminate = true;
       accum->connector_name = g_strdup(fn);
    }
-   free(cbn);
+   free_connector_bus_numbers(cbn);
 
    DBGTRC_RET_BOOL(debug, DDCA_TRC_NONE, terminate, "accum->connector_name = |%s|", accum->connector_name);
    return terminate;
