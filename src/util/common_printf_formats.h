7e60f0970ee003b7a49c56549192ef9a7814005c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2025-01-28 20:21:08 -0500

    defines PRItid, standardized printf format for thread id

---
+++ src/util/common_printf_formats.h
@@ -0,0 +1,11 @@
+/** @file common_printf_formats.h */
+
+// Copyright (C) 2025 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef COMMON_PRINTF_FORMATS_H_
+#define COMMON_PRINTF_FORMATS_H_
+
+#define PRItid "[%6jd]"
+
+#endif /* COMMON_PRINTF_FORMATS_H_ */
