8a5fc59483a657deb73b39affe2b159d38287e9e
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2025-02-06 07:06:18 -0500

    typo in NANOS2MILLIS()

--- src/util/timestamp.h
+++ src/util/timestamp.h
@@ -28,7 +28,7 @@ char *   formatted_time_t(uint64_t nanos);
 char *   formatted_epoch_time_t(time_t epoch_seconds);
 
 #define NANOS2MICROS( _nanosec ) (((_nanosec) + 500)/1000)
-#define NANOS2MILLIS( _namosec ) (((_nanosec)+500000)/1000000)
+#define NANOS2MILLIS( _nanosec ) (((_nanosec)+500000)/1000000)
 #define MILLIS2NANOS( _millisec) (_millisec*(uint64_t)1000000)
 #define MILLIS2MICROS(_millisec) (_millisec*(uint64_t)1000)
 
