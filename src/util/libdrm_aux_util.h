45e8b6021dc39b7ca2499d59537ae3b2fdcd4242
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2025-03-06 10:50:10 -0500

    whitespace only

--- src/util/libdrm_aux_util.h
+++ src/util/libdrm_aux_util.h
@@ -14,12 +14,12 @@
 #include "data_structures.h"
 /** \endcond */
 
-const char * drm_bus_type_name(uint8_t bus);
-bool         check_drm_supported_using_drm_api(char * busid2);
-bool         adapter_supports_drm_using_drm_api(const char * adapter_path);
-bool         all_displays_drm_using_drm_api();
-GPtrArray *  get_dri_device_names_using_filesys();
-bool         all_video_adapters_support_drm_using_drm_api(GPtrArray * adapter_paths);
+const char *  drm_bus_type_name(uint8_t bus);
+bool          check_drm_supported_using_drm_api(char * busid2);
+bool          adapter_supports_drm_using_drm_api(const char * adapter_path);
+bool          all_displays_drm_using_drm_api();
+GPtrArray *   get_dri_device_names_using_filesys();
+bool          all_video_adapters_support_drm_using_drm_api(GPtrArray * adapter_paths);
 None_Some_All check_drivers_support_drm_using_drm_api();
 
 #endif /* LIBDRM_AUX_UTIL_H_ */
