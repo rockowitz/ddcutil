b9084f89bdbe13f60661d780e525c045325b2de2
Sanford Rockowitz <rockowitz@minsoft.com>
2025-04-17 12:00:31 -0400

    bump copyright year

--- src/vcp/vcp_feature_codes.c
+++ src/vcp/vcp_feature_codes.c
@@ -3,7 +3,7 @@
  *  VCP Feature Code Table and related functions
  */
 
-// Copyright (C) 2014-2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2025 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 /** \cond */
