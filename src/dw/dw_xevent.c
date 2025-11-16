8fbb60ae2d9793c6f8c03ac5428c4a6217aed525
Sanford Rockowitz <rockowitz@minsoft.com>
2025-11-16 16:21:47 -0500

    add copyright statement

--- src/dw/dw_xevent.c
+++ src/dw/dw_xevent.c
@@ -1,5 +1,8 @@
 /** @file dw_xevent.c */
 
+// Copyright (C) 2024-2025 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include <stdbool.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
@@ -68,7 +71,8 @@ XEvent_Data * dw_init_xevent_screen_change_notification() {
       int min = 0;
       XRRQueryVersion(evdata->dpy, &maj, &min);
       int version = (maj << 8) | min;
-      if (version < 0x0102)     // is this the right version check?
+      if (version < 0x0102)     // is this the right version check// Copyright (C) 2024-2025 Sanford Rockowitz <rockowitz@minsoft.com>
+         // SPDX-License-Identifier: GPL-2.0-or-later?
          have_rr = false;
    }
    if (!have_rr) {
