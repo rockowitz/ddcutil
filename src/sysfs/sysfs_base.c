cebca7ea0598a615e8a22701c9a0e370d179aa62
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-02-10 05:14:55 -0500

    ignorable_i2c_device_sysfs_name() add "AMDGPU i2c bit bus*" to list of ignored prefixes

--- src/sysfs/sysfs_base.c
+++ src/sysfs/sysfs_base.c
@@ -1422,6 +1422,7 @@ ignorable_i2c_device_sysfs_name(const char * name, const char * driver) {
          "mac-io",       // Mac G5
          "u4",           // Mac G5
          "AMDGPU SMU",   // AMD Navi2 variants, e.g. RX 6000 series
+         "AMDGPU i2c bit bus",
          NULL };
    if (name) {
       if (starts_with_any(name, ignorable_prefixes) >= 0)
