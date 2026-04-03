f1125b0d95478fefe7b099ed7142b88928c7c44c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-03 07:32:14 -0400

    use dbus_error_emit() to output error msg
    also: clean up includes, format

--- src/util/dbus_util.c
+++ src/util/dbus_util.c
@@ -6,102 +6,104 @@
 // Copyright (C) 2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "config.h"
-
 /** \cond */
 #include <assert.h>
+#include <dbus-1.0/dbus/dbus.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <glib-2.0/glib.h>
-#include <grp.h>
 #include <inttypes.h>
-#include <pwd.h>
+#include <inttypes.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
-
-#include <sys/stat.h>
-#include <sys/utsname.h>
-#include <unistd.h>
-
-// for dbus
-#include <dbus-1.0/dbus/dbus.h>
-#include <stdio.h>
 #include <time.h>
-#include <inttypes.h>
-#include <stdlib.h>
+/** \endcond */
 
-
-
-
-#include "common_inlines.h"
 #include "debug_util.h"
-#include "file_util.h"
-#include "report_util.h"
 #include "string_util.h"
-#include "subprocess_util.h"
 
 #include "dbus_util.h"
+
+void dbus_error_emit(
+      DBusError    error,
+      const char * funcname,
+      int          lineno,
+      const char * filename)
+{
+   char * msg = g_strdup_printf("dbus error %s:%s in %s near line %d in file %s",
+         error.name, error.message, funcname, lineno, filename);
+   fprintf(stderr, "%s", msg);
+   syslog(LOG_ERR, "%s", msg);
+   free(msg);
+}
+
+#define DBUS_ERROR_EMIT(_error) \
+   dbus_error_emit(_error, __func__, __LINE__, __FILE__)
 
 
 uint64_t last_sleep_change_timestamp() {
    bool debug = false;
-    DBGF(debug, "Starting.");
+   DBGF(debug, "Starting.");
 
-    DBusError error;
-    dbus_error_init(&error);
-    uint64_t last_sleep_change_timestamp = 0;
+   DBusError error;
+   dbus_error_init(&error);
+   uint64_t last_sleep_change_timestamp = 0;
 
-    DBusConnection *conn = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
-    if (dbus_error_is_set(&error)) {
-         fprintf(stderr, "Connection Error (%s)\n", error.message);
-         syslog(LOG_ERR, "dbus connection error (%s)\n", error.message);
-         dbus_error_free(&error);
-         goto bye;
-    }
+   DBusConnection *conn = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
+   if (dbus_error_is_set(&error)) {
+         // fprintf(stderr, "Connection Error (%s)\n", error.message);
+         // syslog(LOG_ERR, "dbus connection error (%s)\n", error.message);
+      DBUS_ERROR_EMIT(error);
+      dbus_error_free(&error);
+      goto bye;
+   }
 
-    DBusMessage *dbus_msg = dbus_message_new_method_call("org.freedesktop.login1",
+   DBusMessage *dbus_msg = dbus_message_new_method_call("org.freedesktop.login1",
                                                      "/org/freedesktop/login1",
                                                      "org.freedesktop.DBus.Properties",
                                                      "Get");
-    if (!dbus_msg) {
-         fprintf(stderr, "dbus_message_new_method_call failed\n");
-         syslog(LOG_ERR, "dbus_message_new_method_call failed");
-         goto bye;
-    }
+   if (!dbus_msg) {
+      // fprintf(stderr, "dbus_message_new_method_call failed\n");
+      // syslog(LOG_ERR, "dbus_message_new_method_call failed");
+      DBUS_ERROR_EMIT(error);
+      goto bye;
+   }
 
-    char * arg1 = "org.freedesktop.login1.Manager";
-    char * arg2 = "IdleSinceHintMonotonic";
-    dbus_message_append_args(dbus_msg,
+   char * arg1 = "org.freedesktop.login1.Manager";
+   char * arg2 = "IdleSinceHintMonotonic";
+   dbus_message_append_args(dbus_msg,
                               DBUS_TYPE_STRING, &arg1,
                               DBUS_TYPE_STRING, &arg2,
                               DBUS_TYPE_INVALID);
 
-    DBusMessage *reply = dbus_connection_send_with_reply_and_block(
+   DBusMessage *reply = dbus_connection_send_with_reply_and_block(
            conn, dbus_msg, /* timeout_millis */ 3000, &error);
-    dbus_message_unref(dbus_msg);
-    if (dbus_error_is_set(&error)) {
-         fprintf(stderr, "Error (%s)\n", error.message);
-         syslog(LOG_ERR,  "dbus error (%s)\n", error.message);
-         dbus_error_free(&error);
-         goto bye;
-    }
+   dbus_message_unref(dbus_msg);
+   if (dbus_error_is_set(&error)) {
+      DBUS_ERROR_EMIT(error);
+      // fprintf(stderr, "Error (%s)\n", error.message);
+      // syslog(LOG_ERR,  "dbus error (%s)\n", error.message);
+      dbus_error_free(&error);
+      goto bye;
+   }
 
-    if (reply) {
-         DBusMessageIter outer, inner;
-         dbus_message_iter_init(reply, &outer);
-         dbus_message_iter_recurse(&outer, &inner);
+   if (reply) {
+      DBusMessageIter outer, inner;
+      dbus_message_iter_init(reply, &outer);
+      dbus_message_iter_recurse(&outer, &inner);
 
-         dbus_message_iter_get_basic(&inner, &last_sleep_change_timestamp);
-         DBGF(debug, "Last sleep change (monotonic usec): %" PRIu64 , last_sleep_change_timestamp);
-    }
+      dbus_message_iter_get_basic(&inner, &last_sleep_change_timestamp);
+      DBGF(debug, "Last sleep change (monotonic usec): %" PRIu64 , last_sleep_change_timestamp);
+   }
 
-    dbus_message_unref(reply);
-    dbus_connection_unref(conn);
+   dbus_message_unref(reply);
+   dbus_connection_unref(conn);
 
  bye:
-    DBGF(debug, "Done.    Returning %"PRIu64, last_sleep_change_timestamp);
-    return last_sleep_change_timestamp;
+   DBGF(debug, "Done.    Returning %"PRIu64, last_sleep_change_timestamp);
+   return last_sleep_change_timestamp;
 }
 
 
