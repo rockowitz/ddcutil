33067ddf9153adf7b42668066804595025af7bab
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-10 06:32:56 -0400

    fix doxydoc

--- src/sysfs/sysfs_services.h
+++ src/sysfs/sysfs_services.h
@@ -1,7 +1,7 @@
-/* @file sysfs_services.h
+/** @file sysfs_services.h
  */
 
-// Copyright (C) 2022-2025 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2022-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef SYSFS_SERVICES_H_
