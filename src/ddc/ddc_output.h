b2f160a3fdba249d355849fafea708e862222371
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-10 18:25:58 -0400

    doxydoc

--- src/ddc/ddc_output.h
+++ src/ddc/ddc_output.h
@@ -1,7 +1,7 @@
-/* \file ddc_output.h
+/** \file ddc_output.h
  */
 
-// Copyright (C) 2014-2024 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DDC_OUTPUT_H_
