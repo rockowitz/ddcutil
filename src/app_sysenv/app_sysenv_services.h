ae9e5140793953a94b245c2e9c2bbf47e015720b
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-12 07:48:38 -0400

    misplaced include guard

--- src/app_sysenv/app_sysenv_services.h
+++ src/app_sysenv/app_sysenv_services.h
@@ -1,14 +1,14 @@
-// app_sysenv_services.h
+/** @file app_sysenv_services.h */
 
-// Copyright (C) 2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2023-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef APP_SYSENV_SERVICES_H_
+#define APP_SYSENV_SERVICES_H_
 
 #include "query_sysenv_detailed_bus_pci_devices.h"
 #include "query_sysenv_sysfs.h"
 #include "query_sysenv.h"
-
-#ifndef APP_SYSENV_SERVICES_H_
-#define APP_SYSENV_SERVICES_H_
 
 void init_app_sysenv_services();
 
