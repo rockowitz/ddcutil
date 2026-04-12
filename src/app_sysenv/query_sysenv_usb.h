d773d6d8b5f8669df5c94e2b31aa50aad7a826aa
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-12 08:11:07 -0400

    fix doxygen header

--- src/app_sysenv/query_sysenv_usb.h
+++ src/app_sysenv/query_sysenv_usb.h
@@ -1,9 +1,9 @@
-/** @file query_usb_sysenv.h
+/** @file query_sysenv_usb.h
  *
  *  Probe the USB environment
  */
 
-// Copyright (C) 2014-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef QUERY_USB_SYSENV_H_
