5056c3299d14d19ab4b1e215a99d5b3010823133
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-12 02:59:29 -0400

    fixed @file name, name of include guard

--- src/app_ddcutil/app_ddcutil_services.h
+++ src/app_ddcutil/app_ddcutil_services.h
@@ -1,12 +1,12 @@
-/** @file app_services.h
+/** @file app_ddcutil_services.h
  *  Master initializer for app_ddcutil directory
  */
 
-// Copyright (C) 2022 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2022-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#ifndef APP_SERVICES_H_
-#define APP_SERVICES_H_
+#ifndef APP_DDCUTIL_SERVICES_H_
+#define APP_DDCUTIL_SERVICES_H_
 
 void init_app_ddcutil_services();
 
