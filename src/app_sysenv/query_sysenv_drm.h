c2decccd95cc1b3ff7a8bf493b5b7f05f190404e
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-12 08:18:21 -0400

    fix docygen header

--- src/app_sysenv/query_sysenv_drm.h
+++ src/app_sysenv/query_sysenv_drm.h
@@ -1,9 +1,9 @@
-/** @file query_drm_sysenv.h
+/** @file query_sysenv_drm.h
  *
  *  drm reporting for the environment command
  */
 
-// Copyright (C) 2017-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2017-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef QUERY_DRM_SYSENV_H_
