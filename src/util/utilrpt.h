e23994b8dece14d88b263cfb17321ce014df6a1f
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-13 17:02:13 -0400

    convert headers to current doxygen/spdx style

--- src/util/utilrpt.h
+++ src/util/utilrpt.h
@@ -1,31 +1,12 @@
-/* utilrpt.h
+/** \file utilrpt.h
  *
- * <copyright>
- * Copyright (C) 2014-2017 Sanford Rockowitz <rockowitz@minsoft.com>
+ * Report functions for other utility modules.
  *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * </endcopyright>
+ * Avoids complex dependencies between report_util.c and other util files.
  */
 
-/** \f 
-* Report functions for other utility modules. 
-* Avoids complex dependencies between report_util.c and other
-* util files.
-*/
+// Copyright (C) 2014-2026 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef UTILRPT_H_
 #define UTILRPT_H_
