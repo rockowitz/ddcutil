6765018297e65ad98dcccf5763c55aa866ec8364
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-13 17:03:52 -0400

    dpxygen fix

--- src/util/sysfs_util.c
+++ src/util/sysfs_util.c
@@ -6,7 +6,7 @@
 // Copyright (C) 2016-2024 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-//* \cond */
+/** \cond */
 #include <assert.h>
 #include <errno.h>
 #include <glib-2.0/glib.h>
