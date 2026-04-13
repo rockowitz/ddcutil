b58c5579577f5be114333c07d38dd0a474110eef
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-12 22:40:44 -0400

    rename include guard QUERY_USB_SYENV_H_ -> QUERY_SYSENV_USB_H_

--- src/app_sysenv/query_sysenv_usb.h
+++ src/app_sysenv/query_sysenv_usb.h
@@ -6,9 +6,9 @@
 // Copyright (C) 2014-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#ifndef QUERY_USB_SYSENV_H_
-#define QUERY_USB_SYSENV_H_
+#ifndef QUERY_SYSENV_USB_H_
+#define QUERY_SYSENV_USB_H_
 
 void query_usbenv();
 
-#endif /* QUERY_USB_SYSENV_H_ */
+#endif /* QUERY_SYSENV_USB_H_ */
