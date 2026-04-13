63a24258e44e839da36c50cf0a8c5a433d234f9b
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-13 16:45:46 -0400

    copyright year

--- src/vcp/persistent_capabilities.h
+++ src/vcp/persistent_capabilities.h
@@ -1,6 +1,6 @@
 /** \file persistent_capabilities.h */
 
-// Copyright (C) 2021-2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2021-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef PERSISTENT_CAPABILITIES_H_
