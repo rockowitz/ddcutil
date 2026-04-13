c3461b0672c1ecbd55e4d2ba3fd1a5ae2efcaeee
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-13 16:43:06 -0400

    fix doxygen header

--- src/vcp/persistent_capabilities.h
+++ src/vcp/persistent_capabilities.h
@@ -1,4 +1,4 @@
-/** \f persistent_capabilities.h */
+/** \file persistent_capabilities.h */
 
 // Copyright (C) 2021-2023 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
