4302320f87c5b33396fff652684a3bfa7dca2161
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-04-17 04:04:42 -0400

    delete include guard that does not belong in .c file

--- src/base/stats.c
+++ src/base/stats.c
@@ -1,14 +1,10 @@
 // stats.c
 
-// Copyright (C) 2018-2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "stats.h"
  
-#ifndef STATS_H
-#define STATS_H
-
-
 static char * retry_type_descriptions[] = {
       "write only",
       "write-read",
@@ -30,5 +26,3 @@ const char * retry_type_name(Retry_Operation type_id) {
 const char * retry_type_description(Retry_Operation type_id) {
    return retry_type_descriptions[type_id];
 }
-
-#endif
