342defe3aa312c30a07df61308d02824173eeea9
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-03 20:58:30 -0400

    i2c_open_bus_basic(): eliminate use of recently_resumed_by_clocktime()
    as implemented, each call incurs sleep, cumulatively

--- src/i2c/i2c_bus_core.c
+++ src/i2c/i2c_bus_core.c
@@ -392,7 +392,9 @@ i2c_open_bus_basic(const char * filename,  Byte callopts, int* fd_loc) {
    int eacces_retry_interval_ms = 200;
    int limit_eacces_retry_ct = 4;
 
+#ifdef BY_CLOCKTIME
    bool recently_resumed_by_clocktime = recently_resumed_from_sleep_by_clocktime();
+#endif
    int paused_ms = 0;
 #ifdef USE_DBUS
    paused_ms = ldbus_pause_if_recent_return_from_sleep(pause_after_resume_ms);
@@ -403,11 +405,14 @@ i2c_open_bus_basic(const char * filename,  Byte callopts, int* fd_loc) {
             "pause for %d millisec at start of i2c_open_bus_basic()", paused_ms);
    }
 #endif
+#ifdef BY_CLOCKTIME
    if (paused_ms == 0 && recently_resumed_by_clocktime) {
       SLEEP_MILLIS_WITH_SYSLOG(pause_after_resume_ms, "Pausing for recent resume by clocktime");
    }
    bool recently_resumed = paused_ms > 0 || recently_resumed_by_clocktime;
-
+#else
+   bool recently_resumed = paused_ms > 0;
+#endif
 
 retry:
    RECORD_IO_EVENT(
