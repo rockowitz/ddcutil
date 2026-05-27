e9dd9370c8935e5d8151d85d50baf606249b351d
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-27 13:17:22 -0400

    clean up includes

--- src/dw/dw_services.h
+++ src/dw/dw_services.h
@@ -3,16 +3,11 @@
  * dw layer initialization and configuration, statistics management
  */
 
-// Copyright (C) 2024-2025 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2024-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DW_SERVICES_H_
 #define DW_SERVICES_H_
-
-#include <stdbool.h>
-#include <stdio.h>
-
-#include "public/ddcutil_types.h"
 
 void init_dw_services();
 void terminate_dw_services();
