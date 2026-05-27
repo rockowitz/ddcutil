46e2358619196af97439908d13b13fe551f90559
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-27 11:33:24 -0400

    signature change i2c_check_bus(), i2c_get_and_check_bus_info()
    also delete declaration of cross_instance_locks_enabled,
    moved to flock.h

--- src/i2c/i2c_bus_core.h
+++ src/i2c/i2c_bus_core.h
@@ -26,7 +26,6 @@
 
 extern bool try_get_edid_from_sysfs_first;
 
-extern bool cross_instance_locks_enabled;
 extern bool edp_always_laptop;
 
 extern bool force_failure_i2c_open;
@@ -47,9 +46,16 @@ Error_Info *     i2c_open_bus(int busno, Display_Lock_Record lockrec, Byte callo
 Status_Errno     i2c_close_bus_basic(int busno, int fd, Call_Options callopts);
 Status_Errno     i2c_close_bus(int busno, int fd, Call_Options callopts);
 
+typedef enum {
+   EDID_STATUS_UNKNOWN,
+   EDID_EXISTS,
+   EDID_DOES_NOT_EXIST
+}  I2C_Check_Bus_Mode;
+
+
 // Bus inspection
-Error_Info *     i2c_check_bus(I2C_Bus_Info * businfo);
-I2C_Bus_Info *   i2c_get_and_check_bus_info(int busno);
+Error_Info *     i2c_check_bus(I2C_Bus_Info * businfo, I2C_Check_Bus_Mode check_bus_mode);
+I2C_Bus_Info *   i2c_get_and_check_bus_info(int busno,  I2C_Check_Bus_Mode check_bus_mode);
 bool             i2c_edid_exists(int busno);
 bool             i2c_check_edid_exists_by_dh(Display_Handle * dh);
 Error_Info *     i2c_check_open_bus_alive(Display_Handle * dh);
