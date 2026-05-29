f4a2ea336afda02954d4e0a6222062bbd54c07fd
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-29 10:12:36 -0400

    terminate_ddc_services(): call terminate_dw_services()

--- src/ddc/ddc_services.c
+++ src/ddc/ddc_services.c
@@ -261,6 +261,10 @@ void init_ddc_services() {
 void terminate_ddc_services() {
    bool debug = false;
    DBGTRC_STARTING(debug, DDCA_TRC_DDCIO, "");
+
+#ifdef WATCH_DISPLAYS
+   terminate_dw_services();
+#endif
    // ddc_stop_watch_displays(true,NULL);
    terminate_ddc_serialize();
    terminate_ddc_displays();  // must be called before terminate_ddc_packet_io()
