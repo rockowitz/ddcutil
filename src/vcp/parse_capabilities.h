29f81ca82d10f489cd87e46502c6221a93f9dff8
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-07-24 16:38:38 -0400

    parse_capabilities.h: declare capabilities_validity_name()
    The function has external linkage in parse_capabilities.c but was
    missing from the header; needed to call it from a standalone unit test.

--- src/vcp/parse_capabilities.h
+++ src/vcp/parse_capabilities.h
@@ -45,6 +45,7 @@ Parsed_Capabilities* parse_capabilities_string(const char * capabilities);
 void                 free_parsed_capabilities(Parsed_Capabilities * pcaps);
 Bit_Set_256          get_parsed_capabilities_feature_ids(Parsed_Capabilities * pcaps, bool readable_only);
 bool                 parsed_capabilities_supports_table_commands(Parsed_Capabilities * pcaps);
+char *               capabilities_validity_name(Parsed_Capabilities_Validity validity);
 // char *            parsed_capabilities_validity_name(Parsed_Capabilities_Validity validity); // unimplemented
 void                 dbgrpt_parsed_capabilities(Parsed_Capabilities * pcaps, int depth);
 void                 init_parse_capabilities();
