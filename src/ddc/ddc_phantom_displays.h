2b566df5f056873b2ee7666da6cf3706ac97fc6c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-07-24 17:21:17 -0400

    ddc_phantom_displays.h: declare drefs_edid_equal()
    The function has external linkage in ddc_phantom_displays.c but was
    missing from the header; needed to call it from a standalone unit test.

--- src/ddc/ddc_phantom_displays.h
+++ src/ddc/ddc_phantom_displays.h
@@ -9,8 +9,11 @@
 #include <stdbool.h>
 #include <glib-2.0/glib.h>
 
+#include "base/displays.h"
+
 extern bool  detect_phantom_displays;
 
+bool drefs_edid_equal(Display_Ref * dref1, Display_Ref * dref2);
 bool filter_phantom_displays(GPtrArray * all_displays);
 void init_ddc_phantom_displays();
 
