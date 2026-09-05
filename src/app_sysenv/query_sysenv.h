35bfc4738b886591314124a196951c1ba0641b24
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-09-05 01:51:28 -0400

    copyright year, whitespace

--- src/app_sysenv/query_sysenv.h
+++ src/app_sysenv/query_sysenv.h
@@ -3,7 +3,7 @@
  *  Primary file for the ENVIRONMENT command
  */
 
-// Copyright (C) 2014-2024 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef QUERY_SYSENV_H_
@@ -12,14 +12,11 @@
 #include <stdbool.h>
 
 #include "cmdline/parsed_cmd.h"
-
 #include "app_sysenv/query_sysenv_base.h"
 
 void final_analysis(Env_Accumulator * accum, int depth);
-
 void force_envcmd_settings(Parsed_Cmd * parsed_cmd);
 void query_sysenv(bool quickenv);
-
 void init_query_sysenv();
 
 #endif /* QUERY_SYSENV_H_ */
