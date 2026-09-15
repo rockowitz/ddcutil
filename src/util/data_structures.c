4ee0491e2bc032bc6d86c3171cd034be2ba341b0
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-09-14 21:56:27 -0400

    copy trival from ddcui, temporarily iftested out for transition

--- src/util/data_structures.c
+++ src/util/data_structures.c
@@ -33,6 +33,18 @@ const char * none_some_all_name(None_Some_All value) {
    return result;
 }
 
+#ifdef FUTURE
+const char * trival_repr(Optional_True_False value) {
+   char * result = NULL;
+   switch(value) {
+   case TRIVAL_TRUE:    result = "true";    break;
+   case TRIVAL_FALSE:   result = "false";   break;
+   case TRIVAL_UNSET:   result = "not set"; break;
+   }
+   return result;
+}
+#endif
+
 
 //
 // Buffer with length management
--- src/util/data_structures.h
+++ src/util/data_structures.h
@@ -34,6 +34,18 @@ typedef enum {
 const char * none_some_all_name(None_Some_All value);
 
 
+#ifdef FUTURE
+// Used to distinguish between an explicit value set vs no value set
+typedef enum {
+   TRIVAL_UNSET,
+   TRIVAL_FALSE,
+   TRIVAL_TRUE
+} Optional_True_False;
+
+const char * trival_repr(Optional_True_False value);
+#endif
+
+
 //
 // Buffer with length management
 //
